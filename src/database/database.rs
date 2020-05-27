c49f0ede16d7d8bd5d8c85a58e5a84489e4b6406
AppleTheGolden <scotsbox@protonmail.com>
2020-05-27 20:46:59 +0200

    Fix clippy warnings

--- src/database/database.rs
+++ src/database/database.rs
@@ -7,5 +7,5 @@ pub fn connect() -> PgConnection{
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set!");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    PgConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
--- src/routes/mod_editor.rs
+++ src/routes/mod_editor.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse, get, post};
+use actix_web::{web, HttpResponse, get};
 use handlebars::*;
 
 #[get("modeditor")]
