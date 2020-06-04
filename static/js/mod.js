90373806c0080e8bd6abbd39b154a88a887604eb
Jai A <jai.a@tuta.io>
2020-06-03 20:19:19 -0700

    Main mod page

--- static/js/mod.js
+++ static/js/mod.js
@@ -5,4 +5,12 @@ function toggleSection(element) {
     currentlySelected.classList.remove("mod-bar-active");
     currentlySelected = element;
     currentlySelected.classList.add("mod-bar-active");
+
+    currentlySelectedDiv.classList.remove("mod-show");
+    currentlySelectedDiv.classList.add("mod-hide");
+
+    currentlySelectedDiv = document.getElementById(element.id.replace("-bar", ""));
+
+    currentlySelectedDiv.classList.remove("mod-hide");
+    currentlySelectedDiv.classList.add("mod-show");
 }
