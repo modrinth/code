0900d7c76441e8f6f28f2803efc5103afe1c7668
Jai A <jai.a@tuta.io>
2020-06-27 10:29:49 -0700

    Add staging popup

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,7 @@ window.onload = function () {
 		document.documentElement.setAttribute("data-theme", localStorage.getItem("data-theme"));
 	}
 }
+
+function closePopup(e) {
+	e.parentElement.outerHTML = "";
+}
