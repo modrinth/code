75d67207aa981c4d157becc69a24312a2ae5c7a7
Jai A <jaiagr+gpg@pm.me>
2021-03-07 18:53:38 -0700

    Register users notification route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -69,6 +69,7 @@ pub fn users_config(cfg: &mut web::ServiceConfig) {
             .service(users::user_delete)
             .service(users::user_edit)
             .service(users::user_icon_edit)
+            .service(users::user_notifications)
             .service(users::user_follows),
     );
 }
