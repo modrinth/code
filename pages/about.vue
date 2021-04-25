40704c454927da1f8572476a0463d20030bcae53
Emma <emma@waffle.coffee>
2021-04-24 22:54:33 -0400

    Add more detailed infos to about page (#180)

--- pages/about.vue
+++ pages/about.vue
@@ -9,13 +9,51 @@
 
       <p>
         Our primary goal is to be as open as possible, with all our code being
-        Open Source, while giving back to the modding community as much as
-        possible.
+        <a target="_blank" href="https://github.com/modrinth">open source</a>,
+        while giving back to the modding community as much as possible.
       </p>
 
       <p>
         While we still are in beta, we hope we can be a major modding platform
         for all modders.
+      </p>
+      <h2>How does Modrinth work?</h2>
+      <p>
+        Modrinth is not just a website: it is made of several different
+        components that all come together to make a fast and flexible modding
+        platform.
+      </p>
+
+      <p>
+        On the technical level, Modrinth is made up of two main components: the
+        Rust-based backend named
+        <a target="_blank" href="https://github.com/modrinth/labrinth"
+          >Labrinth</a
+        >, and the Vue-based frontend named
+        <a target="_blank" href="https://github.com/modrinth/knossos">Knossos</a
+        >.
+      </p>
+
+      <p>
+        Additionally, some other custom-created resources exist, including but
+        not limited to:
+        <a target="_blank" href="https://github.com/modrinth/minotaur"
+          >Minotaur</a
+        >, a Gradle plugin for easily publishing mods to Modrinth, and
+        <a target="_blank" href="https://github.com/modrinth/minos">Minos</a>,
+        an authentication provider. All of Modrinth's code can be found on
+        <a target="_blank" href="https://github.com/modrinth">our GitHub page</a
+        >.
+      </p>
+      <h2>Backend Documentation</h2>
+      <p>
+        Documentation for the Modrinth API (Labrinth) can be found on the GitHub
+        repository's wiki
+        <a
+          target="_blank"
+          href="https://github.com/modrinth/labrinth/wiki/API-Documentation"
+          >here</a
+        >.
       </p>
     </div>
     <m-footer class="footer" centered />
@@ -68,5 +106,9 @@ export default {
 .container {
   @extend %card;
   padding: var(--spacing-card-sm) var(--spacing-card-lg);
+}
+
+a {
+  text-decoration: underline;
 }
 </style>
