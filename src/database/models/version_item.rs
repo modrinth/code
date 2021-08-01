8ece3b00f5a0cca045582ee9230829eeecc2c746
Geometrically <18202329+Geometrically@users.noreply.github.com>
2021-08-01 16:11:07 -0700

    Fix project dependencies returning invalid values (#231)
    * Fix project dependencies returning invalid values
    
    * Run prepare

--- src/database/models/version_item.rs
+++ src/database/models/version_item.rs
@@ -709,9 +709,9 @@ impl Version {
 
                         if dependency.len() >= 3 {
                             Some(QueryDependency {
-                                project_id: match &*dependency[2] {
+                                project_id: match &*dependency[1] {
                                     "0" => None,
-                                    _ => match dependency[2].parse() {
+                                    _ => match dependency[1].parse() {
                                         Ok(x) => Some(ProjectId(x)),
                                         Err(_) => None,
                                     },
@@ -723,7 +723,7 @@ impl Version {
                                         Err(_) => None,
                                     },
                                 },
-                                dependency_type: dependency[1].to_string(),
+                                dependency_type: dependency[2].to_string(),
                             })
                         } else {
                             None
@@ -834,9 +834,9 @@ impl Version {
 
                                 if dependency.len() >= 3 {
                                     Some(QueryDependency {
-                                        project_id: match &*dependency[2] {
+                                        project_id: match &*dependency[1] {
                                             "0" => None,
-                                            _ => match dependency[2].parse() {
+                                            _ => match dependency[1].parse() {
                                                 Ok(x) => Some(ProjectId(x)),
                                                 Err(_) => None,
                                             },
@@ -848,7 +848,7 @@ impl Version {
                                                 Err(_) => None,
                                             },
                                         },
-                                        dependency_type: dependency[1].to_string(),
+                                        dependency_type: dependency[2].to_string(),
                                     })
                                 } else {
                                     None
