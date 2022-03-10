1a23b6178be66dd2c9df9ef97d4526dc15b48708
Glare <admin@glaremasters.me>
2022-03-10 11:37:41 -0600

    Fixes confusing text in analytics description (#414)

--- privacy-toggles.js
+++ privacy-toggles.js
@@ -11,7 +11,7 @@ export default {
       title: 'Analytics',
       description: `Modrinth uses in-house tools that allow us to get insights into how each user is using the platform
        in order to improve the experience for everyone.\n By enabling this toggle, you allow us to gather information
-       across requests. Disabling this toggle will simply remove all PII from now on.`,
+       across requests. Disabling this toggle will simply remove all Personal Identifying Information from now on.`,
       default: true,
     },
   },
