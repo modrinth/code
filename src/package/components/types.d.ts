512bf1d0130719fd5e4bfe065bbf889a769401ba
venashial <venashial.levo@aleeas.com>
2022-05-31 19:13:36 -0700

    Update Option interface for type-checking

--- src/package/components/types.d.ts
+++ src/package/components/types.d.ts
@@ -4,5 +4,5 @@ export interface Option {
 	label: string
 	/** The element that will be in the `value` array while the option is checked */
 	value: string | number
-	icon?: SvelteComponentDev | string
+	icon?: SvelteComponentDev | string | any // SvelteComponentDev fails to type check
 }
