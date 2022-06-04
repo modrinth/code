0b16839d7ff16ee90f4a3f5c69d61b3261953191
Ellemes <3llemes@gmail.com>
2022-06-04 19:02:04 +0100

    Fix custom license being empty when editing a project. (#527)

--- pages/_type/_id/edit.vue
+++ pages/_type/_id/edit.vue
@@ -484,7 +484,9 @@ export default {
 
       switch (newValue.short) {
         case 'custom':
-          this.license_url = ''
+          if (oldValue === null || oldValue.short !== '') {
+            this.license_url = ''
+          }
           break
         default:
           this.license_url = `https://cdn.modrinth.com/licenses/${newValue.short}.txt`
