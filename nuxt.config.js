b16475b8bd352a75f6fc4e89219545d93f603430
frikinjay <88995340+frikinjay@users.noreply.github.com>
2022-08-07 00:36:55 +0530

    Change the link to Modrinth CDN from #581 (#582)
    * Fix the bunny fonts link from #580
    
    * Change the link to Modrinth CDN from #581

--- nuxt.config.js
+++ nuxt.config.js
@@ -114,7 +114,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.bunny.net/css2?family=Inter:wght@400;500;700;800&display=swap',
+        href: 'https://cdn-raw.modrinth.com/fonts/inter/inter.css',
       },
       {
         rel: 'search',
