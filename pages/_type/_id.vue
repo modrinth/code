456160fa2950facbd78bea48c4fe44e5d681e742
GreenTriangle <71787934+m44ster@users.noreply.github.com>
2022-10-17 23:41:44 +0200

    remove link to PolyMC (#693)

--- pages/_type/_id.vue
+++ pages/_type/_id.vue
@@ -505,9 +505,11 @@
               >our documentation</a
             >
             which provides instructions on using
-            <a href="https://atlauncher.com/about" target="_blank">ATLauncher</a
-            >, <a href="https://multimc.org/" target="_blank">MultiMC</a>, and
-            <a href="https://polymc.org/" target="_blank">PolyMC</a>.
+            <a href="https://atlauncher.com/about" target="_blank"
+              >ATLauncher</a
+            >
+            and <a href="https://multimc.org/" target="_blank">MultiMC</a>. Pack
+            creators
           </div>
           <Advertisement
             v-if="
