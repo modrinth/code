9ff3ab1a2b74d8412de3ca24e206327d48f47569
Jai A <jaiagr+gpg@pm.me>
2023-03-28 10:42:03 -0700

    lint

--- lib/components/nav/NavRow.vue
+++ lib/components/nav/NavRow.vue
@@ -81,8 +81,8 @@ export default {
     pickLink() {
       this.activeIndex = this.query
         ? this.filteredLinks.findIndex(
-          (x) => (x.href === '' ? undefined : x.href) === this.$route.path[this.query]
-        )
+            (x) => (x.href === '' ? undefined : x.href) === this.$route.path[this.query]
+          )
         : this.filteredLinks.findIndex((x) => x.href === decodeURIComponent(this.$route.path))
 
       if (this.activeIndex !== -1) {
