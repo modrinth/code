865a2fd8d8f4fb8526bc079a0609c7ea72bf0400
Zachary Baird <zdb1994@yahoo.com>
2023-04-12 13:09:24 -0400

    Wires up auto-detect modal. Wires up Java version browse. Styling updates.

--- theseus_gui/src/pages/Settings.vue
+++ theseus_gui/src/pages/Settings.vue
@@ -1,21 +1,32 @@
 <script setup>
 import { ref, watch } from 'vue'
-import { Card, Slider, DropdownSelect, Button, SearchIcon, PlayIcon } from 'omorphia'
+import { Card, Slider, DropdownSelect, Button, SearchIcon, PlayIcon, Modal } from 'omorphia'
 import { BrowseIcon } from '@/assets/icons'
 import { useTheming } from '@/store/state'
 import { get, set, deepEqual } from '@/helpers/settings'
+import { find_jre_8_jres, find_jre_17_jres } from '@/helpers/jre'
+import { open } from '@tauri-apps/api/dialog'
+
+const themeStore = useTheming()
 
 // Original object to track changes
 const originalSettings = ref(await get())
 // Object to bind
 const settings = ref(await get())
+// Finding possible Java 8 and 17 installations
+const java8InstallOptions = ref(await find_jre_8_jres())
+const java17InstallOptions = ref(await find_jre_17_jres())
 
-const saveButton = ref(null)
-
+// Setting java version defaults
 if (!settings.value.java_globals.JAVA_8)
   settings.value.java_globals.JAVA_8 = { path: '', version: '' }
 if (!settings.value.java_globals.JAVA_17)
   settings.value.java_globals.JAVA_17 = { path: '', version: '' }
+
+// DOM refs
+const detectJava17Modal = ref(null)
+const detectJava8Modal = ref(null)
+const saveButton = ref(null)
 
 watch(settings.value, (newSettings) => {
   // Validate the changed state
@@ -34,19 +45,74 @@ watch(settings.value, (newSettings) => {
     settings.value.java_globals.JAVA_17 = { path: '', version: '' }
 })
 
-const themeStore = useTheming()
-
 const handleTheme = (e) => themeStore.setThemeState(e.option.toLowerCase())
 const saveJavaPath = () => {}
 const saveSettings = async () => {
   await set(settings.value)
   saveButton.value.$el.style.opacity = 0
 }
+
+const handleJava17FileInput = async () => {
+  let filePath = await open()
+  settings.value.java_globals.JAVA_17 = {
+    path: filePath,
+    version: '17',
+  }
+}
+const handleJava8FileInput = async () => {
+  let filePath = await open()
+  settings.value.java_globals.JAVA_8 = {
+    path: filePath,
+    version: '8',
+  }
+}
+
+const setJava17Install = (chosenInstall) => (settings.value.java_globals.JAVA_17 = chosenInstall)
+const setJava8Install = (chosenInstall) => (settings.value.java_globals.JAVA_8 = chosenInstall)
 </script>
 
 <template>
   <div>
     <Button ref="saveButton" color="primary" class="save-btn" @click="saveSettings">Save</Button>
+    <Modal ref="detectJava17Modal" header="Auto Detect Java Version" class="auto-detect-modal">
+      <div class="version-table">
+        <div class="header">
+          <span>Version</span>
+          <span>Path</span>
+        </div>
+        <div
+          v-for="java17Install in java17InstallOptions"
+          :key="java17Install.path"
+          class="row"
+          :class="[settings.java_globals.JAVA_17.path === java17Install.path ? 'active' : '']"
+          @click="() => setJava17Install(java17Install)"
+        >
+          <span>{{ java17Install.version }}</span>
+          <span>{{ java17Install.path }}</span>
+        </div>
+      </div>
+
+      <Button color="primary" @click="() => detectJava17Modal.hide()">Select</Button>
+    </Modal>
+    <Modal ref="detectJava8Modal" header="Auto Detect Java Version" class="auto-detect-modal">
+      <div class="version-table">
+        <div class="header">
+          <span>Version</span>
+          <span>Path</span>
+        </div>
+        <div
+          v-for="java8Install in java8InstallOptions"
+          :key="java8Install.path"
+          class="row"
+          :class="[settings.java_globals.JAVA_8.path === java8Install.path ? 'active' : '']"
+          @click="() => setJava8Install(java8Install)"
+        >
+          <span>{{ java8Install.version }}</span>
+          <span>{{ java8Install.path }}</span>
+        </div>
+      </div>
+      <Button color="primary" @click="() => detectJava8Modal.hide()">Select</Button>
+    </Modal>
     <Card class="theming">
       <h2>Themes</h2>
       <div class="setting-row">
@@ -76,11 +142,11 @@ const saveSettings = async () => {
             placeholder="/path/to/java17"
           />
           <span class="installation-buttons">
-            <Button @click="saveJavaPath">
+            <Button @click="() => detectJava17Modal.show()">
               <SearchIcon />
               Auto Detect
             </Button>
-            <Button @click="saveJavaPath">
+            <Button @click="handleJava17FileInput">
               <BrowseIcon />
               Browse
             </Button>
@@ -101,11 +167,11 @@ const saveSettings = async () => {
             placeholder="/path/to/java8"
           />
           <span class="installation-buttons">
-            <Button @click="saveJavaPath">
+            <Button @click="() => detectJava8Modal.show()">
               <SearchIcon />
               Auto Detect
             </Button>
-            <Button @click="saveJavaPath">
+            <Button @click="handleJava8FileInput">
               <BrowseIcon />
               Browse
             </Button>
@@ -132,7 +198,7 @@ const saveSettings = async () => {
           v-model="settings.custom_env_args"
           type="text"
           class="input installation-input"
-          placeholder="-Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled"
+          placeholder="ENV1=TEST ENV2=TEST2"
         />
       </div>
       <hr class="card-divider" />
@@ -171,7 +237,7 @@ const saveSettings = async () => {
             Maximum Concurrent Downloads
             <input
               v-model="settings.max_concurrent_downloads"
-              type="text"
+              type="number"
               name="concurrent-downloads"
               class="concurrent_downloads"
             />
@@ -179,7 +245,7 @@ const saveSettings = async () => {
         </div>
       </div>
     </Card>
-    <Card class="settings-card">
+    <Card class="settings-card" style="margin-bottom: 3.5rem">
       <h2 class="settings-title">Commands</h2>
       <div class="settings-group">
         <div class="toggle-setting">
@@ -200,6 +266,70 @@ const saveSettings = async () => {
 </template>
 
 <style lang="scss">
+.active {
+  background: var(--color-brand-highlight) !important;
+}
+
+.auto-detect-modal {
+  .modal-body {
+    .content {
+      padding: 1.5rem;
+
+      .version-table {
+        border: 1px solid var(--color-contrast);
+        width: 100%;
+        overflow: hidden;
+        padding: 0.3rem;
+
+        .header {
+          display: flex;
+          gap: 2rem;
+          width: 100%;
+          margin-bottom: 0.3rem;
+
+          span:first-child {
+            width: 5rem !important;
+          }
+
+          span {
+            white-space: nowrap;
+            color: var(--color-contrast);
+          }
+
+          span:first-child {
+            border-right: 1px solid var(--color-contrast);
+          }
+        }
+
+        .row {
+          display: flex;
+          gap: 2rem;
+          width: 100%;
+          max-height: 100%;
+          white-space: nowrap;
+          margin-bottom: 0.2rem;
+          cursor: pointer;
+          transition: 0.1s ease-in-out all;
+
+          &:hover {
+            background: var(--color-brand-highlight);
+          }
+
+          span:first-child {
+            width: 5rem !important;
+          }
+
+          span:nth-child(2) {
+            display: inline-block;
+            width: 25rem;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+        }
+      }
+    }
+  }
+}
 .save-btn {
   position: absolute !important;
   z-index: 100;
