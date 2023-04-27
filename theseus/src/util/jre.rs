57a61b9ed70fe8684f92ac47b2e0f5eb4cd3b1bf
Wyatt <wyatt@modrinth.com>
2023-04-26 20:18:23 -0700

    linux jre missing folders

--- theseus/src/util/jre.rs
+++ theseus/src/util/jre.rs
@@ -168,6 +168,7 @@ pub async fn get_all_jre() -> Result<Vec<JavaVersion>, JREError> {
             }
         }
     }
+
     // Get JRE versions from potential paths concurrently
     let j = check_java_at_filepaths(jre_paths)
         .await?
@@ -202,7 +203,7 @@ pub async fn check_java_at_filepaths(
         .map(|p: PathBuf| {
             tokio::task::spawn(async move { check_java_at_filepath(&p).await })
         })
-        .buffer_unordered(500)
+        .buffer_unordered(64)
         .collect::<Vec<_>>()
         .await;
 
