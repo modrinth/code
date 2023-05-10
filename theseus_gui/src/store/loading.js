22a8c1a179fb09aee7591eb88cd56f96040d7c93
Jai A <jaiagr+gpg@pm.me>
2023-05-09 18:49:42 -0700

    remove print

--- theseus_gui/src/store/loading.js
+++ theseus_gui/src/store/loading.js
@@ -4,11 +4,9 @@ export const useLoading = defineStore('loadingStore', {
   state: () => ({ loading: false }),
   actions: {
     startLoading() {
-      console.log('I was told to start loading')
       this.loading = true
     },
     stopLoading() {
-      console.log('I was told to stop loading')
       this.loading = false
     },
   },
