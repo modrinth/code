5f28c238c1cac424dcd42df6f536a6e258a8da9e
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-05-12 20:51:05 -0400

    Run linter

--- theseus_gui/src/pages/instance/Logs.vue
+++ theseus_gui/src/pages/instance/Logs.vue
@@ -4,27 +4,31 @@
       <DropdownSelect
         v-model="selectedLog"
         :options="logs"
-        :display-name="option => option?.name"
+        :display-name="(option) => option?.name"
         :disabled="logs.length === 0"
       />
       <div class="button-group">
         <Button :disabled="!selectedLog" @click="copyLog()">
-          <ClipboardCopyIcon v-if="!copied"/>
+          <ClipboardCopyIcon v-if="!copied" />
           <CheckIcon v-else />
-          {{ copied ? 'Copied' : 'Copy'}}
+          {{ copied ? 'Copied' : 'Copy' }}
         </Button>
         <Button disabled color="primary" @click="shareLog()">
           <SendIcon />
           Share
         </Button>
-        <Button :disabled="!selectedLog || selectedLog.live === true" color="danger" @click="deleteLog()">
+        <Button
+          :disabled="!selectedLog || selectedLog.live === true"
+          color="danger"
+          @click="deleteLog()"
+        >
           <TrashIcon />
           Delete
         </Button>
       </div>
     </div>
     <div class="log-text">
-      {{logs[1]}}
+      {{ logs[1] }}
       <div v-for="line in selectedLog?.stdout.split('\n')" :key="line" class="no-wrap">
         {{ line }}
       </div>
@@ -40,13 +44,13 @@ import {
   ClipboardCopyIcon,
   DropdownSelect,
   SendIcon,
-  TrashIcon
+  TrashIcon,
 } from 'omorphia'
-import {delete_logs_by_datetime, get_logs} from "@/helpers/logs.js";
-import {onMounted, ref, watch} from "vue";
-import dayjs from "dayjs";
-import {get_stdout_by_uuid, get_uuids_by_profile_path} from "@/helpers/process.js";
-import {useRoute} from "vue-router";
+import { delete_logs_by_datetime, get_logs } from '@/helpers/logs.js'
+import { onMounted, ref, watch } from 'vue'
+import dayjs from 'dayjs'
+import { get_stdout_by_uuid, get_uuids_by_profile_path } from '@/helpers/process.js'
+import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
@@ -58,10 +62,10 @@ const props = defineProps({
 })
 
 const getLiveLog = async () => {
-  const uuids = await get_uuids_by_profile_path(route.params.id);
+  const uuids = await get_uuids_by_profile_path(route.params.id)
   let returnValue
   if (uuids.length === 0) {
-    returnValue = "No live game detected. \nStart your game to proceed"
+    returnValue = 'No live game detected. \nStart your game to proceed'
   } else {
     returnValue = await get_stdout_by_uuid(uuids[0])
   }
@@ -69,7 +73,7 @@ const getLiveLog = async () => {
 }
 
 const getLogs = async () => {
-  return (await get_logs(props.instance.uuid)).reverse().map(log => {
+  return (await get_logs(props.instance.uuid)).reverse().map((log) => {
     log.name = dayjs(log.datetime_string.slice(0, 8) + 'T' + log.datetime_string.slice(9))
     return log
   })
@@ -79,10 +83,7 @@ const logs = ref([])
 
 try {
   console.log('initializing logs', logs.value)
-  logs.value = [
-    await getLiveLog(),
-    ... await getLogs()
-  ]
+  logs.value = [await getLiveLog(), ...(await getLogs())]
 
   console.log('finalizing logs', logs.value)
 } catch (e) {
@@ -93,7 +94,7 @@ const selectedLog = ref(logs.value[0])
 const copied = ref(false)
 
 const shareLog = () => {
-  console.log("share")
+  console.log('share')
 }
 
 const copyLog = () => {
@@ -106,10 +107,7 @@ const copyLog = () => {
 const deleteLog = async () => {
   if (selectedLog.value && selectedLog.value !== logs.value[0]) {
     await delete_logs_by_datetime(props.instance.uuid, selectedLog.value.datetime_string)
-    logs.value = [
-      await getLiveLog(),
-      ... await getLogs()
-    ]
+    logs.value = [await getLiveLog(), ...(await getLogs())]
   }
 }
 
