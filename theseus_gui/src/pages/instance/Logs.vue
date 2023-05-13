53b84806494d0b606d9ac6ec0bce27afef3e222d
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-05-12 20:49:41 -0400

    Finish up wireup

--- theseus_gui/src/pages/instance/Logs.vue
+++ theseus_gui/src/pages/instance/Logs.vue
@@ -17,13 +17,14 @@
           <SendIcon />
           Share
         </Button>
-        <Button :disabled="!selectedLog || selectedLog === logs[0]" color="danger" @click="deleteLog()">
+        <Button :disabled="!selectedLog || selectedLog.live === true" color="danger" @click="deleteLog()">
           <TrashIcon />
           Delete
         </Button>
       </div>
     </div>
     <div class="log-text">
+      {{logs[1]}}
       <div v-for="line in selectedLog?.stdout.split('\n')" :key="line" class="no-wrap">
         {{ line }}
       </div>
@@ -32,13 +33,19 @@
 </template>
 
 <script setup>
-import { Card, Button, TrashIcon, SendIcon, ClipboardCopyIcon, DropdownSelect, CheckIcon } from 'omorphia'
+import {
+  Button,
+  Card,
+  CheckIcon,
+  ClipboardCopyIcon,
+  DropdownSelect,
+  SendIcon,
+  TrashIcon
+} from 'omorphia'
 import {delete_logs_by_datetime, get_logs} from "@/helpers/logs.js";
 import {onMounted, ref, watch} from "vue";
 import dayjs from "dayjs";
-import {
-  get_stdout_by_uuid, get_uuids_by_profile_path
-} from "@/helpers/process.js";
+import {get_stdout_by_uuid, get_uuids_by_profile_path} from "@/helpers/process.js";
 import {useRoute} from "vue-router";
 
 const route = useRoute()
@@ -52,27 +59,37 @@ const props = defineProps({
 
 const getLiveLog = async () => {
   const uuids = await get_uuids_by_profile_path(route.params.id);
-  if (uuids.length === 0) return ""
-  return await get_stdout_by_uuid(uuids[0])
+  let returnValue
+  if (uuids.length === 0) {
+    returnValue = "No live game detected. \nStart your game to proceed"
+  } else {
+    returnValue = await get_stdout_by_uuid(uuids[0])
+  }
+  return { name: 'Live Log', stdout: returnValue, live: true }
+}
+
+const getLogs = async () => {
+  return (await get_logs(props.instance.uuid)).reverse().map(log => {
+    log.name = dayjs(log.datetime_string.slice(0, 8) + 'T' + log.datetime_string.slice(9))
+    return log
+  })
 }
 
 const logs = ref([])
 
 try {
-  logs.value = ref([
-    {
-      name: 'Live Log',
-      stdout: await getLiveLog(),
-    },
-    ... await get_logs(props.instance.uuid).then(log => log.reverse()).then(log => log.map(log => {
-    log.name = dayjs(log.datetime_string.slice(0, 8) + 'T' + log.datetime_string.slice(9))
-    return log
-  }))])
+  console.log('initializing logs', logs.value)
+  logs.value = [
+    await getLiveLog(),
+    ... await getLogs()
+  ]
+
+  console.log('finalizing logs', logs.value)
 } catch (e) {
-  console.error(e)
+  logs.value = [await getLiveLog()]
 }
 
-const selectedLog = ref(null)
+const selectedLog = ref(logs.value[0])
 const copied = ref(false)
 
 const shareLog = () => {
@@ -90,11 +107,9 @@ const deleteLog = async () => {
   if (selectedLog.value && selectedLog.value !== logs.value[0]) {
     await delete_logs_by_datetime(props.instance.uuid, selectedLog.value.datetime_string)
     logs.value = [
-      getLiveLog(),
-      ...await get_logs(props.instance.uuid).then(log => log.reverse()).then(log => log.map(log => {
-      log.name = dayjs(log.datetime_string.slice(0, 8) + 'T' + log.datetime_string.slice(9))
-      return log
-    }))]
+      await getLiveLog(),
+      ... await getLogs()
+    ]
   }
 }
 
@@ -105,7 +120,7 @@ watch(selectedLog, () => {
 onMounted(() => {
   setInterval(async () => {
     if (logs.value.length > 0) {
-      logs.value[0].stdout = await getLiveLog()
+      logs.value[0] = await getLiveLog()
     }
   }, 1000)
 })
