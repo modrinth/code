91607adc64a5a764ec8fcf4e9823073fb14894e0
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-05-25 14:32:48 -0400

    Add grouping

--- theseus_gui/src/components/GridDisplay.vue
+++ theseus_gui/src/components/GridDisplay.vue
@@ -1,7 +1,12 @@
 <script setup>
 import Instance from '@/components/ui/Instance.vue'
 import {computed, ref} from 'vue'
-import {SearchIcon, DropdownSelect, Card} from "omorphia";
+import {
+  SearchIcon,
+  DropdownSelect,
+  Card,
+  formatCategoryHeader,
+} from "omorphia";
 
 const props = defineProps({
   instances: {
@@ -23,9 +28,8 @@ const props = defineProps({
   canPaginate: Boolean,
 })
 
-const modsRow = ref(null)
-
 const search = ref('')
+const group = ref('None')
 const filters = ref('All profiles')
 const sortBy = ref('Name')
 
@@ -64,66 +68,113 @@ const filteredResults = computed(() => {
     })
   }
 
-  if (!filters.value || filters.value === 'All profiles') {
-    return instances
-  } else if (filters.value === 'Custom instances') {
-    return instances.filter((instance) => {
+  if (filters.value === 'Custom instances') {
+    instances = instances.filter((instance) => {
       return !instance.metadata?.linked_data
     })
   } else if (filters.value === 'Downloaded modpacks') {
-    return instances.filter((instance) => {
+    instances = instances.filter((instance) => {
       return instance.metadata?.linked_data
     })
   }
 
-  return instances
+  const instanceMap = new Map()
+
+  if (group.value === 'Loader') {
+    instances.forEach((instance) => {
+      const loader = formatCategoryHeader(instance.metadata.loader)
+      if (!instanceMap.has(loader)) {
+        instanceMap.set(loader, [])
+      }
+
+      instanceMap.get(loader).push(instance)
+    })
+  } else if (group.value === 'Game version') {
+    instances.forEach((instance) => {
+      if (!instanceMap.has(instance.metadata.game_version)) {
+        instanceMap.set(instance.metadata.game_version, [])
+      }
+
+      instanceMap.get(instance.metadata.game_version).push(instance)
+    })
+  } else if (group.value === 'Category') {
+    instances.forEach((instance) => {
+      if (!instanceMap.has(instance.metadata.category)) {
+        instanceMap.set(instance.metadata.category, [])
+      }
+
+      instanceMap.get(instance.metadata.category).push(instance)
+    })
+  } else {
+    return instanceMap.set('None', instances)
+  }
+
+  return instanceMap
 })
 
 </script>
 <template>
-  <div class="row">
-    <Card class="header">
-      <div class="iconified-input">
-        <SearchIcon/>
-        <input
-          v-model="search"
-          type="text"
-          placeholder="Search"
-          class="search-input"
-        />
-      </div>
-      <div class="labeled_button">
-        <span>Sort by</span>
-        <DropdownSelect
-          v-model="sortBy"
-          class="sort-dropdown"
-          :options="[
-            'Name',
-            'Last played',
-            'Date created',
-            'Date modified',
-            'Game version',
-          ]"
-          placeholder="Select..."
-        />
-      </div>
-      <div class="labeled_button">
-        <span>Filter by</span>
-        <DropdownSelect
-          v-model="filters"
-          class="filter-dropdown"
-          :options="[
-            'All profiles',
-            'Custom instances',
-            'Downloaded modpacks'
-          ]"
-          placeholder="Select..."
-        />
-      </div>
-    </Card>
-    <section ref="modsRow" class="instances">
+  <Card class="header">
+    <div class="iconified-input">
+      <SearchIcon/>
+      <input
+        v-model="search"
+        type="text"
+        placeholder="Search"
+        class="search-input"
+      />
+    </div>
+    <div class="labeled_button">
+      <span>Sort by</span>
+      <DropdownSelect
+        v-model="sortBy"
+        class="sort-dropdown"
+        :options="[
+          'Name',
+          'Last played',
+          'Date created',
+          'Date modified',
+          'Game version',
+        ]"
+        placeholder="Select..."
+      />
+    </div>
+    <div class="labeled_button">
+      <span>Filter by</span>
+      <DropdownSelect
+        v-model="filters"
+        class="filter-dropdown"
+        :options="[
+          'All profiles',
+          'Custom instances',
+          'Downloaded modpacks'
+        ]"
+        placeholder="Select..."
+      />
+    </div>
+    <div class="labeled_button">
+      <span>Group by</span>
+      <DropdownSelect
+        v-model="group"
+        class="group-dropdown"
+        :options="[
+          'None',
+          'Loader',
+          'Game version',
+          'Category'
+        ]"
+        placeholder="Select..."
+      />
+    </div>
+  </Card>
+  <div v-for="instanceSection in Array.from(filteredResults, ([key, value]) => ({ key, value }))" :key="instanceSection.key" class="row">
+    <div v-if="instanceSection.key !== 'None'" class="divider">
+      <p>{{ instanceSection.key }}</p>
+      <hr aria-hidden="true" />
+    </div>
+    <section class="instances">
       <Instance
-        v-for="instance in filteredResults"
+        v-for="instance in instanceSection.value"
         :key="instance.id"
         display="card"
         :instance="instance"
@@ -135,56 +186,77 @@ const filteredResults = computed(() => {
 .row {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
   padding: 1rem;
 
-  .header {
+  .divider {
     display: flex;
-    gap: 0.5rem;
-    align-items: inherit;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
-    margin-bottom: 0 !important;
-    padding: 1rem;
+    gap: 1rem;
 
-    .iconified-input {
-      flex-grow: 1;
-    }
-
-    .sort-dropdown {
-      width: 10rem;
-    }
-
-    .filter-dropdown {
-      width: 15rem;
-    }
-
-    .labeled_button {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      gap: 0.5rem;
+    p {
+      font-size: 1rem;
       white-space: nowrap;
+      color: var(--color-contrast);
+    }
+
+    hr {
+      background-color: var(--color-gray);
+      height: 1px;
+      width: 100%;
+      border: none;
     }
   }
+}
 
-  section {
+.header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 1rem;
+  align-items: inherit;
+  margin: 1rem 1rem 0 !important;
+  padding: 1rem;
+  width: calc(100% - 2rem);
+
+  .iconified-input {
+    flex-grow: 1;
+  }
+
+  .sort-dropdown {
+    width: 10rem;
+  }
+
+  .filter-dropdown {
+    width: 15rem;
+  }
+
+  .group-dropdown {
+    width: 10rem;
+  }
+
+  .labeled_button {
     display: flex;
-    align-items: inherit;
-    transition: all ease-in-out 0.4s;
-    gap: 1rem;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5rem;
+    white-space: nowrap;
   }
+}
 
-  .instances {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
-    width: 100%;
-    gap: 1rem;
-    margin-right: auto;
-    margin-top: 0.8rem;
-    scroll-behavior: smooth;
-    overflow-y: auto;
-  }
+.instances {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+  width: 100%;
+  gap: 1rem;
+  margin-right: auto;
+  margin-top: 0.8rem;
+  scroll-behavior: smooth;
+  overflow-y: auto;
 }
 
 .dark-mode {
