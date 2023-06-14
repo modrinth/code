73b492f3b198f67eebf29f4b3666601f44ac2bc5
thesuzerain <wverchere@gmail.com>
2023-06-13 17:16:13 -0700

    prettier/fmt

--- theseus/src/event/mod.rs
+++ theseus/src/event/mod.rs
@@ -137,7 +137,7 @@ impl Drop for LoadingBarId {
                     // Emit event to indicatif progress bar arc
                     #[cfg(feature = "cli")]
                     {
-                        let cli_progress_bar = bar.cli_progress_bar.clone();
+                        let cli_progress_bar = bar.cli_progress_bar;
                         cli_progress_bar.finish();
                     }
                 }
@@ -150,7 +150,6 @@ impl Drop for LoadingBarId {
                 loader_uuid,
             )
             .await;
-
         });
     }
 }
