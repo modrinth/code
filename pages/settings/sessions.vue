3fd74292ef2e2e34b8c1781f969dd2a35c71f8e2
Sasha Sorokin <10401817+brawaru@users.noreply.github.com>
2023-09-29 05:53:44 +0200

    Remove unnecessary space in description on Sessions settings page (#1381)

--- pages/settings/sessions.vue
+++ pages/settings/sessions.vue
@@ -81,7 +81,7 @@ const messages = defineMessages({
   sessionsDescription: {
     id: 'settings.sessions.description',
     defaultMessage:
-      "Here are all the devices that are currently logged in with your Modrinth account. You can log out of each one individually.\n\n If you see an entry you don't recognize, log out of that device and change your Modrinth account password immediately.",
+      "Here are all the devices that are currently logged in with your Modrinth account. You can log out of each one individually.\n\nIf you see an entry you don't recognize, log out of that device and change your Modrinth account password immediately.",
   },
   sessionsLastAccessedAt: {
     id: 'settings.sessions.label.last-accessed-at',
