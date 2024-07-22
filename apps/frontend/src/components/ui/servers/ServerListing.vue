968e3e8f1428a85cb2cb8af66b23f8f4370f0cef
fero <hi@evan.graphics>
2024-07-21 17:20:13 -0700

    fix: display installing status on server listings

--- apps/frontend/src/components/ui/servers/ServerListing.vue
+++ apps/frontend/src/components/ui/servers/ServerListing.vue
@@ -1,13 +1,39 @@
 <template>
   <NuxtLink
     :href="`/servers/manage/${serverId}`"
-    class="flex flex-row items-center rounded-3xl bg-bg-raised p-4"
+    class="flex flex-row items-center overflow-x-hidden rounded-3xl bg-bg-raised p-4"
     data-pyro-server-listing
   >
     <UiAvatar no-shadow size="md" :src="''" alt="Server Icon" />
     <div class="ml-8 flex flex-col gap-3">
-      <div class="flex flex-row items-center gap-2">
+      <div class="flex flex-col gap-2 md:flex-row md:items-center">
         <h2 class="m-0 text-xl font-bold">{{ name }}</h2>
+        <div
+          v-if="state === 'installing'"
+          class="flex w-fit flex-row items-center rounded-full bg-red-500 px-2 py-0.5 text-sm font-bold text-white"
+        >
+          <svg
+            class="mr-2 h-3 w-3 animate-spin text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Installing
+        </div>
         <ChevronRightIcon />
       </div>
 
