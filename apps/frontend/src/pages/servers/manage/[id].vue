c7281c55c16a67c8a9f7f4e9bf6f5f8b816a1d88
fero <hi@evan.graphics>
2024-07-21 17:56:47 -0700

    fix: frozen serverslisting page when the state of specific server fetching is pending

--- apps/frontend/src/pages/servers/manage/[id].vue
+++ apps/frontend/src/pages/servers/manage/[id].vue
@@ -103,11 +103,61 @@
     <UiServersPoweredByPyro />
   </div>
 
-  <div
-    v-else-if="!data || status === 'pending' || data.state === 'installing'"
-    class="flex h-screen w-full items-center justify-center"
-  >
+  <div v-else-if="status === 'pending'" class="flex h-screen w-full items-center justify-center">
     <PyroIcon class="pyro-logo-animation size-32 opacity-10" />
+  </div>
+
+  <div
+    v-else-if="data && data.state === 'installing'"
+    class="relative -mt-12 flex h-screen min-h-[400px] w-full flex-1 items-center justify-center"
+  >
+    <div
+      class="bg-loading-animation absolute inset-0 -mt-8"
+      style="
+        background: linear-gradient(0deg, rgba(22, 24, 28, 0.64), rgba(22, 24, 28, 0.64)),
+          linear-gradient(180deg, rgba(66, 131, 92, 0.275) 0%, rgba(17, 35, 43, 0.5) 97.29%);
+      "
+    ></div>
+    <div
+      class="bg-loading-animation pointer-events-none absolute inset-0 mx-auto flex h-full w-full max-w-7xl select-none items-center justify-center"
+    >
+      <img
+        src="~/assets/images/games/bg-mock.png"
+        alt="Background"
+        class="absolute inset-0 mt-12 h-full w-full object-fill"
+      />
+    </div>
+    <div class="pyro-logo-animation relative flex flex-col items-center gap-4 p-8">
+      <svg
+        class="h-5 w-5 animate-spin text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <h1
+        class="m-0 inline-flex items-center gap-2 text-4xl font-bold text-[var(--color-contrast)]"
+      >
+        Your server is installing...
+      </h1>
+      <p class="max-w-sm text-center leading-relaxed text-secondary">
+        Sit tight, your server is being set up. This should only take a minute.
+      </p>
+      <Button color="primary" @click="refreshNuxtData('specificServer')"> Refresh </Button>
+    </div>
   </div>
 
   <div
@@ -189,7 +239,7 @@ import type { Server } from "~/types/servers";
 
 const auth = await useAuth();
 
-const { data, status } = await useLazyAsyncData("serversList", async () => {
+const { data, status } = await useLazyAsyncData("specificServer", async () => {
   return await usePyroFetch<Server>(auth.value.token, `servers/${serverId}`);
 });
 </script>
