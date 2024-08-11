a57a799e7f325ca5117d738b52cd85087a97cb13
fero <hi@evan.graphics>
2024-08-11 00:04:30 -0700

    refactor: server listing again

--- apps/frontend/src/components/ui/servers/ServerListing.vue
+++ apps/frontend/src/components/ui/servers/ServerListing.vue
@@ -1,47 +1,52 @@
 <template>
   <NuxtLink
     :to="`/servers/manage/${server_id}`"
-    :aria-disabled="state === 'Installing'"
-    :tabindex="state === 'Installing' ? -1 : 0"
-    :class="state === 'Installing' ? 'pointer-events-none cursor-not-allowed' : ''"
+    :aria-disabled="status.isInstalling"
+    :tabindex="status.isInstalling ? -1 : 0"
+    :class="status.isInstalling ? 'pointer-events-none cursor-not-allowed' : ''"
     class="flex flex-row items-center overflow-x-hidden rounded-3xl bg-bg-raised p-4"
     data-pyro-server-listing
     :data-pyro-server-listing-id="server_id"
   >
-    <UiAvatar no-shadow size="md" :src="icon_url" alt="Server Icon" />
+    <UiAvatar no-shadow size="md" alt="Server Icon" />
     <div class="ml-8 flex flex-col gap-3">
       <div class="flex flex-col gap-2 md:flex-row md:items-center">
         <h2 class="m-0 text-xl font-bold">{{ name }}</h2>
-        <UiServersServerInstallStatusPill v-if="state" :state="state as StatusState" />
-        <ChevronRightIcon v-if="state !== 'Installing'" />
+        <UiServersServerInstallStatusPill v-if="status.state" :state="status.state" />
+        <ChevronRightIcon v-if="!status.isInstalling" />
       </div>
 
       <div class="flex flex-row items-center gap-4 text-[var(--color-text-secondary)]">
-        <UiServersServerGameLabel v-if="game" :game="game" :mcVersion="mc_version ?? ''" />
+        <UiServersServerGameLabel
+          v-if="showGameLabel"
+          :game="game!"
+          :mcVersion="mc_version ?? ''"
+        />
         <UiServersServerLoaderLabel
-          v-if="loader"
-          :loader="loader"
+          v-if="showLoaderLabel"
+          :loader="loader!"
           :loaderVersion="loader_version ?? ''"
         />
-        <UiServersServerModLabel v-if="mods?.length ?? 0 > 0" :mods="mods" />
+        <UiServersServerModLabel v-if="showModLabel" :mods="mods" />
       </div>
     </div>
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import type { Server } from "~/types/servers";
 import type { StatusState } from "./ServerInstallStatusPill.vue";
-
 import { ChevronRightIcon } from "@modrinth/assets";
 
 const props = defineProps<Partial<Server>>();
 
-let icon_url: string | undefined = undefined;
+const status = computed(() => ({
+  state: props.state as StatusState | undefined,
+  isInstalling: props.state === "Installing",
+}));
 
-if (props.modpack) {
-  const pid: any = await toRaw(useBaseFetch(`version/${await props.modpack}`));
-  const project: any = await toRaw(useBaseFetch(`project/${pid.project_id}`));
-  icon_url = project.icon_url;
-}
+const showGameLabel = computed(() => !!props.game);
+const showLoaderLabel = computed(() => !!props.loader);
+const showModLabel = computed(() => (props.mods?.length ?? 0) > 0);
 </script>
