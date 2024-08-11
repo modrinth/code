9564f5e0a4e3e1457f0f5b85df3625bd5554a321
fero <hi@evan.graphics>
2024-08-10 23:59:02 -0700

    chore: use autoimport

--- apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
+++ apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
@@ -2,7 +2,7 @@
   <div>
     <div v-if="loader && loaderVersion" class="h-6 w-0.5 bg-button-border"></div>
     <div v-if="loader && loaderVersion" class="flex flex-row items-center gap-2">
-      <LoaderIcon v-if="loader" :loader="loader" />
+      <UiServersLoaderIcon v-if="loader" :loader="loader" />
       <span class="text-sm font-semibold capitalize"> {{ loader }} {{ loaderVersion }} </span>
     </div>
   </div>
@@ -10,7 +10,6 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import LoaderIcon from "./LoaderIcon.vue";
 
 export default defineComponent({
   name: "LoaderInfo",
