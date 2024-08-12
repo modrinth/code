bdb0b1db0f1c6b412fc9fd453569455f7131ba9d
Evan Song <theevansong@gmail.com>
2024-08-12 12:15:31 -0700

    refactor: bubble up conditional

--- apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
+++ apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-row items-center gap-4">
-    <div v-if="loader" class="experimental-styles-within h-6 w-0.5 bg-button-border"></div>
-    <div v-if="loader" class="flex flex-row items-center gap-2">
-      <UiServersLoaderIcon v-if="loader" :loader="loader" />
+  <div v-if="loader" class="flex flex-row items-center gap-4">
+    <div class="experimental-styles-within h-6 w-0.5 bg-button-border"></div>
+    <div class="flex flex-row items-center gap-2">
+      <UiServersLoaderIcon :loader="loader" />
       <span class="text-sm font-semibold capitalize">
         {{ loader }} <span v-if="loaderVersion">{{ loaderVersion }}</span>
       </span>
