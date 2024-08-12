6556a84a60fbe9f16e82b8e1a4315e7aee791512
Evan Song <theevansong@gmail.com>
2024-08-11 20:37:47 -0700

    feat: play btn component

---
+++ apps/frontend/src/components/ui/servers/PanelPlay.vue
@@ -0,0 +1,39 @@
+<template>
+  <a class="btn btn-primary" :href="playUrl">
+    <PlayIcon />
+    Play
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import { PlayIcon } from "@modrinth/assets";
+
+export default defineComponent({
+  name: "PlayButton",
+  components: {
+    PlayIcon,
+  },
+  props: {
+    serverId: {
+      type: String,
+      required: true,
+    },
+    ip: {
+      type: String,
+      required: true,
+    },
+    port: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const playUrl = computed(
+      () => `modrinth://servers/${props.serverId}/${props.ip}:${props.port}`,
+    );
+
+    return { playUrl };
+  },
+});
+</script>
