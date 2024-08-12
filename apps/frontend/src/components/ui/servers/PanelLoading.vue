1fc678c3bc28b995811884871203f05794065c9c
Evan Song <theevansong@gmail.com>
2024-08-12 13:53:30 -0700

    chore: the 2

--- apps/frontend/src/components/ui/servers/PanelLoading.vue
+++ apps/frontend/src/components/ui/servers/PanelLoading.vue
@@ -88,8 +88,9 @@
           >
             <path
               d="M1 12.5A4.5 4.5 0 0 0 5.5 17H15a4 4 0 0 0 1.866-7.539 3.504 3.504 0 0 0-4.504-4.272A4.5 4.5 0 0 0 4.06 8.235 4.5 4.5 0 0 0 1 12.5"
-            ></path></svg
-          ><span>Backups</span>
+            ></path>
+          </svg>
+          <span>Backups</span>
         </div>
         <div class="button-animation flex flex-row items-center gap-2 px-4 py-2 focus:rounded-full">
           <svg
@@ -102,8 +103,9 @@
               fill-rule="evenodd"
               d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a7 7 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7 7 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a7 7 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a7 7 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7 7 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54a7 7 0 0 1 1.93-1.115zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
               clip-rule="evenodd"
-            ></path></svg
-          ><span>Options</span>
+            ></path>
+          </svg>
+          <span>Options</span>
         </div>
         <div
           class="pointer-events-none absolute h-[calc(100%-0.5rem)] overflow-hidden rounded-full bg-brand p-1 transition-all"
