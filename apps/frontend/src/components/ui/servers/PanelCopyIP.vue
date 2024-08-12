64e3746cff88027d96f8320bbe3be4ba0827d2ab
Evan Song <theevansong@gmail.com>
2024-08-11 20:37:41 -0700

    feat: copy IP btn component

---
+++ apps/frontend/src/components/ui/servers/PanelCopyIP.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="btn btn-transparent" @click="copyText">
+    <CopyIcon />
+    Copy IP
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { CopyIcon } from "@modrinth/assets";
+
+export default defineComponent({
+  name: "CopyIPButton",
+  components: {
+    CopyIcon,
+  },
+  props: {
+    ip: {
+      type: String,
+      required: true,
+    },
+    port: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const copyText = () => {
+      const text = `${props.ip}:${props.port}`;
+      navigator.clipboard.writeText(text);
+    };
+
+    return { copyText };
+  },
+});
+</script>
