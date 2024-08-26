948af2b1c7856e7ce0cff33ba44f72b0c3d3c5ed
Prospector <prospectordev@gmail.com>
2024-08-26 16:47:29 -0700

    Remove unnecessary observer options

--- apps/frontend/src/components/ui/AutomaticAccordion.vue
+++ apps/frontend/src/components/ui/AutomaticAccordion.vue
@@ -31,10 +31,7 @@ onMounted(() => {
   mutationObserver.value = new MutationObserver(updateContent);
 
   mutationObserver.value.observe(slotContainer.value, {
-    attributes: true,
     childList: true,
-    characterData: true,
-    subtree: true,
   });
 
   updateContent();
