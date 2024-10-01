00a8717980d1d1e91220ecd27416ad661fe0c315
Evan Song <theevansong@gmail.com>
2024-09-30 20:08:02 -0700

    chore(frontend): snazzify server status pill

--- apps/frontend/src/components/ui/servers/PanelServerStatus.vue
+++ apps/frontend/src/components/ui/servers/PanelServerStatus.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="relative inline-flex items-center"
+    :aria-label="`Server is ${getStatusText}`"
+    class="relative inline-flex select-none items-center"
     @mouseenter="isExpanded = true"
     @mouseleave="isExpanded = false"
   >
@@ -8,16 +9,19 @@
       :class="`h-4 w-4 rounded-full transition-all duration-300 ease-in-out ${getStatusClass.main}`"
     >
       <div
-        :class="`absolute inline-flex h-full w-full animate-ping rounded-full ${getStatusClass.bg}`"
+        :class="`absolute inline-flex h-4 w-4 animate-ping rounded-full ${getStatusClass.bg}`"
       ></div>
     </div>
     <div
-      :class="`absolute -left-2 flex w-auto items-center gap-2 rounded-full px-2 py-1 transition-all duration-300 ease-in-out ${getStatusClass.bg} ${
-        isExpanded ? 'scale-x-100 opacity-100' : 'scale-x-0 opacity-0'
+      :class="`absolute -left-4 flex w-auto items-center gap-2 rounded-full px-2 py-1 transition-all duration-150 ease-in-out ${getStatusClass.bg} ${
+        isExpanded ? 'translate-x-2 scale-100 opacity-100' : 'translate-x-0 scale-90 opacity-0'
       }`"
     >
       <div class="h-3 w-3 rounded-full"></div>
-      <span class="whitespace-nowrap text-sm font-semibold text-contrast">
+      <span
+        class="origin-left whitespace-nowrap text-sm font-semibold text-contrast transition-all duration-[200ms] ease-in-out"
+        :class="`${isExpanded ? 'translate-x-0 scale-100' : '-translate-x-1 scale-x-75'}`"
+      >
         {{ getStatusText }}
       </span>
     </div>
