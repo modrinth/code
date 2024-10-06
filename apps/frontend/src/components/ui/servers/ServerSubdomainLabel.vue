b46f3efe2354d6b122d454be8644e1912c6fc8f7
Elizabeth <checksum@pyro.host>
2024-10-06 18:32:22 -0500

    feat: show copyable subdomain in server information

---
+++ apps/frontend/src/components/ui/servers/ServerSubdomainLabel.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="subdomain" class="flex flex-row items-center gap-4">
+        <div class="experimental-styles-within h-6 w-0.5 bg-button-border"></div>
+        <div class="flex flex-row items-center gap-2">
+            <LinkIcon />
+            <span class="text-sm font-semibold capitalize">
+                <UiCopyCode :text="subdomain" />
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    subdomain: string;
+}>();
+</script>
