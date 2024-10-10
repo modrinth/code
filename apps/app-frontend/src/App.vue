01d7580688f42f745356d0051a1d405528b13eb7
Evan Song <theevansong@gmail.com>
2024-10-10 13:56:03 -0700

    Remove Pyro integration from app
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/app-frontend/src/App.vue
+++ apps/app-frontend/src/App.vue
@@ -283,25 +283,6 @@ async function checkUpdates() {
           <RouterLink v-tooltip="'Library'" to="/library" class="btn icon-only collapsed-button">
             <LibraryIcon />
           </RouterLink>
-          <RouterLink v-tooltip="'Servers'" to="/servers" class="btn icon-only collapsed-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-server"
-            >
-              <rect width="20" height="8" x="2" y="2" rx="2" ry="2" />
-              <rect width="20" height="8" x="2" y="14" rx="2" ry="2" />
-              <line x1="6" x2="6.01" y1="6" y2="6" />
-              <line x1="6" x2="6.01" y1="18" y2="18" />
-            </svg>
-          </RouterLink>
           <Suspense>
             <InstanceCreationModal ref="installationModal" />
           </Suspense>
--- apps/app-frontend/src/pages/servers/manage/Backups.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>backups</div>
-</template>
