18426fc38f57528da04905ee027082ad2d3f2b1b
Evan Song <theevansong@gmail.com>
2024-10-15 15:08:26 -0700

    chore: fix startup page padding
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/[id]/options/startup.vue
+++ apps/frontend/src/pages/servers/manage/[id]/options/startup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative h-full w-full">
-    <div v-if="data" class="flex h-full w-full flex-col gap-4 px-4">
+    <div v-if="data" class="flex h-full w-full flex-col gap-4">
       <div class="rounded-xl border-solid border-rose-500 bg-rose-500/20 p-4">
         These settings are meant for advanced users. Changing these settings may break your server.
       </div>
