5e710401a3a6ffaa10fe875260ff1d8a63aa1b4c
Evan Song <theevansong@gmail.com>
2024-10-15 12:27:53 -0700

    chore: move panel terminal icons to components
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/PanelTerminal.vue
+++ apps/frontend/src/components/ui/servers/PanelTerminal.vue
@@ -75,36 +75,8 @@
       class="absolute right-4 top-4 grid size-12 place-content-center rounded-lg bg-bg-raised text-contrast transition-transform duration-200 hover:scale-110 active:scale-95"
       @click="toggleFullscreen"
     >
-      <svg
-        v-if="!isFullScreen"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M4.5 9V4.5H9M4.5 9L9 4.5M15 4.5h4.5V9M15 4.5l4.5 4.5M4.5 15v4.5H9M4.5 15L9 19.5M15 19.5h4.5V15M15 19.5l4.5-4.5"
-        />
-      </svg>
-      <svg
-        v-else
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M9 9V4.5M9 9H4.5M9 9 3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5 5.25 5.25"
-        />
-      </svg>
+      <UiServersPanelTerminalFullscreen v-if="isFullScreen" />
+      <UiServersPanelTerminalMinimize v-else />
       <span class="sr-only">Toggle browser full screen</span>
     </button>
   </div>
