7babe8e1c6276e1d7b093bea0f0f406d1057a766
TheWander02 <48934424+thewander02@users.noreply.github.com>
2024-10-16 03:06:20 -0700

    fix(frontend): open pyro in new tab

--- apps/frontend/src/components/ui/servers/PoweredByPyro.vue
+++ apps/frontend/src/components/ui/servers/PoweredByPyro.vue
@@ -1,5 +1,9 @@
 <template>
-  <a href="https://pyro.host" class="mx-auto mt-8 flex flex-row items-center gap-2 hover:underline">
+  <a
+    href="https://pyro.host"
+    target="_blank"
+    class="mx-auto mt-8 flex flex-row items-center gap-2 hover:underline"
+  >
     <PyroIcon class="size-4 text-[var(--color-text-secondary)]" />
     <span class="text-sm text-[var(--color-text-secondary)]">Powered by Pyro</span>
   </a>
