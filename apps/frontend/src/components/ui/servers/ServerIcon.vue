fcd4eed6e406d85b8815539bcd32427bd2c7d3f2
Evan Song <theevansong@gmail.com>
2024-10-16 16:48:05 -0700

    chore: clean
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/ServerIcon.vue
+++ apps/frontend/src/components/ui/servers/ServerIcon.vue
@@ -1,9 +1,8 @@
 <template>
   <client-only>
-    <img v-if="image" no-shadow alt="Server Icon" :class="computedClass" :src="image" />
+    <img v-if="image" alt="Server Icon" :class="computedClass" :src="image" />
     <img
       v-else
-      no-shadow
       alt="Server Icon"
       :class="computedClass"
       src="~/assets/images/servers/minecraft_server_icon.png"
