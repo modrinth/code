87a19b30387aa5d4a1a61a0339fe740eccf68a6e
Evan Song <theevansong@gmail.com>
2024-10-17 21:42:22 -0700

    chore: disallow underscores in custom url
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/[id]/options/network.vue
+++ apps/frontend/src/pages/servers/manage/[id]/options/network.vue
@@ -68,7 +68,7 @@
           </div>
           <span v-if="!isValidSubdomain" class="text-sm text-rose-400">
             Subdomain must be at least 5 characters long and can only contain alphanumeric
-            characters, dashes, and underscores.
+            characters and dashes.
           </span>
         </div>
 
@@ -252,7 +252,7 @@ const newAllocationPort = ref(0);
 
 const hasUnsavedChanges = computed(() => serverSubdomain.value !== data?.value?.net?.domain);
 
-const isValidSubdomain = computed(() => /^[a-zA-Z0-9-_]{5,}$/.test(serverSubdomain.value));
+const isValidSubdomain = computed(() => /^[a-zA-Z0-9-]{5,}$/.test(serverSubdomain.value));
 
 const addNewAllocation = async () => {
   if (!newAllocationName.value) return;
