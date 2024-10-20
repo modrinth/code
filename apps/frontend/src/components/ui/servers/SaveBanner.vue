43479964e09577119c85edf2dc7466a5a08046a1
Evan Song <theevansong@gmail.com>
2024-10-20 13:55:23 -0700

    fix: i concede
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/SaveBanner.vue
+++ apps/frontend/src/components/ui/servers/SaveBanner.vue
@@ -49,8 +49,8 @@ const saveAndRestart = async () => {
 <style scoped>
 .save-banner-enter-active {
   transition:
-    opacity 800ms,
-    transform 800ms;
+    opacity 400ms,
+    transform 400ms;
 }
 
 .save-banner-leave-active {
@@ -69,38 +69,5 @@ const saveAndRestart = async () => {
 .save-banner-leave-from {
   opacity: 1;
   transform: none;
-}
-
-.save-banner-enter-to {
-  transition-timing-function: linear(
-    0 0%,
-    0.01 0.8%,
-    0.04 1.6%,
-    0.161 3.3%,
-    0.816 9.4%,
-    1.046 11.9%,
-    1.189 14.4%,
-    1.231 15.7%,
-    1.254 17%,
-    1.259 17.8%,
-    1.257 18.6%,
-    1.236 20.45%,
-    1.194 22.3%,
-    1.057 27%,
-    0.999 29.4%,
-    0.955 32.1%,
-    0.942 33.5%,
-    0.935 34.9%,
-    0.933 36.65%,
-    0.939 38.4%,
-    1 47.3%,
-    1.011 49.95%,
-    1.017 52.6%,
-    1.016 56.4%,
-    1 65.2%,
-    0.996 70.2%,
-    1.001 87.2%,
-    1 100%
-  ) !important;
 }
 </style>
