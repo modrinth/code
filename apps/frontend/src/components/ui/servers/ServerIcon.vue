edfd3d862b9e361f661d5d2798750b39147feb0d
Evan Song <theevansong@gmail.com>
2024-10-21 14:55:46 -0700

    oops
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/ServerIcon.vue
+++ apps/frontend/src/components/ui/servers/ServerIcon.vue
@@ -9,7 +9,7 @@
     <img
       v-else
       alt="Server Icon"
-      class="size-24 rounded-xl border-[1px] border-solid border-button-border bg-button-bg shadow-sm"
+      class="experimental-styles-within size-24 rounded-xl border-[1px] border-solid border-button-border bg-button-bg shadow-sm"
       src="~/assets/images/servers/minecraft_server_icon.png"
     />
   </client-only>
