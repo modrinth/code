ee17d430ee2ffc572bfaae00fb6c2468f075ea00
Evan Song <theevansong@gmail.com>
2024-10-21 23:35:05 -0700

    chore: clean deadcode
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/index.vue
+++ apps/frontend/src/pages/servers/manage/index.vue
@@ -151,15 +151,3 @@ onUnmounted(() => {
   }
 });
 </script>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.1s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
