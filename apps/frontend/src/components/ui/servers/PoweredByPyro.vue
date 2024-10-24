5e2057d66bdd52471b47cee64713d9cc79d46dff
Evan Song <theevansong@gmail.com>
2024-10-23 18:37:08 -0700

    chore: clean
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/PoweredByPyro.vue
+++ apps/frontend/src/components/ui/servers/PoweredByPyro.vue
@@ -2,10 +2,10 @@
   <a
     href="https://pyro.host"
     target="_blank"
-    class="mx-auto mt-8 flex flex-row items-center gap-2 hover:underline"
+    class="mx-auto mt-8 flex select-none flex-row items-center gap-2 hover:underline"
   >
-    <PyroIcon class="size-4 text-[var(--color-text-secondary)]" />
-    <span class="text-sm text-[var(--color-text-secondary)]">Powered by Pyro</span>
+    <PyroIcon class="size-4 text-secondary" />
+    <span class="text-sm text-secondary">Powered by Pyro</span>
   </a>
 </template>
 
