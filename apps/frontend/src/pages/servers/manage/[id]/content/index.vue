b472608ca52d849d3db8c86ba563d2a555b66fdc
Evan Song <theevansong@gmail.com>
2024-10-25 11:30:57 -0700

    refactor(content): wrap mod rows in universalcard
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/[id]/content/index.vue
+++ apps/frontend/src/pages/servers/manage/[id]/content/index.vue
@@ -33,7 +33,7 @@
   <div v-if="data && localMods" class="flex h-full w-full flex-col">
     <div class="flex h-full w-full flex-col">
       <div class="flex items-center justify-between">
-        <div class="flex w-full flex-row items-center justify-end gap-4">
+        <div class="flex w-full max-w-lg flex-row items-center gap-4">
           <div class="relative w-full text-sm">
             <label class="sr-only" for="search">Search</label>
             <SearchIcon
@@ -43,7 +43,7 @@
             <input
               id="search"
               v-model="searchInput"
-              class="w-full border-[1px] border-solid border-button-border pl-9"
+              class="!h-9 !min-h-0 w-full border-[1px] border-solid border-button-border pl-9"
               type="search"
               name="search"
               autocomplete="off"
@@ -51,6 +51,27 @@
               @input="debouncedSearch"
             />
           </div>
+          <ButtonStyled>
+            <UiServersTeleportOverflowMenu
+              position="bottom"
+              direction="left"
+              aria-label="Filter mods"
+              :options="[
+                { id: 'all', action: () => filterMods('all') },
+                { id: 'enabled', action: () => filterMods('enabled') },
+                { id: 'disabled', action: () => filterMods('disabled') },
+              ]"
+            >
+              <span class="whitespace-pre text-sm font-medium">
+                {{ filterMethodLabel }}
+              </span>
+              <FilterIcon aria-hidden="true" />
+              <DropdownIcon aria-hidden="true" class="h-5 w-5 text-secondary" />
+              <template #all> All mods </template>
+              <template #enabled> Enabled mods </template>
+              <template #disabled> Disabled mods </template>
+            </UiServersTeleportOverflowMenu>
+          </ButtonStyled>
         </div>
       </div>
 
@@ -59,150 +80,154 @@
           <div :style="{ position: 'relative', height: `${totalHeight}px` }">
             <div :style="{ position: 'absolute', top: `${visibleTop}px`, width: '100%' }">
               <!-- Modrinth Section -->
-              <template v-if="visibleItems.modrinth.header">
-                <div class="h-[72px]">
-                  <h2 class="mb-0 mt-8 text-xl font-bold text-contrast">Modrinth mods</h2>
-                  <p class="mb-4 mt-3">
-                    These are mods installed on your server that are listed on Modrinth.
-                  </p>
-                </div>
-              </template>
-
-              <template v-for="mod in visibleItems.modrinth.items" :key="mod.name">
-                <div
-                  class="relative mb-2 flex w-full items-center justify-between rounded-xl bg-bg-raised hover:bg-table-alternateRow"
-                  :class="mod.disabled ? 'bg-table-alternateRow text-secondary' : ''"
-                  style="height: 64px"
-                >
-                  <NuxtLink
-                    :to="`/project/${mod.project_id}/version/${mod.version_id}`"
-                    class="group flex w-full items-center rounded-xl p-2"
-                  >
-                    <div class="flex items-center gap-2">
-                      <UiAvatar
-                        :src="mod.icon_url"
-                        size="sm"
-                        alt="Server Icon"
-                        :class="mod.disabled ? 'grayscale' : ''"
-                      />
-                      <div class="flex flex-col">
-                        <span class="flex items-center gap-2 text-lg font-bold">
-                          {{ mod.name }}
-                          <span
-                            v-if="mod.disabled"
-                            class="rounded-full bg-button-bg p-1 px-2 text-xs text-contrast"
-                            >Disabled</span
-                          >
-                        </span>
-                        <span class="text-xs text-secondary">{{ mod.version_number }}</span>
-                      </div>
-                    </div>
-                  </NuxtLink>
-                  <div
-                    class="absolute right-2 flex gap-2 rounded-xl p-1 font-semibold text-contrast"
-                  >
-                    <ButtonStyled type="transparent">
-                      <button
-                        v-if="mod.project_id"
-                        v-tooltip="'Edit mod version'"
-                        :disabled="
-                          isFetchingVersionsForMod[mod.project_id] ||
-                          modActionsInProgress[mod.project_id]
-                        "
-                        @click="showEditModModal(mod)"
-                      >
-                        <template v-if="isFetchingVersionsForMod[mod.project_id]">
-                          <UiServersLoadingIcon />
-                        </template>
-                        <template v-else>
-                          <EditIcon />
-                        </template>
-                      </button>
-                    </ButtonStyled>
-                    <ButtonStyled type="transparent">
-                      <button
-                        v-tooltip="'Delete mod'"
-                        :disabled="mod.project_id ? modActionsInProgress[mod.project_id] : false"
-                        @click="removeModOptimistic(mod)"
-                      >
-                        <TrashIcon />
-                      </button>
-                    </ButtonStyled>
-                    <input
-                      :id="`toggle-${mod.project_id}`"
-                      :checked="!mod.disabled"
-                      class="switch stylized-toggle"
-                      type="checkbox"
-                      @change="toggleModOptimistic(mod)"
-                    />
+              <div class="universal-card">
+                <template v-if="visibleItems.modrinth.header">
+                  <div class="h-[72px]">
+                    <h2 class="mb-0 mt-8 text-xl font-bold text-contrast">Modrinth mods</h2>
+                    <p class="mb-4 mt-3">
+                      These are mods installed on your server that are listed on Modrinth.
+                    </p>
                   </div>
-                </div>
-              </template>
+                </template>
+
+                <template v-for="mod in visibleItems.modrinth.items" :key="mod.name">
+                  <div
+                    class="relative mb-2 flex w-full items-center justify-between rounded-xl bg-table-alternateRow"
+                    :class="mod.disabled ? 'bg-table-alternateRow text-secondary' : ''"
+                    style="height: 64px"
+                  >
+                    <NuxtLink
+                      :to="`/project/${mod.project_id}/version/${mod.version_id}`"
+                      class="group flex w-full items-center rounded-xl p-2"
+                    >
+                      <div class="flex items-center gap-2">
+                        <UiAvatar
+                          :src="mod.icon_url"
+                          size="sm"
+                          alt="Server Icon"
+                          :class="mod.disabled ? 'grayscale' : ''"
+                        />
+                        <div class="flex flex-col">
+                          <span class="flex items-center gap-2 text-lg font-bold">
+                            {{ mod.name }}
+                            <span
+                              v-if="mod.disabled"
+                              class="rounded-full bg-button-bg p-1 px-2 text-xs text-contrast"
+                              >Disabled</span
+                            >
+                          </span>
+                          <span class="text-xs text-secondary">{{ mod.version_number }}</span>
+                        </div>
+                      </div>
+                    </NuxtLink>
+                    <div
+                      class="absolute right-2 flex gap-2 rounded-xl p-1 font-semibold text-contrast"
+                    >
+                      <ButtonStyled type="transparent">
+                        <button
+                          v-if="mod.project_id"
+                          v-tooltip="'Edit mod version'"
+                          :disabled="
+                            isFetchingVersionsForMod[mod.project_id] ||
+                            modActionsInProgress[mod.project_id]
+                          "
+                          @click="showEditModModal(mod)"
+                        >
+                          <template v-if="isFetchingVersionsForMod[mod.project_id]">
+                            <UiServersLoadingIcon />
+                          </template>
+                          <template v-else>
+                            <EditIcon />
+                          </template>
+                        </button>
+                      </ButtonStyled>
+                      <ButtonStyled type="transparent">
+                        <button
+                          v-tooltip="'Delete mod'"
+                          :disabled="mod.project_id ? modActionsInProgress[mod.project_id] : false"
+                          @click="removeModOptimistic(mod)"
+                        >
+                          <TrashIcon />
+                        </button>
+                      </ButtonStyled>
+                      <input
+                        :id="`toggle-${mod.project_id}`"
+                        :checked="!mod.disabled"
+                        class="switch stylized-toggle"
+                        type="checkbox"
+                        @change="toggleModOptimistic(mod)"
+                      />
+                    </div>
+                  </div>
+                </template>
+              </div>
 
               <!-- External Section -->
-              <template v-if="visibleItems.external.header">
-                <div class="h-[72px]">
-                  <h2 class="mb-0 mt-8 text-xl font-bold text-contrast">External mods</h2>
-                  <p class="mb-4 mt-3">
-                    External mods are mods that are directly uploaded to your server or are part of
-                    a modpack, but are not listed on Modrinth.
-                  </p>
-                </div>
-              </template>
-
-              <template v-for="mod in visibleItems.external.items" :key="mod.filename">
-                <div
-                  class="relative mb-2 flex w-full items-center justify-between rounded-xl bg-bg-raised hover:bg-table-alternateRow"
-                  :class="mod.disabled ? 'bg-table-alternateRow text-secondary' : ''"
-                  style="height: 64px"
-                >
-                  <NuxtLink
-                    :to="`files?path=mods`"
-                    class="group flex w-full items-center rounded-xl p-2"
-                  >
-                    <div class="flex items-center gap-2">
-                      <UiAvatar
-                        :src="mod.icon_url"
-                        size="sm"
-                        alt="Server Icon"
-                        :class="mod.disabled ? 'grayscale' : ''"
-                      />
-                      <div class="flex flex-col">
-                        <span class="flex items-center gap-2 text-lg font-bold">
-                          {{ mod.filename.replace(".disabled", "") }}
-                          <span
-                            v-if="mod.disabled"
-                            class="rounded-full bg-button-bg p-1 px-2 text-xs text-contrast"
-                            >Disabled</span
-                          >
-                        </span>
-                        <span class="text-xs text-secondary"> External Mod </span>
-                      </div>
-                    </div>
-                  </NuxtLink>
-                  <div
-                    class="absolute right-2 flex gap-2 rounded-xl p-1 font-semibold text-contrast"
-                  >
-                    <ButtonStyled type="transparent">
-                      <button
-                        v-tooltip="'Delete mod'"
-                        :disabled="modActionsInProgress[mod.filename]"
-                        @click="removeModOptimistic(mod)"
-                      >
-                        <TrashIcon />
-                      </button>
-                    </ButtonStyled>
-                    <input
-                      :id="`toggle-${mod.filename}`"
-                      :checked="!mod.disabled"
-                      :disabled="modActionsInProgress[mod.filename]"
-                      class="switch stylized-toggle"
-                      type="checkbox"
-                      @change="toggleModOptimistic(mod)"
-                    />
+              <div class="universal-card">
+                <template v-if="visibleItems.external.header">
+                  <div class="h-[72px]">
+                    <h2 class="mb-0 mt-8 text-xl font-bold text-contrast">External mods</h2>
+                    <p class="mb-4 mt-3">
+                      External mods are mods that are directly uploaded to your server or are part
+                      of a modpack, but are not listed on Modrinth.
+                    </p>
                   </div>
-                </div>
-              </template>
+                </template>
+
+                <template v-for="mod in visibleItems.external.items" :key="mod.filename">
+                  <div
+                    class="relative mb-2 flex w-full items-center justify-between rounded-xl bg-table-alternateRow"
+                    :class="mod.disabled ? 'bg-table-alternateRow text-secondary' : ''"
+                    style="height: 64px"
+                  >
+                    <NuxtLink
+                      :to="`files?path=mods`"
+                      class="group flex w-full items-center rounded-xl p-2"
+                    >
+                      <div class="flex items-center gap-2">
+                        <UiAvatar
+                          :src="mod.icon_url"
+                          size="sm"
+                          alt="Server Icon"
+                          :class="mod.disabled ? 'grayscale' : ''"
+                        />
+                        <div class="flex flex-col">
+                          <span class="flex items-center gap-2 text-lg font-bold">
+                            {{ mod.filename.replace(".disabled", "") }}
+                            <span
+                              v-if="mod.disabled"
+                              class="rounded-full bg-button-bg p-1 px-2 text-xs text-contrast"
+                              >Disabled</span
+                            >
+                          </span>
+                          <span class="text-xs text-secondary"> External Mod </span>
+                        </div>
+                      </div>
+                    </NuxtLink>
+                    <div
+                      class="absolute right-2 flex gap-2 rounded-xl p-1 font-semibold text-contrast"
+                    >
+                      <ButtonStyled type="transparent">
+                        <button
+                          v-tooltip="'Delete mod'"
+                          :disabled="modActionsInProgress[mod.filename]"
+                          @click="removeModOptimistic(mod)"
+                        >
+                          <TrashIcon />
+                        </button>
+                      </ButtonStyled>
+                      <input
+                        :id="`toggle-${mod.filename}`"
+                        :checked="!mod.disabled"
+                        :disabled="modActionsInProgress[mod.filename]"
+                        class="switch stylized-toggle"
+                        type="checkbox"
+                        @change="toggleModOptimistic(mod)"
+                      />
+                    </div>
+                  </div>
+                </template>
+              </div>
             </div>
           </div>
         </div>
@@ -220,7 +245,14 @@
 </template>
 
 <script setup lang="ts">
-import { SearchIcon, EditIcon, TrashIcon, PackageClosedIcon } from "@modrinth/assets";
+import {
+  SearchIcon,
+  EditIcon,
+  TrashIcon,
+  PackageClosedIcon,
+  FilterIcon,
+  DropdownIcon,
+} from "@modrinth/assets";
 import { ButtonStyled, NewModal } from "@modrinth/ui";
 import { ref, computed, watch, onMounted, onUnmounted } from "vue";
 import type { Server } from "~/composables/pyroServers";
@@ -258,6 +290,23 @@ const modActionsInProgress = ref<Record<string, boolean>>({});
 const searchInput = ref("");
 const modSearchInput = ref("");
 const isFetchingVersionsForMod = ref<Record<string, boolean>>({});
+
+const filterMethod = ref("all");
+
+const filterMethodLabel = computed(() => {
+  switch (filterMethod.value) {
+    case "disabled":
+      return "Disabled";
+    case "enabled":
+      return "Enabled";
+    default:
+      return "All";
+  }
+});
+
+const filterMods = (method: string) => {
+  filterMethod.value = method;
+};
 
 const totalHeight = computed(() => {
   const modrinthHeight = filteredModrinthMods.value.length * ITEM_HEIGHT;
@@ -498,14 +547,22 @@ const hasMods = computed(() => {
 });
 
 const filteredMods = computed(() => {
-  if (!modSearchInput.value.trim()) {
-    return localMods.value;
+  const mods = modSearchInput.value.trim()
+    ? localMods.value.filter(
+        (mod) =>
+          mod.name?.toLowerCase().includes(modSearchInput.value.toLowerCase()) ||
+          mod.filename.toLowerCase().includes(modSearchInput.value.toLowerCase()),
+      )
+    : localMods.value;
+
+  switch (filterMethod.value) {
+    case "disabled":
+      return mods.filter((mod) => mod.disabled);
+    case "enabled":
+      return mods.filter((mod) => !mod.disabled);
+    default:
+      return mods;
   }
-  return localMods.value.filter(
-    (mod) =>
-      mod.name?.toLowerCase().includes(modSearchInput.value.toLowerCase()) ||
-      mod.filename.toLowerCase().includes(modSearchInput.value.toLowerCase()),
-  );
 });
 
 const filteredExternalMods = computed(() => {
