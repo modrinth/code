a4a0bf1e6306e795255984d0ae69468e4099136e
Evan Song <theevansong@gmail.com>
2024-10-25 22:23:59 -0700

    fix: root node
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/[id]/options/network.vue
+++ apps/frontend/src/pages/servers/manage/[id]/options/network.vue
@@ -1,243 +1,245 @@
 <template>
-  <NewModal ref="newAllocationModal" header="New Allocation">
-    <form class="flex flex-col gap-2 md:w-[600px]" @submit.prevent="addNewAllocation">
-      <label for="new-allocation-name" class="font-semibold text-contrast"> Name </label>
-      <input
-        id="new-allocation-name"
-        ref="newAllocationInput"
-        v-model="newAllocationName"
-        type="text"
-        class="bg-bg-input w-full rounded-lg p-4"
-        maxlength="32"
-        placeholder="e.g. Secondary allocation"
-      />
-      <div class="mb-1 mt-4 flex justify-start gap-4">
-        <ButtonStyled color="brand">
-          <button :disabled="!newAllocationName" type="submit">
-            <PlusIcon /> Create allocation
-          </button>
-        </ButtonStyled>
-        <ButtonStyled>
-          <button @click="newAllocationModal?.hide()">Cancel</button>
-        </ButtonStyled>
-      </div>
-    </form>
-  </NewModal>
-
-  <NewModal ref="editAllocationModal" header="Edit Allocation">
-    <form class="flex flex-col gap-2 md:w-[600px]" @submit.prevent="editAllocation">
-      <label for="edit-allocation-name" class="font-semibold text-contrast"> Name </label>
-      <input
-        id="edit-allocation-name"
-        ref="editAllocationInput"
-        v-model="newAllocationName"
-        type="text"
-        class="bg-bg-input w-full rounded-lg p-4"
-        maxlength="32"
-        placeholder="e.g. Secondary allocation"
-      />
-      <div class="mb-1 mt-4 flex justify-start gap-4">
-        <ButtonStyled color="brand">
-          <button :disabled="!newAllocationName" type="submit">
-            <SaveIcon /> Update Allocation
-          </button>
-        </ButtonStyled>
-        <ButtonStyled>
-          <button @click="editAllocationModal?.hide()">Cancel</button>
-        </ButtonStyled>
-      </div>
-    </form>
-  </NewModal>
-
-  <ConfirmModal
-    ref="confirmDeleteModal"
-    title="Are you sure you want to delete this allocation?"
-    description="This will permanently delete this allocation."
-    proceed-label="Delete"
-    @proceed="confirmDeleteAllocation"
-  />
-
-  <div class="relative h-full w-full overflow-y-auto">
-    <div v-if="data" class="flex h-full w-full flex-col justify-between gap-4">
-      <div class="flex h-full flex-col">
-        <!-- Subdomain section -->
-        <div class="card flex flex-col gap-4">
-          <label for="server-subdomain" class="flex flex-col gap-2">
-            <span class="text-lg font-bold text-contrast">Custom subdomain</span>
-            <span> Your friends can connect to your server using this URL. </span>
-          </label>
-          <div class="flex w-full items-center gap-2 md:w-[60%]">
-            <input
-              id="server-subdomain"
-              v-model="serverSubdomain"
-              class="h-[50%] w-[63%]"
-              maxlength="32"
-              @keyup.enter="saveNetwork"
-            />
-            .modrinth.gg
-          </div>
-          <span v-if="!isValidSubdomain" class="text-sm text-rose-400">
-            Subdomain must be at least 5 characters long and can only contain alphanumeric
-            characters and dashes.
-          </span>
+  <div class="contents">
+    <NewModal ref="newAllocationModal" header="New Allocation">
+      <form class="flex flex-col gap-2 md:w-[600px]" @submit.prevent="addNewAllocation">
+        <label for="new-allocation-name" class="font-semibold text-contrast"> Name </label>
+        <input
+          id="new-allocation-name"
+          ref="newAllocationInput"
+          v-model="newAllocationName"
+          type="text"
+          class="bg-bg-input w-full rounded-lg p-4"
+          maxlength="32"
+          placeholder="e.g. Secondary allocation"
+        />
+        <div class="mb-1 mt-4 flex justify-start gap-4">
+          <ButtonStyled color="brand">
+            <button :disabled="!newAllocationName" type="submit">
+              <PlusIcon /> Create allocation
+            </button>
+          </ButtonStyled>
+          <ButtonStyled>
+            <button @click="newAllocationModal?.hide()">Cancel</button>
+          </ButtonStyled>
         </div>
+      </form>
+    </NewModal>
 
-        <div class="card flex flex-col gap-4">
-          <div class="flex w-full flex-row items-center justify-between">
-            <label for="user-domain" class="flex flex-col gap-2">
-              <span class="text-lg font-bold text-contrast">Generated DNS records</span>
-              <span>
-                Set up your personal domain to connect to your server via custom DNS records.
-              </span>
+    <NewModal ref="editAllocationModal" header="Edit Allocation">
+      <form class="flex flex-col gap-2 md:w-[600px]" @submit.prevent="editAllocation">
+        <label for="edit-allocation-name" class="font-semibold text-contrast"> Name </label>
+        <input
+          id="edit-allocation-name"
+          ref="editAllocationInput"
+          v-model="newAllocationName"
+          type="text"
+          class="bg-bg-input w-full rounded-lg p-4"
+          maxlength="32"
+          placeholder="e.g. Secondary allocation"
+        />
+        <div class="mb-1 mt-4 flex justify-start gap-4">
+          <ButtonStyled color="brand">
+            <button :disabled="!newAllocationName" type="submit">
+              <SaveIcon /> Update Allocation
+            </button>
+          </ButtonStyled>
+          <ButtonStyled>
+            <button @click="editAllocationModal?.hide()">Cancel</button>
+          </ButtonStyled>
+        </div>
+      </form>
+    </NewModal>
+
+    <ConfirmModal
+      ref="confirmDeleteModal"
+      title="Are you sure you want to delete this allocation?"
+      description="This will permanently delete this allocation."
+      proceed-label="Delete"
+      @proceed="confirmDeleteAllocation"
+    />
+
+    <div class="relative h-full w-full overflow-y-auto">
+      <div v-if="data" class="flex h-full w-full flex-col justify-between gap-4">
+        <div class="flex h-full flex-col">
+          <!-- Subdomain section -->
+          <div class="card flex flex-col gap-4">
+            <label for="server-subdomain" class="flex flex-col gap-2">
+              <span class="text-lg font-bold text-contrast">Custom subdomain</span>
+              <span> Your friends can connect to your server using this URL. </span>
             </label>
-
-            <ButtonStyled>
-              <button @click="exportDnsRecords">
-                <UploadIcon />
-                <span>Export DNS Records</span>
-              </button>
-            </ButtonStyled>
+            <div class="flex w-full items-center gap-2 md:w-[60%]">
+              <input
+                id="server-subdomain"
+                v-model="serverSubdomain"
+                class="h-[50%] w-[63%]"
+                maxlength="32"
+                @keyup.enter="saveNetwork"
+              />
+              .modrinth.gg
+            </div>
+            <span v-if="!isValidSubdomain" class="text-sm text-rose-400">
+              Subdomain must be at least 5 characters long and can only contain alphanumeric
+              characters and dashes.
+            </span>
           </div>
 
-          <input
-            id="user-domain"
-            v-model="userDomain"
-            class="w-full md:w-[50%]"
-            maxlength="64"
-            minlength="1"
-            type="text"
-            placeholder="domain.com"
-          />
+          <div class="card flex flex-col gap-4">
+            <div class="flex w-full flex-row items-center justify-between">
+              <label for="user-domain" class="flex flex-col gap-2">
+                <span class="text-lg font-bold text-contrast">Generated DNS records</span>
+                <span>
+                  Set up your personal domain to connect to your server via custom DNS records.
+                </span>
+              </label>
 
-          <div class="w-full rounded-xl bg-table-alternateRow p-4">
-            <table
-              class="flex w-full border-collapse overflow-hidden truncate rounded-lg border-2 border-gray-300"
+              <ButtonStyled>
+                <button @click="exportDnsRecords">
+                  <UploadIcon />
+                  <span>Export DNS Records</span>
+                </button>
+              </ButtonStyled>
+            </div>
+
+            <input
+              id="user-domain"
+              v-model="userDomain"
+              class="w-full md:w-[50%]"
+              maxlength="64"
+              minlength="1"
+              type="text"
+              placeholder="domain.com"
+            />
+
+            <div class="w-full rounded-xl bg-table-alternateRow p-4">
+              <table
+                class="flex w-full border-collapse overflow-hidden truncate rounded-lg border-2 border-gray-300"
+              >
+                <tbody class="w-full">
+                  <tr v-for="record in dnsRecords" :key="record.content" class="w-full">
+                    <td class="py-3">
+                      <div class="ml-2 flex flex-col gap-1" @click="copyText(record.type)">
+                        <span
+                          class="text-md font-bold tracking-wide text-contrast hover:cursor-pointer"
+                        >
+                          {{ record.type }}
+                        </span>
+                        <span class="text-xs uppercase text-secondary">type</span>
+                      </div>
+                    </td>
+                    <td class="truncate py-3">
+                      <div class="flex flex-col gap-1 truncate" @click="copyText(record.name)">
+                        <span
+                          class="text-md truncate font-bold tracking-wide text-contrast hover:cursor-pointer"
+                        >
+                          {{ record.name }}
+                        </span>
+                        <span class="text-xs uppercase text-secondary">name</span>
+                      </div>
+                    </td>
+                    <td class="truncate px-4">
+                      <div class="flex flex-col gap-1 truncate" @click="copyText(record.content)">
+                        <span
+                          class="text-md w-fit truncate font-bold tracking-wide text-contrast hover:cursor-pointer"
+                        >
+                          {{ record.content }}
+                        </span>
+                        <span class="text-xs uppercase text-secondary">content</span>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="flex items-center gap-2">
+              <InfoIcon />
+              <span class="text-sm text-secondary">
+                You must own your own domain to use this feature.
+              </span>
+            </div>
+          </div>
+
+          <!-- Allocations section -->
+          <div class="card flex flex-col gap-4">
+            <div class="flex w-full flex-row items-center justify-between">
+              <div class="flex flex-col gap-2">
+                <span class="text-lg font-bold text-contrast">Allocations</span>
+                <span>
+                  Configure additional ports for internet-facing features like map viewers or voice
+                  chat mods.
+                </span>
+              </div>
+
+              <ButtonStyled type="standard" color="brand" @click="showNewAllocationModal">
+                <button>
+                  <PlusIcon />
+                  <span>New allocation</span>
+                </button>
+              </ButtonStyled>
+            </div>
+
+            <div class="flex w-full flex-col overflow-hidden rounded-xl bg-table-alternateRow p-4">
+              <!-- Primary allocation -->
+              <div class="flex flex-row items-center justify-between">
+                <span class="text-md font-bold capitalize tracking-wide text-contrast">
+                  Primary Allocation
+                </span>
+
+                <UiCopyCode :text="`${serverIP}:${serverPrimaryPort}`" />
+              </div>
+            </div>
+
+            <div
+              v-if="allocations?.[0]"
+              class="flex w-full flex-col gap-4 overflow-hidden rounded-xl bg-table-alternateRow p-4"
             >
-              <tbody class="w-full">
-                <tr v-for="record in dnsRecords" :key="record.content" class="w-full">
-                  <td class="py-3">
-                    <div class="ml-2 flex flex-col gap-1" @click="copyText(record.type)">
-                      <span
-                        class="text-md font-bold tracking-wide text-contrast hover:cursor-pointer"
-                      >
-                        {{ record.type }}
-                      </span>
-                      <span class="text-xs uppercase text-secondary">type</span>
-                    </div>
-                  </td>
-                  <td class="truncate py-3">
-                    <div class="flex flex-col gap-1 truncate" @click="copyText(record.name)">
-                      <span
-                        class="text-md truncate font-bold tracking-wide text-contrast hover:cursor-pointer"
-                      >
-                        {{ record.name }}
+              <div
+                v-for="allocation in allocations"
+                :key="allocation.port"
+                class="border-border flex flex-row items-center justify-between"
+              >
+                <div class="flex flex-row items-center gap-4">
+                  <VersionIcon class="h-7 w-7 rotate-90" />
+                  <div class="flex w-[20rem] flex-row items-center justify-between">
+                    <div class="flex flex-col gap-1">
+                      <span class="text-md font-bold capitalize tracking-wide text-contrast">
+                        {{ allocation.name }}
                       </span>
                       <span class="text-xs uppercase text-secondary">name</span>
                     </div>
-                  </td>
-                  <td class="truncate px-4">
-                    <div class="flex flex-col gap-1 truncate" @click="copyText(record.content)">
-                      <span
-                        class="text-md w-fit truncate font-bold tracking-wide text-contrast hover:cursor-pointer"
-                      >
-                        {{ record.content }}
+                    <div class="flex flex-col gap-1">
+                      <span class="text-md w-10 font-bold capitalize tracking-wide text-contrast">
+                        {{ allocation.port }}
                       </span>
-                      <span class="text-xs uppercase text-secondary">content</span>
+                      <span class="text-xs uppercase text-secondary">port</span>
                     </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="flex items-center gap-2">
-            <InfoIcon />
-            <span class="text-sm text-secondary">
-              You must own your own domain to use this feature.
-            </span>
-          </div>
-        </div>
-
-        <!-- Allocations section -->
-        <div class="card flex flex-col gap-4">
-          <div class="flex w-full flex-row items-center justify-between">
-            <div class="flex flex-col gap-2">
-              <span class="text-lg font-bold text-contrast">Allocations</span>
-              <span>
-                Configure additional ports for internet-facing features like map viewers or voice
-                chat mods.
-              </span>
-            </div>
-
-            <ButtonStyled type="standard" color="brand" @click="showNewAllocationModal">
-              <button>
-                <PlusIcon />
-                <span>New allocation</span>
-              </button>
-            </ButtonStyled>
-          </div>
-
-          <div class="flex w-full flex-col overflow-hidden rounded-xl bg-table-alternateRow p-4">
-            <!-- Primary allocation -->
-            <div class="flex flex-row items-center justify-between">
-              <span class="text-md font-bold capitalize tracking-wide text-contrast">
-                Primary Allocation
-              </span>
-
-              <UiCopyCode :text="`${serverIP}:${serverPrimaryPort}`" />
-            </div>
-          </div>
-
-          <div
-            v-if="allocations?.[0]"
-            class="flex w-full flex-col gap-4 overflow-hidden rounded-xl bg-table-alternateRow p-4"
-          >
-            <div
-              v-for="allocation in allocations"
-              :key="allocation.port"
-              class="border-border flex flex-row items-center justify-between"
-            >
-              <div class="flex flex-row items-center gap-4">
-                <VersionIcon class="h-7 w-7 rotate-90" />
-                <div class="flex w-[20rem] flex-row items-center justify-between">
-                  <div class="flex flex-col gap-1">
-                    <span class="text-md font-bold capitalize tracking-wide text-contrast">
-                      {{ allocation.name }}
-                    </span>
-                    <span class="text-xs uppercase text-secondary">name</span>
-                  </div>
-                  <div class="flex flex-col gap-1">
-                    <span class="text-md w-10 font-bold capitalize tracking-wide text-contrast">
-                      {{ allocation.port }}
-                    </span>
-                    <span class="text-xs uppercase text-secondary">port</span>
                   </div>
                 </div>
-              </div>
 
-              <div class="flex flex-row items-center gap-2">
-                <ButtonStyled icon-only>
-                  <button @click="showEditAllocationModal(allocation.port)">
-                    <EditIcon />
-                  </button>
-                </ButtonStyled>
-                <ButtonStyled icon-only color="red">
-                  <button @click="showConfirmDeleteModal(allocation.port)">
-                    <TrashIcon />
-                  </button>
-                </ButtonStyled>
+                <div class="flex flex-row items-center gap-2">
+                  <ButtonStyled icon-only>
+                    <button @click="showEditAllocationModal(allocation.port)">
+                      <EditIcon />
+                    </button>
+                  </ButtonStyled>
+                  <ButtonStyled icon-only color="red">
+                    <button @click="showConfirmDeleteModal(allocation.port)">
+                      <TrashIcon />
+                    </button>
+                  </ButtonStyled>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <UiServersSaveBanner
+        :is-visible="!!hasUnsavedChanges && !!isValidSubdomain"
+        :server="props.server"
+        :is-updating="isUpdating"
+        :save="saveNetwork"
+        :reset="resetNetwork"
+      />
     </div>
-    <UiServersSaveBanner
-      :is-visible="!!hasUnsavedChanges && !!isValidSubdomain"
-      :server="props.server"
-      :is-updating="isUpdating"
-      :save="saveNetwork"
-      :reset="resetNetwork"
-    />
   </div>
 </template>
 
