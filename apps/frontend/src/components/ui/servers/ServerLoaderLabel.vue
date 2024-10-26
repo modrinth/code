f3bc425398ed1a7bc82dc0fb04254de787c2b740
Evan Song <theevansong@gmail.com>
2024-10-26 11:51:27 -0700

    chore: truncation fixes
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
+++ apps/frontend/src/components/ui/servers/ServerLoaderLabel.vue
@@ -2,14 +2,14 @@
   <div
     v-if="loader"
     v-tooltip="'Change server loader'"
-    class="flex min-w-0 flex-row items-center gap-4"
+    class="flex min-w-0 flex-row items-center gap-4 truncate"
   >
     <div class="experimental-styles-within h-6 w-0.5 bg-button-border"></div>
     <div class="flex flex-row items-center gap-2">
       <UiServersIconsLoaderIcon :loader="loader" class="flex shrink-0 [&&]:size-5" />
       <NuxtLink
         :to="serverId ? `/servers/manage/${serverId}/options/loader` : ''"
-        class="min-w-0 truncate text-sm font-semibold"
+        class="min-w-0 text-sm font-semibold"
         :class="serverId ? 'hover:underline' : ''"
       >
         {{ loader }} <span v-if="loaderVersion">{{ loaderVersion }}</span>
