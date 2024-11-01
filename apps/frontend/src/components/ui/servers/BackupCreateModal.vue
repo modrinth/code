55fa00cad9db693bb587d3fea21eedcbd494b5d7
Evan Song <theevansong@gmail.com>
2024-11-01 08:43:29 -0700

    fix: compiler macro warning
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/components/ui/servers/BackupCreateModal.vue
+++ apps/frontend/src/components/ui/servers/BackupCreateModal.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits, nextTick } from "vue";
+import { ref, nextTick } from "vue";
 import { ButtonStyled, NewModal } from "@modrinth/ui";
 import { PlusIcon, XIcon } from "@modrinth/assets";
 import type { Server } from "~/composables/pyroServers";
