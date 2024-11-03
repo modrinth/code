89be8b98d441a9f9df4773d93a78661fa8785416
Evan Song <theevansong@gmail.com>
2024-11-03 00:12:03 -0700

    chore: clarify SFTP and networking features
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/index.vue
+++ apps/frontend/src/pages/servers/index.vue
@@ -294,7 +294,7 @@
               </svg>
               <h2 class="m-0 text-lg font-bold">Backups included</h2>
               <h3 class="m-0 text-base font-normal text-secondary">
-                Every server comes with 15 backups stored off-site with Backblaze.
+                Every server comes with 15 backups stored securely off-site with Backblaze.
               </h3>
             </div>
           </div>
@@ -345,33 +345,12 @@
         </div>
         <div class="grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
           <div class="relative flex flex-col gap-4 rounded-2xl bg-bg p-6 text-left md:p-12">
-            <!-- <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="size-8 text-brand"
-            >
-              <path
-                d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
-              />
-              <path
-                d="M12 5 9.04 7.96a2.17 2.17 0 0 0 0 3.08c.82.82 2.13.85 3 .07l2.07-1.9a2.82 2.82 0 0 1 3.79 0l2.96 2.66"
-              />
-              <path d="m18 15-2-2" />
-              <path d="m15 18-2-2" />
-            </svg> -->
             <TerminalSquareIcon class="size-8 text-brand" />
             <h2 class="m-0 text-lg font-bold">
-              An easy console, server properties manager, and more
+              A powerful console, server properties manager, and more
             </h2>
             <h3 class="m-0 text-base font-normal text-secondary">
-              Super powerful features with super simple access.
+              Modrinth Servers come with powerful tools to manage your server.
             </h3>
           </div>
           <div class="relative flex flex-col gap-4 rounded-2xl bg-bg p-6 text-left md:p-12">
@@ -399,6 +378,24 @@
             <h2 class="m-0 text-lg font-bold">Help when you need it</h2>
             <h3 class="m-0 text-base font-normal text-secondary">
               Reach out to the Modrinth team for help with your server at any time.
+            </h3>
+          </div>
+        </div>
+
+        <div class="grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
+          <div class="relative flex flex-col gap-4 rounded-2xl bg-bg p-6 text-left md:p-12">
+            <TransferIcon class="size-8 text-brand" />
+            <h2 class="m-0 text-lg font-bold">SFTP access</h2>
+            <h3 class="m-0 text-base font-normal text-secondary">
+              Access your server files directly with SFTP built into Modrinth Servers.
+            </h3>
+          </div>
+          <div class="relative flex flex-col gap-4 rounded-2xl bg-bg p-6 text-left md:p-12">
+            <VersionIcon class="size-8 text-brand" />
+            <h2 class="m-0 text-lg font-bold">Advanced networking management</h2>
+            <h3 class="m-0 text-base font-normal text-secondary">
+              Add your own domain to your server, reserve up to 15 ports for mods that require them,
+              and more.
             </h3>
           </div>
         </div>
@@ -640,6 +637,8 @@ import {
   ExternalIcon,
   TerminalSquareIcon,
   UserIcon,
+  TransferIcon,
+  VersionIcon,
 } from "@modrinth/assets";
 import { products } from "~/generated/state.json";
 import LoaderIcon from "~/components/ui/servers/icons/LoaderIcon.vue";
