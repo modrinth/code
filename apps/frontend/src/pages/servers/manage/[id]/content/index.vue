d8d60bcb98730db46617a71c05913a0f810b4971
Evan Song <theevansong@gmail.com>
2024-12-10 12:57:41 -0700

    fix: content type error
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- apps/frontend/src/pages/servers/manage/[id]/content/index.vue
+++ apps/frontend/src/pages/servers/manage/[id]/content/index.vue
@@ -294,7 +294,7 @@ import { ref, computed, watch, onMounted, onUnmounted } from "vue";
 import type { Server } from "~/composables/pyroServers";
 
 const props = defineProps<{
-  server: Server<["general", "mods", "backups", "network", "startup", "ws", "fs"]>;
+  server: Server<["general", "content", "backups", "network", "startup", "ws", "fs"]>;
 }>();
 
 const type = computed(() => {
