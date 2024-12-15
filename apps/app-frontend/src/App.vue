217b5700a2332a0f7b855e1cb93350801f0af208
Geometrically <18202329+Geometrically@users.noreply.github.com>
2024-12-14 21:36:00 -0700

    Fix advanced rendering, ads showing over modals (#3029)

--- apps/app-frontend/src/App.vue
+++ apps/app-frontend/src/App.vue
@@ -133,8 +133,8 @@ async function setupApp() {
     default_page,
   } = await get()
 
-  if (default_page && default_page !== 'Home') {
-    await router.push({ name: default_page })
+  if (default_page === 'Library') {
+    await router.push('/library')
   }
 
   os.value = await getOS()
