a19c249f7abcf5014140260ba4152963a5cd9450
Josiah Glosson <soujournme@gmail.com>
2025-01-28 16:22:50 -0600

    Fix config.rs

--- packages/app-lib/src/config.rs
+++ packages/app-lib/src/config.rs
@@ -4,10 +4,10 @@
 // pub const MODRINTH_API_URL: &str = "https://staging-api.modrinth.com/v2/";
 // pub const MODRINTH_API_URL_V3: &str = "https://staging-api.modrinth.com/v3/";
 
-pub const MODRINTH_URL: &str = "http://localhost:3000/";
-pub const MODRINTH_API_URL: &str = "http://localhost:8000/v2/";
-pub const MODRINTH_API_URL_V3: &str = "http://localhost:8000/v3/";
+pub const MODRINTH_URL: &str = "https://modrinth.com/";
+pub const MODRINTH_API_URL: &str = "https://api.modrinth.com/v2/";
+pub const MODRINTH_API_URL_V3: &str = "https://api.modrinth.com/v3/";
 
-pub const MODRINTH_SOCKET_URL: &str = "ws://localhost:8000/";
+pub const MODRINTH_SOCKET_URL: &str = "wss://api.modrinth.com/";
 
 pub const META_URL: &str = "https://launcher-meta.modrinth.com/";
