905ee137b0a46e84662fdb430653378c0d559eaf
Jai A <jaiagr+gpg@pm.me>
2025-02-04 15:22:18 -0800

    Format traces as bullet list

--- apps/labrinth/src/routes/internal/admin.rs
+++ apps/labrinth/src/routes/internal/admin.rs
@@ -244,7 +244,7 @@ pub async fn delphi_result_ingest(
     for (issue, trace) in &body.issues {
         for path in trace.keys() {
             thread_header
-                .push_str(&format!("\n issue {issue} found at file {}", path));
+                .push_str(&format!("\n\n- issue {issue} found at file {}", path));
         }
     }
 
