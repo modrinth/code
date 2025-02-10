3abed658e9a7b1cca4f04c8c5aee6b03b9cf016d
Erb3 <49862976+Erb3@users.noreply.github.com>
2025-02-10 17:34:27 +0100

    chore(frontend): fmt

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,6 +1,6 @@
 export * from './billing'
 export * from './highlight'
-export * from "./licenses"
+export * from './licenses'
 export * from './parse'
 export * from './projects'
 export * from './types'
