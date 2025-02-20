ca63c09a0d6a0a1528a86000d003beb24cf3ecd9
Prospector <6166773+Prospector@users.noreply.github.com>
2025-02-19 22:00:52 -0800

    Enhance moderation checklist (#3273)

--- apps/frontend/src/composables/featureFlags.ts
+++ apps/frontend/src/composables/featureFlags.ts
@@ -21,6 +21,7 @@ export const DEFAULT_FEATURE_FLAGS = validateValues({
   developerMode: false,
   showVersionFilesInTable: false,
   showAdsWithPlus: false,
+  alwaysShowChecklistAsPopup: true,
 
   // Feature toggles
   projectTypesPrimaryNav: false,
