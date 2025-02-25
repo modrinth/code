8f3fcb6d1235cbcd79292ed389aac33c0df743e8
Evan Song <theevansong@gmail.com>
2025-02-24 19:19:09 -0700

    feat: improved crash UX

--- apps/frontend/src/pages/servers/manage/[id]/index.vue
+++ apps/frontend/src/pages/servers/manage/[id]/index.vue
@@ -7,8 +7,13 @@
     <div
       v-if="inspectingError"
       data-pyro-servers-inspecting-error
-      class="flex justify-between rounded-2xl border-2 border-solid border-red bg-bg-red p-4 font-semibold text-contrast"
+      class="relative flex justify-between rounded-3xl bg-bg-red p-4"
     >
+      <ButtonStyled type="transparent" circular @click="clearError">
+        <button class="absolute right-4 top-4">
+          <XIcon />
+        </button>
+      </ButtonStyled>
       <div class="flex w-full justify-between gap-2">
         <div v-if="inspectingError.analysis.problems.length" class="flex flex-row gap-4">
           <IssuesIcon class="hidden h-8 w-8 text-red sm:block" />
@@ -35,28 +40,144 @@
             </li>
           </div>
         </div>
-        <div v-else-if="props.serverPowerState === 'crashed'" class="flex flex-row gap-4">
-          <IssuesIcon class="hidden h-8 w-8 text-red sm:block" />
 
-          <div class="flex flex-col gap-2">
-            <div class="font-semibold">{{ serverData?.name }} shut down unexpectedly.</div>
-            <div class="font-normal">
-              <template v-if="props.powerStateDetails?.oom_killed">
-                The server stopped because it ran out of memory. There may be a memory leak caused
-                by a mod or plugin, or you may need to upgrade your Modrinth Server.
-              </template>
-              <template v-else-if="props.powerStateDetails?.exit_code !== undefined">
-                We could not automatically determine the specific cause of the crash, but your
-                server exited with code
-                {{ props.powerStateDetails.exit_code }}.
-                {{
-                  props.powerStateDetails.exit_code === 1
-                    ? "There may be a mod or plugin causing the issue, or an issue with your server configuration."
-                    : ""
-                }}
-              </template>
-              <template v-else> We could not determine the specific cause of the crash. </template>
-              <div class="mt-2">You can try restarting the server.</div>
+        <div v-else-if="props.serverPowerState === 'crashed'" class="flex w-full flex-row gap-4">
+          <div class="flex w-full flex-col gap-4">
+            <div class="flex flex-row gap-4">
+              <IssuesIcon class="hidden min-h-8 min-w-8 text-red sm:block" />
+
+              <div class="flex flex-col gap-2">
+                <div class="text-xl font-semibold text-contrast">
+                  {{ serverData?.name }} shut down unexpectedly
+                </div>
+                <template v-if="props.powerStateDetails?.oom_killed">
+                  The server stopped because it ran out of memory. There may be a memory leak caused
+                  by a mod or plugin, or you may need to upgrade your Modrinth Server.
+                </template>
+                <template v-else-if="props.powerStateDetails?.exit_code !== undefined">
+                  <div class="w-full">
+                    <div class="leading-[190%]">
+                      The server crashed with exit code {{ props.powerStateDetails.exit_code }}.
+                      We've gathered some steps to help you troubleshoot the issue.
+                    </div>
+                  </div>
+                </template>
+                <template v-else>
+                  We could not determine the specific cause of the crash.
+                </template>
+              </div>
+            </div>
+            <div class="flex w-full flex-col gap-4 lg:flex-row">
+              <div class="flex w-full flex-col gap-2 rounded-2xl bg-bg p-4">
+                <div class="flex flex-row items-center gap-2 font-semibold text-contrast">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-shapes"
+                  >
+                    <path
+                      d="M8.3 10a.7.7 0 0 1-.626-1.079L11.4 3a.7.7 0 0 1 1.198-.043L16.3 8.9a.7.7 0 0 1-.572 1.1Z"
+                    />
+                    <rect x="3" y="14" width="7" height="7" rx="1" />
+                    <circle cx="17.5" cy="17.5" r="3.5" />
+                  </svg>
+                  Check your server's content
+                </div>
+                <div class="text-sm">
+                  Disable any mods or plugins that you don't need, or any that are only needed on
+                  the client.
+                </div>
+                <ButtonStyled>
+                  <NuxtLink
+                    class="mt-2 !w-full whitespace-pre"
+                    :to="`/servers/manage/${props.server.serverId}/content`"
+                  >
+                    Manage server content
+                  </NuxtLink>
+                </ButtonStyled>
+              </div>
+              <div class="flex w-full flex-col gap-2 rounded-2xl bg-bg p-4">
+                <div class="flex flex-row items-center gap-2 font-semibold text-contrast">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-logs"
+                  >
+                    <path d="M13 12h8" />
+                    <path d="M13 18h8" />
+                    <path d="M13 6h8" />
+                    <path d="M3 12h1" />
+                    <path d="M3 18h1" />
+                    <path d="M3 6h1" />
+                    <path d="M8 12h1" />
+                    <path d="M8 18h1" />
+                    <path d="M8 6h1" />
+                  </svg>
+                  Read your server's logs
+                </div>
+                <div class="text-sm">
+                  Check the logs for errors or warnings. Consider any recent changes made to your
+                  server's configuration.
+                </div>
+                <ButtonStyled @click="fullScreen = !fullScreen">
+                  <button class="mt-2 !w-full whitespace-pre">View logs</button>
+                </ButtonStyled>
+              </div>
+              <div class="flex w-full flex-col gap-2 rounded-2xl bg-bg p-4">
+                <div class="flex flex-row items-center gap-2 font-semibold text-contrast">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-megaphone"
+                  >
+                    <path d="m3 11 18-5v12L3 14v-3z" />
+                    <path d="M11.6 16.8a3 3 0 1 1-5.8-1.6" />
+                  </svg>
+                  Report the issue
+                </div>
+                <div class="text-sm">
+                  Your server may be configured incorrectly. Report the issue to the mod or plugin's
+                  creators.
+                </div>
+                <template v-if="props.server.general?.project">
+                  <ButtonStyled>
+                    <a
+                      class="mt-2 !w-full whitespace-pre"
+                      :href="`https://modrinth.com/modpack/${props.server.general.project.id}`"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Report to {{ props.server.general.project.title }}
+                    </a>
+                  </ButtonStyled>
+                </template>
+              </div>
+            </div>
+            <div class="flex flex-row gap-2">
+              <InfoIcon />
+              If you believe this is an issue with the Modrinth Servers platform, please contact
+              support using the chat bubble in the bottom right.
             </div>
           </div>
         </div>
@@ -70,11 +191,6 @@
             </div>
           </div>
         </div>
-        <ButtonStyled color="red" @click="clearError">
-          <button>
-            <XIcon />
-          </button>
-        </ButtonStyled>
       </div>
     </div>
     <div class="flex flex-col-reverse gap-6 md:flex-col">
@@ -110,7 +226,7 @@
 </template>
 
 <script setup lang="ts">
-import { IssuesIcon, XIcon } from "@modrinth/assets";
+import { InfoIcon, IssuesIcon, XIcon } from "@modrinth/assets";
 import { ButtonStyled } from "@modrinth/ui";
 import type { ServerState, Stats as StatsType } from "~/types/servers";
 import type { Server } from "~/composables/pyroServers";
