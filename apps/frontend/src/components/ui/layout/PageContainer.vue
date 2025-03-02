ce3378eefaaa50e073bc4f582b160a8c8820e942
Prospector <prospectordev@gmail.com>
2025-03-01 17:56:51 -0800

    Fix changelog page

--- apps/frontend/src/components/ui/layout/PageContainer.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="experimental-styles-within mx-auto max-w-[56rem] p-4"></div>
-</template>
--- apps/frontend/src/pages/news/changelog.vue
+++ apps/frontend/src/pages/news/changelog.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts">
-import PageContainer from "~/components/ui/layout/PageContainer.vue";
-</script>
 <template>
-  <PageContainer>
+  <div class="page experimental-styles-within">
     <h1 class="m-0 text-3xl font-extrabold">Changelog</h1>
     <p class="my-3">Keep up-to-date on what's new with Modrinth.</p>
     <NuxtPage />
-  </PageContainer>
+  </div>
 </template>
+<style lang="scss" scoped>
+.page {
+  padding: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 56rem;
+}
+</style>
