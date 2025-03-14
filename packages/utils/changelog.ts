84a9438a70ba358d6949d4d2fa50f0d3ae7a3130
Prospector <prospectordev@gmail.com>
2025-03-13 19:24:34 -0700

    Update changelog

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,6 +11,12 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
   {
+    date: `2025-03-13T19:30:00-08:00`,
+    product: 'web',
+    body: `### Improvements
+- Updated Modrinth Servers marketing page, removing Pyro branding.`,
+  },
+  {
     date: `2025-03-12T10:15:00-08:00`,
     product: 'web',
     body: `### Improvements
