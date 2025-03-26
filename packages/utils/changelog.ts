86128f953a77ad97699ef568d53bd522a52d95bd
Prospector <prospectordev@gmail.com>
2025-03-25 18:17:48 -0700

    Update changelog

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,32 +11,39 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
   {
-    date: `2025-03-24T22:30:00-08:00`,
+    date: `2025-03-25T18:25:00-07:00`,
+    product: 'web',
+    body: `### Improvements
+- Fixed random 'displayName' error on search pages on some browsers such as Firefox.
+- Fixed 'Resubmit' icon in publishing checklist showing up when it hasn't been submitted before.`,
+  },
+  {
+    date: `2025-03-25T10:40:00-07:00`,
     product: 'web',
     body: `### Improvements
 - Fixed error with links on error pages.`,
   },
   {
-    date: `2025-03-24T22:30:00-08:00`,
+    date: `2025-03-24T22:30:00-07:00`,
     product: 'servers',
     body: `### Improvements
 - Fixed server plugin loaders not being populated when browsing for plugins
 - Fixed modpack search being filtered by Minecraft version when browsing for modpacks.`,
   },
   {
-    date: `2025-03-24T22:30:00-08:00`,
+    date: `2025-03-24T22:30:00-07:00`,
     product: 'web',
     body: `### Improvements
 - Improved error handling, especially when the Modrinth API is down.`,
   },
   {
-    date: `2025-03-13T19:30:00-08:00`,
+    date: `2025-03-13T19:30:00-07:00`,
     product: 'web',
     body: `### Improvements
 - Updated Modrinth Servers marketing page, removing Pyro branding.`,
   },
   {
-    date: `2025-03-12T10:15:00-08:00`,
+    date: `2025-03-12T10:15:00-07:00`,
     product: 'web',
     body: `### Improvements
 - Fixed low-res icons being pixelated.
