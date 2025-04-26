c4c8f2d649804e5ea693b2882d027db4acf11043
Prospector <prospectordev@gmail.com>
2025-04-25 19:33:16 -0700

    Begin skins frontend

---
+++ apps/app-frontend/src/components/ui/skin/CapeButton.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { ButtonStyled, commonMessages } from '@modrinth/ui'
+import { EditIcon } from '@modrinth/assets'
+import { useVIntl } from '@vintl/vintl'
+import type { Cape, Skin } from '@/helpers/skins.ts'
+
+const { formatMessage } = useVIntl()
+
+const emit = defineEmits<{
+  (e: 'select'): void
+}>()
+
+const highlighted = computed(() => props.selected ?? props.cape.is_equipped)
+
+const props = withDefaults(
+  defineProps<{
+    cape: Cape
+    selected?: boolean
+  }>(),
+  {
+    selected: undefined,
+  },
+)
+</script>
+
+<template>
+  <button v-tooltip="cape.name" class="block border-0 m-0 p-0 bg-transparent group cursor-pointer" :aria-label="cape.name" @click="emit('select')">
+    <span
+      :class="
+        highlighted
+          ? `bg-brand highlighted-outer-glow`
+          : `bg-button-bg opacity-75 group-hover:opacity-100`
+      "
+      class="block p-[3px] rounded-lg border-0 group-active:scale-95 transition-all"
+    >
+      <span
+        class="block cursed-cape-shit rounded-[5px]"
+        :class="{ 'highlighted-inner-shadow': highlighted }"
+      >
+        <img :src="cape.texture" alt="" />
+      </span>
+    </span>
+  </button>
+</template>
+<style lang="scss" scoped>
+.cursed-cape-shit {
+  aspect-ratio: 10 / 16;
+  position: relative;
+  overflow: hidden;
+  box-sizing: content-box;
+}
+
+.cursed-cape-shit img {
+  position: absolute;
+  object-fit: cover;
+  image-rendering: pixelated;
+
+  // scales image up so that the target area of the texture (10x16) is 100% of the container
+  width: calc(64 / 10 * 100%);
+  height: calc(32 / 16 * 100%);
+
+  // offsets the image so that the target area is in the container
+  left: calc(1 / 10 * -100%);
+  top: calc(1 / 16 * -100%);
+
+  // scale the image up a little bit to avoid edges from the surrounding texture due to rounding
+  scale: 1.01;
+  transform-origin: calc(10 / 2 / 64 * 100%) calc(16 / 2 / 32 * 100%);
+}
+
+.highlighted-inner-shadow::before {
+  content: '';
+  position: absolute;
+  inset: 0;
+  box-shadow: inset 0 0 4px 4px rgba(0, 0, 0, 0.2);
+  z-index: 2;
+}
+
+@supports (background-color: color-mix(in srgb, transparent, transparent)) {
+  .highlighted-outer-glow {
+    box-shadow: 0 0 4px 2px color-mix(in srgb, var(--color-brand), transparent 70%);
+  }
+}
+</style>
---
+++ apps/app-frontend/src/components/ui/skin/EditSkinModal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import ModalWrapper from '@/components/ui/modal/ModalWrapper.vue'
+import { computed, useTemplateRef } from 'vue'
+
+const modal = useTemplateRef('modal')
+
+const previewSkin = computed(() => `https://vzge.me/full/350/ProspectorDev.png?no=ears&y=180`)
+
+function show(e: MouseEvent) {
+  modal.value?.show(e)
+}
+
+function hide() {
+  modal.value?.hide()
+}
+
+defineExpose({
+  show,
+  hide,
+})
+</script>
+<template>
+  <ModalWrapper ref="modal">
+    <template #title>
+      <span class="text-lg font-extrabold text-contrast">Edit skin</span>
+    </template>
+    <div class="grid grid-cols-[auto_1fr] gap-6">
+      <div class="flex">
+        <img :src="previewSkin" alt="" class="w-auto my-auto h-60 object-contain" />
+      </div>
+    </div>
+  </ModalWrapper>
+</template>
