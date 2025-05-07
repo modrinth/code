95b9777ac82f29ed94a6a46090d67d09cbcf93f8
Prospector <prospectordev@gmail.com>
2025-05-07 13:44:53 -0700

    Adjust conflict modal ui

--- apps/frontend/src/components/ui/servers/FilesUploadConflictModal.vue
+++ apps/frontend/src/components/ui/servers/FilesUploadConflictModal.vue
@@ -7,13 +7,19 @@
     @proceed="proceed"
   >
     <div class="flex max-w-[30rem] flex-col gap-4">
-      <p class="m-0 font-bold">
-        The following files already exist on your server, and will be overwritten if you proceed
-        with extraction:
+      <p class="m-0 font-semibold leading-normal">
+        <template v-if="hasMany">
+          Over 100 files will be overwritten if you proceed with extraction; here is just some of
+          them:
+        </template>
+        <template v-else>
+          The following {{ files.length }} files already exist on your server, and will be
+          overwritten if you proceed with extraction:
+        </template>
       </p>
-      <ul class="m-0 grid list-none grid-cols-1 rounded-2xl bg-bg px-4 py-3 sm:grid-cols-2">
+      <ul class="m-0 max-h-80 list-none overflow-auto rounded-2xl bg-bg px-4 py-3">
         <li v-for="file in files" :key="file" class="flex items-center gap-1 py-1 font-medium">
-          <XIcon class="text-red" /> {{ file }}
+          <XIcon class="shrink-0 text-red" /> {{ file }}
         </li>
       </ul>
     </div>
@@ -33,6 +39,8 @@ const emit = defineEmits<{
 }>();
 
 const modal = ref<typeof ConfirmModal>();
+
+const hasMany = computed(() => files.value.length > 100);
 
 const show = (zipPath: string, conflictingFiles: string[]) => {
   path.value = zipPath;
