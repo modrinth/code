b5922b0cfb2f78d825620b3f93ccf7f46efa0b20
Calum H. <hendersoncal117@gmail.com>
2025-06-06 20:21:46 +0100

    fix: flag+region clarifications

--- packages/ui/src/utils/regions.ts
+++ packages/ui/src/utils/regions.ts
@@ -2,12 +2,16 @@ import { defineMessage, type MessageDescriptor } from '@vintl/vintl'
 
 export const regionOverrides = {
   'us-vin': {
-    name: defineMessage({ id: 'servers.region.north-america', defaultMessage: 'North America' }),
+    name: defineMessage({ id: 'servers.region.usa', defaultMessage: 'United States of America' }),
     flag: 'https://flagcdn.com/us.svg',
   },
   'eu-lim': {
-    name: defineMessage({ id: 'servers.region.europe', defaultMessage: 'Europe' }),
+    name: defineMessage({ id: 'servers.region.european-union', defaultMessage: 'European Union' }),
     flag: 'https://flagcdn.com/eu.svg',
+  },
+  'kyros-test': {
+    name: defineMessage({ id: 'servers.region.test', defaultMessage: 'Antarctica' }),
+    flag: 'https://flagcdn.com/aq.svg',
   },
   'de-fra': {
     name: defineMessage({ id: 'servers.region.europe', defaultMessage: 'Europe' }),
