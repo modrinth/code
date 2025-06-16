ba33c4705bc9f9edfc0dbba6d03b5f793f06817d
Calum H. <hendersoncal117@gmail.com>
2025-06-16 18:14:48 +0100

    fix: idiot

--- packages/ui/src/components/base/ErrorInformationCard.vue
+++ packages/ui/src/components/base/ErrorInformationCard.vue
@@ -77,7 +77,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { ButtonStyled } from './ButtonStyled.vue'
+import ButtonStyled from './ButtonStyled.vue'
 import { CopyIcon, CheckIcon } from '@modrinth/assets'
 import type { Component } from 'vue'
 
