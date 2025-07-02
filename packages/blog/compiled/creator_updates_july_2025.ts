0d8f6608a8166ed22780967da9900bae90cab69b
Prospector <prospectordev@gmail.com>
2025-07-02 12:43:31 -0700

    pnpm fix

--- packages/blog/compiled/creator_updates_july_2025.ts
+++ packages/blog/compiled/creator_updates_july_2025.ts
@@ -3,7 +3,7 @@ export const article = {
   html: () => import(`./creator_updates_july_2025.content`).then((m) => m.html),
   title: 'Creator Updates, July 2025',
   summary: 'Addressing recent growth and growing pains that have been affecting creators.',
-  date: '2025-07-02T03:00:00.000Z',
+  date: '2025-07-02T04:20:00.000Z',
   slug: 'creator-updates-july-2025',
   thumbnail: false,
 }
