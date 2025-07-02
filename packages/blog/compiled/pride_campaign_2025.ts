b8dad960d6038135851f6701c50ab8b10ec1fa42
Prospector <prospectordev@gmail.com>
2025-07-01 18:20:32 -0700

    re-run fix

--- packages/blog/compiled/pride_campaign_2025.ts
+++ packages/blog/compiled/pride_campaign_2025.ts
@@ -1,6 +1,6 @@
 // AUTO-GENERATED FILE - DO NOT EDIT
 export const article = {
-  html: () => import('./pride_campaign_2025.content').then((m) => m.html),
+  html: () => import(`./pride_campaign_2025.content`).then((m) => m.html),
   title: 'A Pride Month Success: Over $8,400 Raised for The Trevor Project!',
   summary:
     'A reflection on our Pride Month fundraiser campaign, which raised thousands for LGBTQ+ youth.',
