91cc39b7055d4cb276c11e831579f8e04faee9af
Josiah Glosson <soujournme@gmail.com>
2025-07-03 13:43:13 -0500

    Add in the buttons and body

--- apps/app-frontend/src/components/ui/UpdateModal.vue
+++ apps/app-frontend/src/components/ui/UpdateModal.vue
@@ -1,32 +1,76 @@
 <template>
   <ModalWrapper
     ref="modal"
-    :header="header"
+    :header="formatMessage(messages.header)"
   >
+    <div>{{ formatMessage(messages.body, { version: update!.version }) }}</div>
+    <div class="mt-4 flex flex-wrap gap-2">
+      <ButtonStyled color="green">
+        <button>
+          <RefreshCwIcon />
+          {{ formatMessage(messages.restartNow) }}
+        </button>
+      </ButtonStyled>
+      <ButtonStyled>
+        <button @click="hide()">
+          {{ formatMessage(messages.later) }}
+        </button>
+      </ButtonStyled>
+      <ButtonStyled>
+        <button>
+          {{ formatMessage(messages.skip) }}
+        </button>
+      </ButtonStyled>
+    </div>
   </ModalWrapper>
 </template>
 
 <script setup lang="ts">
 import ModalWrapper from "@/components/ui/modal/ModalWrapper.vue";
-import {defineMessage, useVIntl} from "@vintl/vintl";
-import {computed, useTemplateRef, ref} from 'vue';
+import {defineMessages, useVIntl} from "@vintl/vintl";
+import {useTemplateRef, ref} from 'vue';
+import {Update} from "@tauri-apps/plugin-updater";
+import {ButtonStyled} from "@modrinth/ui";
+import {RefreshCwIcon} from "@modrinth/assets";
 
 const {formatMessage} = useVIntl();
 
-const headerMessage = defineMessage({
-  id: 'app.update.modal-header',
-  defaultMessage: `An update is available!`,
+const messages = defineMessages({
+  header: {
+    id: 'app.update.modal-header',
+    defaultMessage: 'An update is available!',
+  },
+  body: {
+    id: 'app.update.modal-body',
+    defaultMessage: 'Version {version} of the Modrinth App is available for installation.',
+  },
+  restartNow: {
+    id: 'app.update.restart',
+    defaultMessage: 'Restart Now',
+  },
+  later: {
+    id: 'app.update.later',
+    defaultMessage: 'Later',
+  },
+  skip: {
+    id: 'app.update.skip',
+    defaultMessage: 'Skip This Update',
+  },
 })
-const header = computed(() => formatMessage(headerMessage))
+
+const update = ref<Update>();
 
 const modal = useTemplateRef('modal')
 const isOpen = ref(false)
-function show() {
-  modal.value.show()
+
+function show(newUpdate: Update) {
+  update.value = newUpdate;
+  modal.value!.show()
   isOpen.value = true
 }
+
 function hide() {
-  modal.value.hide()
+  modal.value!.hide()
   isOpen.value = false
 }
 
