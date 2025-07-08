deae7b54d19babb7fc0fb41d2a2899ff5d98d9fe
Emma Alexia <emma@modrinth.com>
2025-07-08 18:15:26 -0400

    change order

--- apps/frontend/src/composables/servers/modules/general.ts
+++ apps/frontend/src/composables/servers/modules/general.ts
@@ -6,6 +6,7 @@ import { ServerModule } from "./base.ts";
 export class GeneralModule extends ServerModule implements ServerGeneral {
   server_id!: string;
   name!: string;
+  owner_id!: string;
   net!: { ip: string; port: number; domain: string };
   game!: string;
   backup_quota!: number;
@@ -31,7 +32,6 @@ export class GeneralModule extends ServerModule implements ServerGeneral {
   notices?: any[];
   node!: { token: string; instance: string };
   flows?: { intro?: boolean };
-  owner_id!: string;
 
   async fetch(): Promise<void> {
     const data = await useServersFetch<ServerGeneral>(`servers/${this.serverId}`, {}, "general");
