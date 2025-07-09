5d17663040bf8ad36206db54ba9f63a92b3163d4
Prospector <6166773+Prospector@users.noreply.github.com>
2025-07-09 16:11:17 -0700

    Don't unnecessarily markdown-ify links pasted into markdown editor (#3958)

--- packages/ui/src/components/base/MarkdownEditor.vue
+++ packages/ui/src/components/base/MarkdownEditor.vue
@@ -397,9 +397,10 @@ onMounted(() => {
 
         const selection = view.state.selection.main
         const selectionText = view.state.doc.sliceString(selection.from, selection.to)
-        const linkText = selectionText ? selectionText : url
-        const linkMarkdown = `[${linkText}](${url})`
-        return markdownCommands.replaceSelection(view, linkMarkdown)
+        if (selectionText) {
+          const linkMarkdown = `[${selectionText}](${url})`
+          return markdownCommands.replaceSelection(view, linkMarkdown)
+        }
       }
 
       // Check if the length of the document is greater than the max length. If it is, prevent the paste.
