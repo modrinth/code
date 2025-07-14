69141a7e56cd4f134a33accf7c19773cf54fe8ef
Calum <calum@modrinth.com>
2025-07-14 16:40:37 +0100

    feat: start

--- apps/frontend/src/pages/moderation/index.vue
+++ apps/frontend/src/pages/moderation/index.vue
@@ -1,42 +1,8 @@
-<template>
-  <div>
-    <section class="universal-card">
-      <h2>Statistics</h2>
-      <div class="grid-display">
-        <div class="grid-display__item">
-          <div class="label">Projects</div>
-          <div class="value">
-            {{ formatNumber(stats.projects, false) }}
-          </div>
-        </div>
-        <div class="grid-display__item">
-          <div class="label">Versions</div>
-          <div class="value">
-            {{ formatNumber(stats.versions, false) }}
-          </div>
-        </div>
-        <div class="grid-display__item">
-          <div class="label">Files</div>
-          <div class="value">
-            {{ formatNumber(stats.files, false) }}
-          </div>
-        </div>
-        <div class="grid-display__item">
-          <div class="label">Authors</div>
-          <div class="value">
-            {{ formatNumber(stats.authors, false) }}
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-</template>
-<script setup>
-import { formatNumber } from "@modrinth/utils";
+<template> <NavRow :links="moderationLinks" /> </template>
 
-useHead({
-  title: "Staff overview - Modrinth",
-});
-
-const { data: stats } = await useAsyncData("statistics", () => useBaseFetch("statistics"));
+<script lang="ts" setup>
+ import { defineMessages, useVintl } from "@vintl/vintl"; import NavRow from
+"@/components/ui/NavRow.vue"; const { formatMessage } = useVintl(); const messages =
+defineMessages({}); const moderationLinks = [{ label: }];
 </script>
+
