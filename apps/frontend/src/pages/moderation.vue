8a36bdc0c7a58008b358ea356465d3b470460285
IMB11 <calum@modrinth.com>
2025-07-14 17:51:05 +0100

    feat: header controls for review page

--- apps/frontend/src/pages/moderation.vue
+++ apps/frontend/src/pages/moderation.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="normal-page">
-    <div class="normal-page__sidebar">
-      <aside class="universal-card">
-        <h1>Moderation</h1>
-        <NavStack>
-          <NavStackItem link="/moderation" label="Overview">
-            <ModrinthIcon aria-hidden="true" />
-          </NavStackItem>
-          <NavStackItem link="/moderation/review" label="Review projects">
-            <ScaleIcon aria-hidden="true" />
-          </NavStackItem>
-          <NavStackItem link="/moderation/reports" label="Reports">
-            <ReportIcon aria-hidden="true" />
-          </NavStackItem>
-        </NavStack>
-      </aside>
-    </div>
-    <div class="normal-page__content">
-      <NuxtPage />
-    </div>
+  <div
+    class="experimental-styles-within relative mx-auto mb-6 flex min-h-screen w-full max-w-[1280px] flex-col px-6"
+  >
+    <h1>Moderation</h1>
+    <NavTabs :links="moderationLinks" class="mb-4" />
+    <NuxtPage />
   </div>
 </template>
 
-<script setup>
-import { ModrinthIcon, ScaleIcon, ReportIcon } from "@modrinth/assets";
-import NavStack from "~/components/ui/NavStack.vue";
-import NavStackItem from "~/components/ui/NavStackItem.vue";
+<script setup lang="ts">
+import { defineMessages, useVIntl } from "@vintl/vintl";
+import NavTabs from "@/components/ui/NavTabs.vue";
 
 definePageMeta({
   middleware: "auth",
 });
+
+const { formatMessage } = useVIntl();
+const auth = await useAuth();
+
+const messages = defineMessages({
+  projectsTitle: {
+    id: "moderation.page.projects",
+    defaultMessage: "Projects",
+  },
+  technicalReviewTitle: {
+    id: "moderation.page.technicalReview",
+    defaultMessage: "Technical Review",
+  },
+  reportsTitle: {
+    id: "moderation.page.reports",
+    defaultMessage: "Reports",
+  },
+});
+
+const moderationLinks = [
+  { label: formatMessage(messages.projectsTitle), href: "/moderation" },
+  { label: formatMessage(messages.technicalReviewTitle), href: "/moderation/technical-review" },
+  { label: formatMessage(messages.reportsTitle), href: "/moderation/reports" },
+];
 </script>
--- apps/frontend/src/pages/moderation/index.vue
+++ apps/frontend/src/pages/moderation/index.vue
@@ -1,8 +1,78 @@
-<template> <NavRow :links="moderationLinks" /> </template>
+<template>
+  <div class="flex flex-row justify-between gap-3">
+    <div class="iconified-input">
+      <SearchIcon aria-hidden="true" class="text-lg" />
+      <input
+        v-model="query"
+        class="h-[40px]"
+        autocomplete="off"
+        spellcheck="false"
+        type="text"
+        :placeholder="`Search...`"
+        @input="updateSearchResults()"
+      />
+      <Button v-if="query" class="r-btn" @click="() => (query = '')">
+        <XIcon />
+      </Button>
+    </div>
+    <div class="flex flex-row justify-end gap-2">
+      <DropdownSelect
+        v-slot="{ selected }"
+        v-model="currentFilterType"
+        class="!w-[250px] flex-grow md:flex-grow-0"
+        name="Filter by"
+        :options="filterTypes as unknown[]"
+        @change="updateSearchResults()"
+      >
+        <span class="flex flex-row gap-2 align-middle font-semibold text-secondary"
+          ><FilterIcon class="size-4" />{{ selected }}</span
+        >
+      </DropdownSelect>
+      <DropdownSelect
+        v-slot="{ selected }"
+        v-model="currentSortType"
+        class="!w-[150px] flex-grow md:flex-grow-0"
+        name="Sort by"
+        :options="sortTypes as unknown[]"
+        @change="updateSearchResults()"
+      >
+        <span class="flex flex-row gap-2 align-middle font-semibold text-secondary"
+          ><SortAscIcon class="size-4" v-if="selected === 'Oldest'" />
+          <SortDescIcon class="size-4" v-else />{{ selected }}</span
+        >
+      </DropdownSelect>
+      <ButtonStyled color="orange">
+        <button class="!h-[40px]"><ScaleIcon class="size-4" /> Moderate</button>
+      </ButtonStyled>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { DropdownSelect, Button, ButtonStyled } from "@modrinth/ui";
+import {
+  XIcon,
+  SearchIcon,
+  SortAscIcon,
+  SortDescIcon,
+  FilterIcon,
+  ScaleIcon,
+} from "@modrinth/assets";
+import { useLocalStorage } from "@vueuse/core";
 
-<script lang="ts" setup>
- import { defineMessages, useVintl } from "@vintl/vintl"; import NavRow from
-"@/components/ui/NavRow.vue"; const { formatMessage } = useVintl(); const messages =
-defineMessages({}); const moderationLinks = [{ label: }];
+const query = useLocalStorage("moderation-query", "");
+const currentFilterType = useLocalStorage("moderation-current-filter-type", () => "All");
+const filterTypes: readonly string[] = readonly([
+  "All projects",
+  "Modpacks",
+  "Mods",
+  "Resource Packs",
+  "Data Packs",
+  "Plugins",
+  "Shaders",
+]);
+
+const currentSortType = useLocalStorage("moderation-current-sort-type", () => "Oldest");
+const sortTypes: readonly string[] = readonly(["Oldest", "Newest"]);
+
+function updateSearchResults() {}
 </script>
-
