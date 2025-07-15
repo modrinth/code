e15fa35bad471836377b7ab34c0c83830dec9cbc
Prospector <prospectordev@gmail.com>
2025-07-15 08:15:15 -0700

    Update changelog

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,6 +11,12 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
   {
+    date: `2025-07-15T08:20:00-07:00`,
+    product: 'web',
+    body: `### Improvements
+- Fixed gallery images from overflowing with long words in their descriptions.`,
+  },
+  {
     date: `2025-07-09T22:15:00-07:00`,
     product: 'web',
     body: `### Improvements
