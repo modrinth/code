9559b182d653f113d91bae80fbd69175a8109e85
Calum H. <hendersoncal117@gmail.com>
2025-07-25 16:32:35 +0100

    fix: remove test qr

--- packages/moderation/data/report-quick-replies.ts
+++ packages/moderation/data/report-quick-replies.ts
@@ -1,10 +1,10 @@
 import type { ReportQuickReply } from '../types/reports'
 
 export default [
-  {
-    label: 'Test Quick Reply',
-    message: 'This is a test quick reply message.',
-    shouldShow: (report) => report.project?.project_type === 'mod',
-    private: false,
-  },
+  // {
+  //   label: 'Test Quick Reply',
+  //   message: 'This is a test quick reply message.',
+  //   shouldShow: (report) => report.project?.project_type === 'mod',
+  //   private: false,
+  // },
 ] as ReadonlyArray<ReportQuickReply>
