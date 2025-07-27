8db9b6dc5f156fe1d605bbbbd2ffd368c4f1a2ec
IMB11 <calum@modrinth.com>
2025-07-27 21:14:43 +0100

    fix: top level await

--- packages/moderation/data/report-quick-replies.ts
+++ packages/moderation/data/report-quick-replies.ts
@@ -3,32 +3,32 @@ import type { ReportQuickReply } from '../types/reports'
 export default [
   {
     label: 'Antivirus',
-    message: (await import('./messages/reports/antivirus.md?raw')).default,
+    message: async () => (await import('./messages/reports/antivirus.md?raw')).default,
     private: false,
   },
   {
     label: 'Spam',
-    message: (await import('./messages/reports/spam.md?raw')).default,
+    message: async () => (await import('./messages/reports/spam.md?raw')).default,
     private: false,
   },
   {
     label: 'Gameplay Issue',
-    message: (await import('./messages/reports/gameplay-issue.md?raw')).default,
+    message: async () => (await import('./messages/reports/gameplay-issue.md?raw')).default,
     private: false,
   },
   {
     label: 'Platform Issue',
-    message: (await import('./messages/reports/platform-issue.md?raw')).default,
+    message: async () => (await import('./messages/reports/platform-issue.md?raw')).default,
     private: false,
   },
   {
     label: 'Stale',
-    message: (await import('./messages/reports/stale.md?raw')).default,
+    message: async () => (await import('./messages/reports/stale.md?raw')).default,
     private: false,
   },
   {
     label: 'Confirmed Malware',
-    message: (await import('./messages/reports/confirmed-malware.md?raw')).default,
+    message: async () => (await import('./messages/reports/confirmed-malware.md?raw')).default,
     private: false,
   },
 ] as ReadonlyArray<ReportQuickReply>
