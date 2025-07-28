175b90be5a42e5bfd3289ffdfbf7b201404f82a8
Josiah Glosson <soujournme@gmail.com>
2025-07-28 07:44:34 -0700

    Legacy ping support (#4062)
    * Detection of protocol versions before 18w47b
    
    * Refactor old_protocol_versions into protocol_version
    
    * Ping servers closer to how a client of an instance's version would ping a server
    
    * Allow pinging legacy servers from a modern profile in the same way a modern client would
    
    * Ping 1.4.2 through 1.5.2 like a Vanilla client in those versions would when in such an instance

--- apps/app-frontend/src/pages/instance/Worlds.vue
+++ apps/app-frontend/src/pages/instance/Worlds.vue
@@ -134,6 +134,7 @@ import {
 } from '@modrinth/ui'
 import { PlusIcon, SpinnerIcon, UpdatedIcon, SearchIcon, XIcon } from '@modrinth/assets'
 import {
+  type ProtocolVersion,
   type SingleplayerWorld,
   type World,
   type ServerWorld,
@@ -210,7 +211,9 @@ const worldPlaying = ref<World>()
 const worlds = ref<World[]>([])
 const serverData = ref<Record<string, ServerData>>({})
 
-const protocolVersion = ref<number | null>(await get_profile_protocol_version(instance.value.path))
+const protocolVersion = ref<ProtocolVersion | null>(
+  await get_profile_protocol_version(instance.value.path),
+)
 
 const unlistenProfile = await profile_listener(async (e: ProfileEvent) => {
   if (e.profile_path_id !== instance.value.path) return
@@ -246,7 +249,7 @@ async function refreshAllWorlds() {
   worlds.value = await refreshWorlds(instance.value.path).finally(
     () => (refreshingAll.value = false),
   )
-  await refreshServers(worlds.value, serverData.value, protocolVersion.value)
+  refreshServers(worlds.value, serverData.value, protocolVersion.value)
 
   const hasNoWorlds = worlds.value.length === 0
 
