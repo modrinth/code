73d18cc4f9c619523a022fe77313115e98826931
Calum H. (IMB11) <hendersoncal117@gmail.com>
2025-08-01 14:10:30 +0100

    refactor: migrate to common eslint+prettier configs

---
+++ packages/tooling-config/prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 100,  
+  semi: false,  
+  singleQuote: true,  
+  endOfLine: 'auto'  
+};
+
+module.exports = config;
