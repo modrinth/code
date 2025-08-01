8e51abe4460d1903a9c268a48e2f65f8aea2c8b0
Calum H. (IMB11) <hendersoncal117@gmail.com>
2025-08-01 15:15:42 +0100

    chore: fix util lint issues

--- packages/tooling-config/eslint/base.mjs
+++ packages/tooling-config/eslint/base.mjs
@@ -1,12 +1,12 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettierEslint from 'eslint-plugin-prettier/recommended'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   prettierEslint,
+  ...common,
   {
     languageOptions: {
       parserOptions: {
@@ -16,25 +16,5 @@ export default tseslint.config(
   },
   {
     ignores: ['node_modules/', 'dist/', 'build/'],
-  },
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-type-alias': [
-        'error',
-        {
-          allowGenerics: 'always',
-        },
-      ],
-      '@typescript-eslint/ban-ts-comment': 'off',
-    },
   },
 )
