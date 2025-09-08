8d3943e2e06721de75c93c7f6f07dc2dbbd10fdb
fetch <fetch@ferrous.ch>
2025-09-08 16:43:44 +0100

    Remove old fixtures

--- apps/labrinth/fixtures/sample-pat.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO pats
-(id, name, user_id, access_token, scopes, created, expires, last_used)
-VALUES (RANDOM(100000000,10000000000), 'Sample PAT', (SELECT id FROM users WHERE username = 'Ghost'), 'mrp_abcdefg', 9223372036854775807, NOW(), NOW() + INTERVAL '1 year', NOW());
--- apps/labrinth/fixtures/setup-notifications.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO notifications_types (name, delivery_priority, always_enabled) VALUES ('team_invite', 3, FALSE);
-
-INSERT INTO users_notifications_preferences (id, user_id, channel, notification_type, enabled)
-VALUES (RANDOM(1000000000, 10000000000), NULL, 'email', 'team_invite', FALSE);
-
-INSERT INTO notifications_templates
-(id, channel, notification_type, subject_line, body_fetch_url, plaintext_fallback)
-VALUES (RANDOM(10000000, 100000000), 'email', 'team_invite', 'Invited to a team', 'https://modrinth.com/mail/teaminvite.html', 'Hello {user.name}. You have been invited to {teaminvite.project.name} for the role of {teaminvite.role.name} by {teaminvite.inviter.name}.');
