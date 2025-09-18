ae8fca59aa562b452fe3155a015831bd939cdde9
IMB11 <contact@cal.engineer>
2025-09-18 12:12:19 +0100

    feat: wording

--- apps/frontend/src/emails/templates/account/SubscriptionTaxChange.vue
+++ apps/frontend/src/emails/templates/account/SubscriptionTaxChange.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Column, Heading, Link as VLink, Row, Section, Text } from '@vue-email/components'
+import { Heading, Text, Link as VLink } from '@vue-email/components'
 
 import StyledEmail from '@/emails/shared/StyledEmail.vue'
 </script>
@@ -16,35 +16,13 @@ import StyledEmail from '@/emails/shared/StyledEmail.vue'
 		<Text class="text-muted text-base">Hi {user.name},</Text>
 		<Text class="text-muted text-base">
 			We're writing to let you know that taxes for your {taxnotification.service} subscription will
-			affect your upcoming {taxnotification.billing_interval} billing cycle. This changes your price
-			starting on {taxnotification.due}.
+			affect your upcoming {taxnotification.billing_interval} billing cycle.
 		</Text>
 
-		<Section class="bg-bg-super rounded-lg border border-divider pb-4 pl-4 pr-4 pt-4">
-			<Row>
-				<Column class="w-full">
-					<Row class="mb-2 mt-0">
-						<Column>
-							<Text class="m-0 text-sm font-semibold text-secondary">Old price</Text>
-							<Text class="m-0 text-base"><b>{taxnotification.old_total_amount}</b></Text>
-							<Text class="m-0 text-xs text-secondary">
-								includes {taxnotification.old_amount} + {taxnotification.old_tax_amount} tax
-							</Text>
-						</Column>
-					</Row>
-
-					<Row>
-						<Column>
-							<Text class="m-0 text-sm font-semibold text-secondary">New price</Text>
-							<Text class="m-0 text-base"><b>{taxnotification.new_total_amount}</b></Text>
-							<Text class="m-0 text-xs text-secondary">
-								includes {taxnotification.new_amount} + {taxnotification.new_tax_amount} tax
-							</Text>
-						</Column>
-					</Row>
-				</Column>
-			</Row>
-		</Section>
+		<Text class="text-muted text-base">
+			Previously, you were paying {taxnotification.old_tax_amount} in tax. Your next charge will
+			include {taxnotification.new_tax_amount} in tax starting on {taxnotification.due}.
+		</Text>
 
 		<Text class="text-muted text-base">
 			No action is required on your part. This change will automatically take effect on
