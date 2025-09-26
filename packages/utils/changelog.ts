7fa8d500f4b6d94e236d43b2f70d7719e256ae2c
Alejandro González <me@alegon.dev>
2025-09-26 12:49:42 +0200

    changelog: update for re-enabling organization creation

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,6 +11,12 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
 	{
+		date: `2025-09-26T13:00:00+02:00`,
+		product: 'web',
+		body: `### Improvements
+- Re-enabled the creation of Organizations.`,
+	},
+	{
 		date: `2025-09-25T19:15:00-07:00`,
 		product: 'web',
 		body: `### Improvements
