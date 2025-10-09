a21911497b8f62f15ddcf6a2e9ea867190ce9d75
Josiah Glosson <soujournme@gmail.com>
2025-10-09 13:13:43 -0500

    Fix some prettier things

--- apps/labrinth/migrations/20250914190749_affiliate_codes.sql
+++ apps/labrinth/migrations/20250914190749_affiliate_codes.sql
@@ -1,9 +1,9 @@
 CREATE TABLE affiliate_codes (
-	id BIGINT PRIMARY KEY,
-	created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	created_by BIGINT NOT NULL REFERENCES users (id),
-	affiliate BIGINT NOT NULL REFERENCES users (id),
+	id            bigint      PRIMARY KEY,
+	created_at    timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	created_by    bigint      NOT NULL REFERENCES users(id),
+	affiliate     bigint      NOT NULL REFERENCES users(id),
 	-- left nullable so we can explicitly set payouts if we need to,
 	-- and use a global default if unset
-	revenue_split FLOAT
+	revenue_split float
 );
