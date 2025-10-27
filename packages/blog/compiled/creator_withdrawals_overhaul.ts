2a0f180648bc3abcfd4736a068a0498927586db1
Calum H. (IMB11) <contact@cal.engineer>
2025-10-27 22:20:16 +0000

    fix: lint

--- packages/blog/compiled/creator_withdrawals_overhaul.ts
+++ packages/blog/compiled/creator_withdrawals_overhaul.ts
@@ -1,11 +1,11 @@
 // AUTO-GENERATED FILE - DO NOT EDIT
 export const article = {
   html: () => import(`./creator_withdrawals_overhaul.content`).then(m => m.html),
-  title: "Creator Withdrawals Overhaul",
-  summary: "A new and improved revenue dashboard and withdrawal process, with brand new payout methods.",
+  title: "More Ways to Withdraw",
+  summary: "Coming soon: new withdraw options and a redesigned revenue dashboard",
   date: "2025-10-27T21:38:54.000Z",
   slug: "creator-withdrawals-overhaul",
-  authors: ["AJfd8YH6","bOHH0P9Z"],
+  authors: ["AJfd8YH6","bOHH0P9Z","xSQqYYIN"],
   unlisted: false,
   thumbnail: true,
   
