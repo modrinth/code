8a177f8c75f96da2614c5ad4ce13fe68c736f1e5
Alejandro González <me@alegon.dev>
2025-10-30 21:45:53 +0100

    Address my own review comment

--- packages/muralpay/src/serde_iso3166.rs
+++ packages/muralpay/src/serde_iso3166.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, de::Error};
+use std::borrow::Cow;
+
 use rust_iso3166::CountryCode;
 
 pub fn serialize<S: serde::Serializer>(
@@ -10,27 +13,12 @@ pub fn serialize<S: serde::Serializer>(
 pub fn deserialize<'de, D: serde::Deserializer<'de>>(
     deserializer: D,
 ) -> Result<CountryCode, D::Error> {
-    struct Visitor;
-
-    impl serde::de::Visitor<'_> for Visitor {
-        type Value = CountryCode;
-
-        fn expecting(
-            &self,
-            formatter: &mut std::fmt::Formatter,
-        ) -> std::fmt::Result {
-            write!(formatter, "an ISO 3166 alpha-2 country code")
-        }
-
-        fn visit_str<E>(self, v: &str) -> std::result::Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            rust_iso3166::ALPHA2_MAP.get(v).copied().ok_or_else(|| {
-                E::custom("invalid ISO 3166 alpha-2 country code")
+    <Cow<'_, str>>::deserialize(deserializer).and_then(|country_code| {
+        rust_iso3166::ALPHA2_MAP
+            .get(&country_code)
+            .copied()
+            .ok_or_else(|| {
+                D::Error::custom("invalid ISO 3166 alpha-2 country code")
             })
-        }
-    }
-
-    deserializer.deserialize_str(Visitor)
+    })
 }
