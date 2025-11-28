f7c86f9fc94cdae05b4975542fe891f552a8e34b
Prospector <6166773+Prospector@users.noreply.github.com>
2025-11-28 11:39:32 -0800

    update changelog

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,6 +11,14 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
 	{
+		date: `2025-11-28T11:45:00-08:00`,
+		product: 'app',
+		version: '0.10.21',
+		body: `## Improvements
+- Install dependencies added in an update automatically.
+- Fixed auth server check.`,
+	},
+	{
 		date: `2025-11-19T15:15:00-08:00`,
 		product: 'app',
 		version: '0.10.20',
