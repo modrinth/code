12ac0bbf6d110778ed140d85e314c85d6f5b1df7
tdgao <mr.trumgao@gmail.com>
2025-11-29 10:51:15 -0800

    hide version and dependency relation when no project selected

--- apps/frontend/src/components/ui/create-project-version/stages/AddDependenciesStage.vue
+++ apps/frontend/src/components/ui/create-project-version/stages/AddDependenciesStage.vue
@@ -3,41 +3,47 @@
 		<div class="grid gap-6">
 			<div class="flex flex-col gap-4">
 				<span class="font-semibold text-contrast">Add dependency</span>
-				<div class="grid gap-2.5 rounded-2xl border border-solid border-surface-5 p-4">
+				<div class="grid gap-3 rounded-2xl border border-solid border-surface-5 p-4">
 					<div class="grid gap-2.5">
 						<span class="font-semibold text-contrast">Project <span class="text-red">*</span></span>
 						<ModSelect v-model="newDependencyId" />
 					</div>
 
-					<div class="grid gap-2.5">
-						<span class="font-semibold text-contrast"> Version </span>
-						<Combobox
-							v-model="version"
-							placeholder="Select project"
-							:options="[
-								{ label: '1.0.0', value: '1.0.0' },
-								{ label: '2.0.0', value: '2.0.0' },
-							]"
-							:searchable="true"
-						/>
-					</div>
+					<template v-if="newDependencyId">
+						<div class="grid gap-2.5">
+							<span class="font-semibold text-contrast"> Version </span>
+							<Combobox
+								v-model="version"
+								placeholder="Select project"
+								:options="[
+									{ label: '1.0.0', value: '1.0.0' },
+									{ label: '2.0.0', value: '2.0.0' },
+								]"
+								:searchable="true"
+							/>
+						</div>
 
-					<div class="grid gap-2.5">
-						<span class="font-semibold text-contrast"> Dependency relation </span>
-						<Combobox
-							v-model="newDependencyType"
-							placeholder="Select project"
-							:options="[
-								{ label: 'Required', value: 'required' },
-								{ label: 'Optional', value: 'optional' },
-								{ label: 'Incompatible', value: 'incompatible' },
-								{ label: 'Embedded', value: 'embedded' },
-							]"
-						/>
-					</div>
+						<div class="grid gap-2.5">
+							<span class="font-semibold text-contrast"> Dependency relation </span>
+							<Combobox
+								v-model="newDependencyType"
+								placeholder="Select project"
+								:options="[
+									{ label: 'Required', value: 'required' },
+									{ label: 'Optional', value: 'optional' },
+									{ label: 'Incompatible', value: 'incompatible' },
+									{ label: 'Embedded', value: 'embedded' },
+								]"
+							/>
+						</div>
+					</template>
 				</div>
 				<ButtonStyled>
-					<button class="self-start" @click="addDependency(newDependencyId, newDependencyType)">
+					<button
+						class="self-start"
+						:disabled="!newDependencyId"
+						@click="addDependency(newDependencyId, newDependencyType)"
+					>
 						Add Dependency
 					</button>
 				</ButtonStyled>
