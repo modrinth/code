99e6ca4de984783aa2be2ab9c0b16f9e31d6c0a7
Prospector <6166773+Prospector@users.noreply.github.com>
2025-12-08 10:48:35 -0800

    lint

--- packages/ui/src/components/page/SidebarCard.vue
+++ packages/ui/src/components/page/SidebarCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { injectPageContext } from '../../providers';
-
+import { injectPageContext } from '../../providers'
 
 const { hierarchicalSidebarAvailable } = injectPageContext()
 
@@ -9,7 +8,12 @@ defineProps<{
 }>()
 </script>
 <template>
-	<div class="flex flex-col gap-3  p-4" :class="{ 'card-shadow mb-4 last:mb-0 rounded-2xl bg-bg-raised': !hierarchicalSidebarAvailable}">
+	<div
+		class="flex flex-col gap-3 p-4"
+		:class="{
+			'card-shadow mb-4 last:mb-0 rounded-2xl bg-bg-raised': !hierarchicalSidebarAvailable,
+		}"
+	>
 		<span class="font-semibold">{{ title }}</span>
 		<slot />
 	</div>
