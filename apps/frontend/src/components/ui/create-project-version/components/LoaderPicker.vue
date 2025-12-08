658b03b80fd222b95502e212c59c27902c8e8197
tdgao <mr.trumgao@gmail.com>
2025-12-07 22:08:30 -0800

    move added loaded out of loader picker

--- apps/frontend/src/components/ui/create-project-version/components/LoaderPicker.vue
+++ apps/frontend/src/components/ui/create-project-version/components/LoaderPicker.vue
@@ -1,75 +1,40 @@
 <template>
-	<div class="space-y-6">
-		<div class="flex flex-col gap-2.5">
-			<span class="font-semibold text-contrast">Loaders <span class="text-red">*</span></span>
+	<div class="flex flex-col gap-2.5">
+		<span class="font-semibold text-contrast">Loaders <span class="text-red">*</span></span>
 
-			<Chips
-				v-model="loaderGroup"
-				:items="groupLabels"
-				:never-empty="false"
-				:capitalize="true"
-				size="small"
-			/>
+		<Chips
+			v-model="loaderGroup"
+			:items="groupLabels"
+			:never-empty="false"
+			:capitalize="true"
+			size="small"
+		/>
 
-			<div
-				class="flex flex-1 flex-col gap-4 overflow-y-auto rounded-xl border border-solid border-surface-5 p-3"
-			>
-				<div v-if="groupedLoaders[loaderGroup].length" class="flex flex-col gap-1.5">
-					<div class="flex flex-wrap gap-2">
-						<ButtonStyled
-							v-for="loader in groupedLoaders[loaderGroup]"
-							:key="`loader-${loader.name}`"
-							:color="selectedLoaders.includes(loader.name) ? 'green' : undefined"
-							:highlighted="selectedLoaders.includes(loader.name)"
-							type="chip"
-						>
-							<button
-								:style="`--_icon: var(--color-platform-${loader.name}); color: var(--color-platform-${loader.name})`"
-								@click="toggleLoader(loader.name)"
-							>
-								<div v-html="loader.icon"></div>
-								{{ formatCategory(loader.name) }}
-							</button>
-						</ButtonStyled>
-					</div>
-				</div>
-			</div>
-
-			<span>Select one or more loaders this version supports.</span>
-		</div>
-
-		<div v-if="selectedLoaders.length">
-			<div class="flex items-center justify-between">
-				<span class="font-semibold text-contrast"> Added loaders </span>
-				<ButtonStyled type="transparent" size="standard">
-					<button @click="onClearAll()">Clear all</button>
-				</ButtonStyled>
-			</div>
-			<div
-				class="flex flex-col gap-1.5 gap-y-4 rounded-xl border border-solid border-surface-5 p-3 py-4"
-			>
-				<span class="font-medium">Selected</span>
+		<div
+			class="flex flex-1 flex-col gap-4 overflow-y-auto rounded-xl border border-solid border-surface-5 p-3"
+		>
+			<div v-if="groupedLoaders[loaderGroup].length" class="flex flex-col gap-1.5">
 				<div class="flex flex-wrap gap-2">
-					<template v-if="selectedLoaders.length">
-						<template
-							v-for="loader in selectedLoaders.map((selectedLoader) =>
-								loaders.find((loader) => selectedLoader === loader.name),
-							)"
+					<ButtonStyled
+						v-for="loader in groupedLoaders[loaderGroup]"
+						:key="`loader-${loader.name}`"
+						:color="selectedLoaders.includes(loader.name) ? 'green' : undefined"
+						:highlighted="selectedLoaders.includes(loader.name)"
+						type="chip"
+					>
+						<button
+							:style="`--_icon: var(--color-platform-${loader.name}); color: var(--color-platform-${loader.name})`"
+							@click="toggleLoader(loader.name)"
 						>
-							<ButtonStyled v-if="loader" :key="`loader-${loader.name}`" type="chip">
-								<button
-									:style="`--_icon: var(--color-platform-${loader.name}); color: var(--color-platform-${loader.name})`"
-									@click="toggleLoader(loader.name)"
-								>
-									<div v-html="loader.icon"></div>
-									{{ formatCategory(loader.name) }}
-								</button>
-							</ButtonStyled>
-						</template>
-					</template>
+							<div v-html="loader.icon"></div>
+							{{ formatCategory(loader.name) }}
+						</button>
+					</ButtonStyled>
 				</div>
 			</div>
 		</div>
+
+		<span>Select one or more loaders this version supports.</span>
 	</div>
 </template>
 
@@ -94,8 +59,6 @@ const toggleLoader = (loader: string) => {
 		selectedLoaders.value = [...selectedLoaders.value, loader]
 	}
 }
-
-const onClearAll = () => (selectedLoaders.value = [])
 
 type GroupLabels = 'mods' | 'plugins' | 'packs' | 'shaders' | 'other'
 
--- apps/frontend/src/components/ui/create-project-version/stages/AddLoadersStage.vue
+++ apps/frontend/src/components/ui/create-project-version/stages/AddLoadersStage.vue
@@ -1,13 +1,60 @@
 <template>
-	<LoaderPicker v-model="draftVersion.loaders" :loaders="generatedState.loaders" />
+	<div class="space-y-6">
+		<LoaderPicker v-model="draftVersion.loaders" :loaders="generatedState.loaders" />
+
+		<div v-if="draftVersion.loaders.length">
+			<div class="flex items-center justify-between">
+				<span class="font-semibold text-contrast"> Added loaders </span>
+				<ButtonStyled type="transparent" size="standard">
+					<button @click="onClearAll()">Clear all</button>
+				</ButtonStyled>
+			</div>
+			<div
+				class="flex flex-col gap-1.5 gap-y-4 rounded-xl border border-solid border-surface-5 p-3 py-4"
+			>
+				<div class="flex flex-wrap gap-2">
+					<template
+						v-for="loader in draftVersion.loaders.map((loaderName) =>
+							loaders.find((loader) => loaderName === loader.name),
+						)"
+					>
+						<ButtonStyled v-if="loader" :key="`loader-${loader.name}`" type="chip">
+							<button
+								:style="`--_icon: var(--color-platform-${loader.name}); color: var(--color-platform-${loader.name})`"
+								@click="toggleLoader(loader.name)"
+							>
+								<div v-html="loader.icon"></div>
+								{{ formatCategory(loader.name) }}
+								<XIcon class="text-primary" />
+							</button>
+						</ButtonStyled>
+					</template>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
+import { XIcon } from '@modrinth/assets'
+import { ButtonStyled } from '@modrinth/ui'
+import { formatCategory } from '@modrinth/utils'
 import { useManageVersion } from '~/composables/versions/manage-version'
-
 import LoaderPicker from '../components/LoaderPicker.vue'
 
 const generatedState = useGeneratedState()
 
+const loaders = computed(() => generatedState.value.loaders)
+
 const { draftVersion } = useManageVersion()
+
+const toggleLoader = (loader: string) => {
+	if (draftVersion.value.loaders.includes(loader)) {
+		draftVersion.value.loaders = draftVersion.value.loaders.filter((l) => l !== loader)
+	} else {
+		draftVersion.value.loaders = [...draftVersion.value.loaders, loader]
+	}
+}
+
+const onClearAll = () => (draftVersion.value.loaders = [])
 </script>
