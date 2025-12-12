91accd5578b1e7bbb828285c33dbe83372d60fc5
Prospector <6166773+Prospector@users.noreply.github.com>
2025-12-11 16:19:51 -0800

    Update app changelog + fix accordion issue

--- packages/utils/changelog.ts
+++ packages/utils/changelog.ts
@@ -11,6 +11,15 @@ export type VersionEntry = {
 
 const VERSIONS: VersionEntry[] = [
 	{
+		date: `2025-12-11T17:00:00-08:00`,
+		product: 'app',
+		version: '0.10.22',
+		body: `## Improvements
+- Updated Modrinth Servers branding to new Modrinth Hosting branding.
+- Fixed server pinging blocking the app from loading.
+- Fixed instance overrides for window and Java settings not being able to be disabled.`,
+	},
+	{
 		date: `2025-12-11T16:15:00-08:00`,
 		product: 'hosting',
 		body: `## Improvements
