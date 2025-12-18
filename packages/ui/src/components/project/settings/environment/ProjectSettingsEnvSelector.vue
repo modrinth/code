8703d6bb52b491bcfbc7a8a4ca8b2b1f1c080258
tdgao <mr.trumgao@gmail.com>
2025-12-18 09:28:58 -0800

    fix types

--- packages/ui/src/components/project/settings/environment/ProjectSettingsEnvSelector.vue
+++ packages/ui/src/components/project/settings/environment/ProjectSettingsEnvSelector.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import type { EnvironmentV3 } from '@modrinth/utils'
 import { defineMessage, type MessageDescriptor, useVIntl } from '@vintl/vintl'
 import { computed, ref, watch } from 'vue'
 
+import type { Labrinth } from '@modrinth/api-client'
 import { commonProjectSettingsMessages } from '../../../../utils'
 import LargeRadioButton from '../../../base/LargeRadioButton.vue'
 
 const { formatMessage } = useVIntl()
 
-const value = defineModel<string | undefined>({ required: true })
+const value = defineModel<Labrinth.Projects.v3.Environment | undefined>({ required: true })
 
 withDefaults(
 	defineProps<{
@@ -135,7 +135,7 @@ type SubOptionKey = ValidKeys<(typeof OUTER_OPTIONS)[keyof typeof OUTER_OPTIONS]
 const currentOuterOption = ref<OuterOptionKey>()
 const currentSubOption = ref<SubOptionKey>()
 
-const computedOption = computed<string>(() => {
+const computedOption = computed<Labrinth.Projects.v3.Environment>(() => {
 	switch (currentOuterOption.value) {
 		case 'client':
 			return 'client_only'
@@ -170,7 +170,7 @@ const computedOption = computed<string>(() => {
 	}
 })
 
-function loadEnvironmentValues(env?: EnvironmentV3) {
+function loadEnvironmentValues(env?: Labrinth.Projects.v3.Environment) {
 	switch (env) {
 		case 'client_and_server':
 			currentOuterOption.value = 'client_and_server'
