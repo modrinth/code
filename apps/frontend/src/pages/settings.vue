56252074eeb856fd1064248f4d4e032d12c09027
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2025-12-23 01:24:58 +0100

    Add main dashboard title to make it consistent to settings

--- apps/frontend/src/pages/settings.vue
+++ apps/frontend/src/pages/settings.vue
@@ -1,85 +1,83 @@
 <template>
-	<div>
-		<div class="normal-page no-sidebar">
+	<div class="normal-page">
+		<div class="normal-page__header">
 			<h1>{{ formatMessage(commonMessages.settingsLabel) }}</h1>
 		</div>
-		<div class="normal-page">
-			<div class="normal-page__sidebar">
-				<NavStack
-					:items="
-						[
-							{ type: 'heading', label: 'Display' },
-							{
-								link: '/settings',
-								label: formatMessage(commonSettingsMessages.appearance),
-								icon: PaintbrushIcon,
-							},
-							isStaging
-								? {
-										link: '/settings/language',
-										label: formatMessage(commonSettingsMessages.language),
-										icon: LanguagesIcon,
-										badge: `${formatMessage(commonMessages.beta)}`,
-									}
-								: null,
-							auth.user ? { type: 'heading', label: 'Account' } : null,
-							auth.user
-								? {
-										link: '/settings/profile',
-										label: formatMessage(commonSettingsMessages.profile),
-										icon: UserIcon,
-									}
-								: null,
-							auth.user
-								? {
-										link: '/settings/account',
-										label: formatMessage(commonSettingsMessages.account),
-										icon: ShieldIcon,
-									}
-								: null,
-							auth.user
-								? {
-										link: '/settings/authorizations',
-										label: formatMessage(commonSettingsMessages.authorizedApps),
-										icon: GridIcon,
-									}
-								: null,
-							auth.user
-								? {
-										link: '/settings/sessions',
-										label: formatMessage(commonSettingsMessages.sessions),
-										icon: MonitorSmartphoneIcon,
-									}
-								: null,
-							auth.user
-								? {
-										link: '/settings/billing',
-										label: formatMessage(commonSettingsMessages.billing),
-										icon: CardIcon,
-									}
-								: null,
-							auth.user ? { type: 'heading', label: 'Developer' } : null,
-							auth.user
-								? {
-										link: '/settings/pats',
-										label: formatMessage(commonSettingsMessages.pats),
-										icon: KeyIcon,
-									}
-								: null,
-							auth.user
-								? {
-										link: '/settings/applications',
-										label: formatMessage(commonSettingsMessages.applications),
-										icon: ServerIcon,
-									}
-								: null,
-						].filter(Boolean)
-					"
-				/>
-			</div>
-			<div class="normal-page__content mt-3 lg:mt-0">
-				<NuxtPage :route="route" />
-			</div>
+		<div class="normal-page__sidebar">
+			<NavStack
+				:items="
+					[
+						{ type: 'heading', label: 'Display' },
+						{
+							link: '/settings',
+							label: formatMessage(commonSettingsMessages.appearance),
+							icon: PaintbrushIcon,
+						},
+						isStaging
+							? {
+									link: '/settings/language',
+									label: formatMessage(commonSettingsMessages.language),
+									icon: LanguagesIcon,
+									badge: `${formatMessage(commonMessages.beta)}`,
+								}
+							: null,
+						auth.user ? { type: 'heading', label: 'Account' } : null,
+						auth.user
+							? {
+									link: '/settings/profile',
+									label: formatMessage(commonSettingsMessages.profile),
+									icon: UserIcon,
+								}
+							: null,
+						auth.user
+							? {
+									link: '/settings/account',
+									label: formatMessage(commonSettingsMessages.account),
+									icon: ShieldIcon,
+								}
+							: null,
+						auth.user
+							? {
+									link: '/settings/authorizations',
+									label: formatMessage(commonSettingsMessages.authorizedApps),
+									icon: GridIcon,
+								}
+							: null,
+						auth.user
+							? {
+									link: '/settings/sessions',
+									label: formatMessage(commonSettingsMessages.sessions),
+									icon: MonitorSmartphoneIcon,
+								}
+							: null,
+						auth.user
+							? {
+									link: '/settings/billing',
+									label: formatMessage(commonSettingsMessages.billing),
+									icon: CardIcon,
+								}
+							: null,
+						auth.user ? { type: 'heading', label: 'Developer' } : null,
+						auth.user
+							? {
+									link: '/settings/pats',
+									label: formatMessage(commonSettingsMessages.pats),
+									icon: KeyIcon,
+								}
+							: null,
+						auth.user
+							? {
+									link: '/settings/applications',
+									label: formatMessage(commonSettingsMessages.applications),
+									icon: ServerIcon,
+								}
+							: null,
+					].filter(Boolean)
+				"
+			/>
+		</div>
+		<div class="normal-page__content mt-3 lg:mt-0">
+			<NuxtPage :route="route" />
 		</div>
 	</div>
 </template>
