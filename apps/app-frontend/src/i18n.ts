97a95cb97883804dd627527a734152e3daf634aa
Calum H. (IMB11) <contact@cal.engineer>
2025-12-26 18:31:11 +0000

    feat: consistency + dedupe impl of i18n

--- apps/app-frontend/src/i18n.ts
+++ apps/app-frontend/src/i18n.ts
@@ -1,37 +1,18 @@
-import IntlMessageFormat from 'intl-messageformat'
-import { type CompileError, createI18n, type MessageCompiler, type MessageContext } from 'vue-i18n'
+import { buildLocaleMessages, createMessageCompiler, type CrowdinMessages } from '@modrinth/ui'
+import { createI18n } from 'vue-i18n'
 
-const messageCompiler: MessageCompiler = (msg, { locale, key, onError }) => {
-	if (typeof msg === 'string') {
-		try {
-			const formatter = new IntlMessageFormat(msg, locale)
-			return (ctx: MessageContext) => {
-				try {
-					return formatter.format(ctx.values as Record<string, unknown>) as string
-				} catch {
-					return String(msg)
-				}
-			}
-		} catch (e) {
-			onError?.(e as CompileError)
-			return () => key
-		}
-	} else {
-		onError?.(new Error('AST not supported') as CompileError)
-		return () => key
-	}
-}
+const localeModules = import.meta.glob<{ default: CrowdinMessages }>('./locales/*/index.json', {
+	eager: true,
+})
 
 const i18n = createI18n({
 	legacy: false,
 	locale: 'en-US',
 	fallbackLocale: 'en-US',
-	messageCompiler,
+	messageCompiler: createMessageCompiler(),
 	missingWarn: false,
 	fallbackWarn: false,
-	messages: {
-		'en-US': {},
-	},
+	messages: buildLocaleMessages(localeModules),
 })
 
 export default i18n
