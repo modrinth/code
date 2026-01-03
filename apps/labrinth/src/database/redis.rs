4c3277bc53250348e5b0f36e1244141f713bb90e
François-X. T. <fetch@ferrous.ch>
2026-01-02 22:05:53 -0500

    Optional REDIS_WAIT_TIMEOUT_MS

--- apps/labrinth/src/database/redis.rs
+++ apps/labrinth/src/database/redis.rs
@@ -37,6 +37,14 @@ impl RedisPool {
     // testing pool uses a hashmap to mimic redis behaviour for very small data sizes (ie: tests)
     // PANICS: production pool will panic if redis url is not set
     pub fn new(meta_namespace: Option<String>) -> Self {
+        let wait_timeout =
+            dotenvy::var("REDIS_WAIT_TIMEOUT_MS").ok().map(|x| {
+                Duration::from_millis(
+                    x.parse::<u64>()
+                        .expect("REDIS_WAIT_TIMEOUT_MS must be a valid u64"),
+                )
+            });
+
         let url = dotenvy::var("REDIS_URL").expect("Redis URL not set");
         let pool = Config::from_url(url.clone())
             .builder()
@@ -47,6 +55,7 @@ impl RedisPool {
                     .and_then(|x| x.parse().ok())
                     .unwrap_or(10000),
             )
+            .wait_timeout(wait_timeout)
             .runtime(Runtime::Tokio1)
             .build()
             .expect("Redis connection failed");
