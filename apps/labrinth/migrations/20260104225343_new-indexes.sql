7b53c43ecd8877d9d717a9034736a05454450087
François-X. T. <fetch@ferrous.ch>
2026-01-04 17:59:58 -0500

    Add delphi report issue details index

--- apps/labrinth/migrations/20260104225343_new-indexes.sql
+++ apps/labrinth/migrations/20260104225343_new-indexes.sql
@@ -1,3 +1,7 @@
 -- Adding IF NOT EXISTS because at this time they are already on the production database.
 CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.collections_mods USING btree (mod_id);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.collections USING btree (user_id);
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.delphi_report_issue_details USING btree (
+	issue_id
+);
