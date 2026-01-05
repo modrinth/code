de7a7017a41063b82a055c3fe0193f15718e2f92
François-X. T. <fetch@ferrous.ch>
2026-01-04 19:02:48 -0500

    Remove CONCURRENTLY

--- apps/labrinth/migrations/20260104225343_new-indexes.sql
+++ apps/labrinth/migrations/20260104225343_new-indexes.sql
@@ -1,24 +1,24 @@
 -- Adding IF NOT EXISTS because at this time they are already on the production database.
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.collections_mods USING btree (mod_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.collections USING btree (user_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.mods_gallery USING btree (image_url);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.delphi_report_issue_details USING btree (
+CREATE INDEX IF NOT EXISTS ON public.collections_mods USING btree (mod_id);
+CREATE INDEX IF NOT EXISTS ON public.collections USING btree (user_id);
+CREATE INDEX IF NOT EXISTS ON public.mods_gallery USING btree (image_url);
+CREATE INDEX IF NOT EXISTS ON public.delphi_report_issue_details USING btree (
 	issue_id
 );
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.users USING btree (github_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.team_members USING btree (accepted);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.charges USING btree (parent_charge_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.charges USING btree (subscription_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.charges USING btree (user_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.versions USING btree (author_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.versions USING btree (status)
+CREATE INDEX IF NOT EXISTS ON public.users USING btree (github_id);
+CREATE INDEX IF NOT EXISTS ON public.team_members USING btree (accepted);
+CREATE INDEX IF NOT EXISTS ON public.charges USING btree (parent_charge_id);
+CREATE INDEX IF NOT EXISTS ON public.charges USING btree (subscription_id);
+CREATE INDEX IF NOT EXISTS ON public.charges USING btree (user_id);
+CREATE INDEX IF NOT EXISTS ON public.versions USING btree (author_id);
+CREATE INDEX IF NOT EXISTS ON public.versions USING btree (status)
 WHERE (requested_status IS NOT NULL);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.dependencies USING btree (mod_dependency_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.payouts_values USING btree (user_id, created);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.payouts_values USING btree (
+CREATE INDEX IF NOT EXISTS ON public.dependencies USING btree (mod_dependency_id);
+CREATE INDEX IF NOT EXISTS ON public.payouts_values USING btree (user_id, created);
+CREATE INDEX IF NOT EXISTS ON public.payouts_values USING btree (
 	user_id,
 	date_available
 );
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.threads_messages USING btree (author_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.threads_messages USING btree (thread_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS ON public.mod_follows USING btree (mod_id);
+CREATE INDEX IF NOT EXISTS ON public.threads_messages USING btree (author_id);
+CREATE INDEX IF NOT EXISTS ON public.threads_messages USING btree (thread_id);
+CREATE INDEX IF NOT EXISTS ON public.mod_follows USING btree (mod_id);
