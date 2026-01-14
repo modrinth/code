4e5d234701ddcac9ffd5c07cc48528e994b24ded
aecsocket <aecsocket@tutanota.com>
2026-01-14 15:25:48 +0000

    wip: server listing API

--- apps/labrinth/migrations/20260114130019_server_listing_projects.sql
+++ apps/labrinth/migrations/20260114130019_server_listing_projects.sql
@@ -1,2 +1,9 @@
 ALTER TABLE mods
 ADD COLUMN components JSONB NOT NULL DEFAULT '{}';
+
+CREATE TABLE minecraft_server_projects (
+	id              bigint PRIMARY KEY NOT NULL REFERENCES mods(id),
+	java_address    varchar(255) NOT NULL,
+	bedrock_address varchar(255) NOT NULL,
+	max_players     int
+);
