d324768208518ccb693ae404b575fdc199d3e464
Calum H. (IMB11) <contact@cal.engineer>
2026-01-16 09:47:13 +0000

    feat: base content card component

--- packages/ui/src/components/instances/index.ts
+++ packages/ui/src/components/instances/index.ts
@@ -1,18 +1,2 @@
-export { default as ContentCardItem } from './ContentCardItem.vue'
-export { default as ContentCardTable } from './ContentCardTable.vue'
-/**
- * @deprecated Use `ContentCardTable` with `ContentCardItem` instead.
- * This alias is kept for backwards compatibility and will be removed in a future version.
- */
-export { default as ContentCard } from './ContentCardItem.vue'
-export { default as ContentModpackCard } from './ContentModpackCard.vue'
-// export { default as ContentUpdaterModal } from './modals/ContentUpdaterModal.vue'
-export type {
-	ContentCardProject,
-	ContentCardTableItem,
-	ContentCardVersion,
-	ContentModpackCardCategory,
-	ContentModpackCardProject,
-	ContentModpackCardVersion,
-	ContentOwner,
-} from './types'
+export type { ContentCardOwner, ContentCardProject, ContentCardVersion } from './ContentCard.vue'
+export { default as ContentCard } from './ContentCard.vue'
