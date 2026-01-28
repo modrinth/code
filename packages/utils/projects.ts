c1176720e40b9d6d3a1063192d9e863fe0ed6a02
tdgao <mr.trumgao@gmail.com>
2026-01-28 14:21:51 -0700

    fix: weird snapshot format by only grouping consecutive tags with 3 or more

--- packages/utils/projects.ts
+++ packages/utils/projects.ts
@@ -65,6 +65,7 @@ export function formatVersionsForDisplay(
 	gameVersions: string[],
 	allGameVersions: GameVersionTag[],
 ) {
+	console.log('game versions', gameVersions)
 	const inputVersions = gameVersions.slice()
 	const allVersions = allGameVersions.slice()
 
@@ -126,12 +127,13 @@ export function formatVersionsForDisplay(
 
 	// show all snapshots if there's no release versions
 	if (releaseVersionsAsRanges.length === 0) {
-		const snapshotVersionsAsRanges = groupConsecutiveIndices(
-			inputVersions.filter((projVer) =>
-				allSnapshots.some((gameVer) => gameVer.version === projVer),
-			),
-			allSnapshots,
+		const snapshotVersions = inputVersions.filter((projVer) =>
+			allSnapshots.some((gameVer) => gameVer.version === projVer),
 		)
+		const snapshotVersionsAsRanges =
+			snapshotVersions.length > 3
+				? groupConsecutiveIndices(snapshotVersions, allSnapshots)
+				: snapshotVersions
 		output = [...snapshotVersionsAsRanges, ...output]
 	} else {
 		output = [...releaseVersionsAsRanges, ...output]
