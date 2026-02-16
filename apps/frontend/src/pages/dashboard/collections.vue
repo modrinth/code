6c294d1cd25db9296105ec0b25856f7e94874623
Calum H. <hendersoncal117@gmail.com>
2026-02-16 09:47:20 +0000

    Update apps/frontend/src/pages/dashboard/collections.vue
    Signed-off-by: Calum H. <hendersoncal117@gmail.com>

--- apps/frontend/src/pages/dashboard/collections.vue
+++ apps/frontend/src/pages/dashboard/collections.vue
@@ -204,13 +204,6 @@ const orderedCollections = computed(() => {
 				font-size: var(--font-size-sm);
 
 				word-break: break-word;
-				//additional stuff if we specifically want cutoff by elipsis
-				// overflow: hidden;
-				// text-overflow: ellipsis;
-				// line-clamp: 2;
-				// -webkit-line-clamp: 2;
-				// display: -webkit-box;
-				// -webkit-box-orient: vertical;
 			}
 
 			.stat-bar {
