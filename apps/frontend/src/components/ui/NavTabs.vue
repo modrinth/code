4ed4560ba836515487b7b1109969a4dec00dfb00
tdgao <mr.trumgao@gmail.com>
2026-02-28 10:25:56 -0700

    hide navtabs if theres only one tab

--- apps/frontend/src/components/ui/NavTabs.vue
+++ apps/frontend/src/components/ui/NavTabs.vue
@@ -1,6 +1,6 @@
 <template>
-	<!-- TODO_SERVER_PROJECTS, only show tabs if theres more than one in filteredLinks -->
 	<nav
+		v-if="filteredLinks.length > 1"
 		ref="scrollContainer"
 		class="experimental-styles-within relative flex w-fit overflow-x-auto rounded-full bg-bg-raised p-1 text-sm font-bold"
 		:class="{ 'card-shadow': mode === 'navigation' }"
