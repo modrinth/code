ea508eabc31b8967b33d259c421059c782774c88
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2026-02-28 22:17:26 -0800

    don't suggest adding a bedrock IP

--- packages/moderation/src/data/nags/server-projects.ts
+++ packages/moderation/src/data/nags/server-projects.ts
@@ -2,30 +2,7 @@ import { defineMessage } from '@modrinth/ui'
 import type { Nag, NagContext } from '../../types/nags'
 
 export const serverProjectsNags: Nag[] = [
-	/*
-	{
-		id: 'add-banner',
-		title: defineMessage({
-			id: 'nags.add-banner.title',
-			defaultMessage: 'Add a banner',
-		}),
-		description: defineMessage({
-			id: 'nags.add-banner.description',
-			defaultMessage:
-				"Add your server's banner.",
-		}),
-		status: 'suggestion',
-		shouldShow: (context: NagContext) => !!context.projectV3?.minecraft_server,
-		link: {
-			path: 'settings',
-			title: defineMessage({
-				id: 'nags.settings.title',
-				defaultMessage: 'Visit general settings',
-			}),
-			shouldShow: (context: NagContext) => context.currentRoute !== 'type-id-settings',
-		},
-	},
-*/
+
 	{
 		id: 'select-country',
 		title: defineMessage({
@@ -85,30 +62,6 @@ export const serverProjectsNags: Nag[] = [
 		status: 'required',
 		shouldShow: (context: NagContext) =>
 			!!context.projectV3?.minecraft_server && !context.projectV3?.minecraft_java_server?.address,
-		link: {
-			path: 'settings/server',
-			title: defineMessage({
-				id: 'nags.server.title',
-				defaultMessage: 'Visit server settings',
-			}),
-			shouldShow: (context: NagContext) => context.currentRoute !== 'type-id-settings-server',
-		},
-	},
-	{
-		id: 'add-bedrock-address',
-		title: defineMessage({
-			id: 'nags.add-bedrock-address.title',
-			defaultMessage: 'Add a Bedrock address',
-		}),
-		description: defineMessage({
-			id: 'nags.add-bedrock-address.description',
-			defaultMessage:
-				'If your server supports connections from Bedrock Edition players, add the IP address and port they can use to join.',
-		}),
-		status: 'suggestion',
-		shouldShow: (context: NagContext) =>
-			!!context.projectV3?.minecraft_server &&
-			!context.projectV3?.minecraft_bedrock_server?.address,
 		link: {
 			path: 'settings/server',
 			title: defineMessage({
