c886c2ad843ec83e03cc3467cbab5c50a03b75e9
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2026-03-05 19:07:41 -0800

    tweak status alerts

--- packages/moderation/src/data/stages/status-alerts.ts
+++ packages/moderation/src/data/stages/status-alerts.ts
@@ -1,6 +1,6 @@
 import { TriangleAlertIcon } from '@modrinth/assets'
 
-import type { ButtonAction, DropdownAction, DropdownActionOption } from '../../types/actions'
+import type { ButtonAction } from '../../types/actions'
 import type { Stage } from '../../types/stage'
 
 const statusAlerts: Stage = {
@@ -72,45 +72,11 @@ const statusAlerts: Stage = {
 				(await import('../messages/status-alerts/account_issues.md?raw')).default,
 		} as ButtonAction,
 		{
-			id: 'status_tec_source_request',
-			type: 'button',
-			label: `Request Source`,
-			suggestedStatus: 'rejected',
-			severity: 'critical',
-			disablesActions: ['status_corrections_applied', 'status_private_use'],
-			shouldShow: (project) =>
-				project.project_type === 'mod' ||
-				project.project_type === 'shader' ||
-				project.project_type.toString() === 'plugin',
-			weight: -999999,
-			message: async () => '',
-			enablesActions: [
-				{
-					id: 'status_tec_source_request_options',
-					type: 'dropdown',
-					label: 'Why are you requesting source?',
-					options: [
-						{
-							label: 'Obfuscated',
-							weight: 999999,
-							message: async () =>
-								(await import('../messages/status-alerts/tec/source_request-obfs.md?raw')).default,
-						} as DropdownActionOption,
-						{
-							label: 'Binaries',
-							weight: 999000,
-							message: async () =>
-								(await import('../messages/status-alerts/tec/source_request-bins.md?raw')).default,
-						} as DropdownActionOption,
-					],
-				} as DropdownAction,
-			],
-		} as ButtonAction,
-		{
 			id: 'status_automod_confusion',
 			type: 'button',
 			label: `Automod confusion`,
 			weight: -999999,
+			shouldShow: (project, projectV3) => !projectV3?.minecraft_server,
 			message: async () =>
 				(await import('../messages/status-alerts/automod_confusion.md?raw')).default,
 		} as ButtonAction,
