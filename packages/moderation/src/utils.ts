e36f961c11df98422c271024251476e81576cad7
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2026-03-05 17:31:23 -0800

    Tweak + add description messages

--- packages/moderation/src/utils.ts
+++ packages/moderation/src/utils.ts
@@ -334,9 +334,8 @@ export function flattenProjectVariables(
 	vars[`PROJECT_TITLE_FLINK`] = `[Name](https://modrinth.com/project/${project.id}/settings)`
 	vars[`PROJECT_SLUG_FLINK`] = `[URL](https://modrinth.com/project/${project.id}/settings)`
 	vars[`PROJECT_SUMMARY_FLINK`] = `[Summary](https://modrinth.com/project/${project.id}/settings)`
-	// Depreciated
 	vars[`PROJECT_ENVIRONMENT_FLINK`] =
-		`[Environment Information](https://modrinth.com/project/${project.id}/settings/environment)`
+		`[Environment Information](https://modrinth.com/project/${project.id}/settings/environment)` 	// Depreciated
 	vars[`PROJECT_TAGS_LINK`] = `https://modrinth.com/project/${project.id}/settings/tags`
 	vars[`PROJECT_TAGS_FLINK`] = `[Tags](https://modrinth.com/project/${project.id}/settings/tags)`
 	vars[`PROJECT_DESCRIPTION_LINK`] =
@@ -358,6 +357,10 @@ export function flattenProjectVariables(
 	vars[`PROJECT_MODERATION_LINK`] = `https://modrinth.com/project/${project.id}/moderation`
 	vars[`PROJECT_MODERATION_FLINK`] =
 		`[moderation tab](https://modrinth.com/project/${project.id}/moderation)`
+	vars[`PROJECT_SERVER_SETTINGS`] = `https://modrinth.com/project/${project.id}/settings/server`
+	vars[`PROJECT_SERVER_SETTINGS_FLINK`] = `[Server Settings](https://modrinth.com/project/${project.id}/settings/server)`
+	vars[`PROJECT_LANGUAGE_SETTINGS`] = `https://modrinth.com/project/${project.id}/settings/server`
+	vars[`PROJECT_LANGUAGE_SETTINGS_FLINK`] = `[Language Settings](https://modrinth.com/project/${project.id}/settings/server)`
 
 	return vars
 }
