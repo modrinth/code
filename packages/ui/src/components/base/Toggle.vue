3dad42a0ccd26bdb7a96170fe48b44ffec39d3dd
Calum H. (IMB11) <contact@cal.engineer>
2026-03-13 18:05:44 +0000

    fix: toggle gap

--- packages/ui/src/components/base/Toggle.vue
+++ packages/ui/src/components/base/Toggle.vue
@@ -7,7 +7,7 @@
 		:disabled="disabled"
 		class="relative inline-flex shrink-0 rounded-full m-0 transition-all duration-200 cursor-pointer border-none"
 		:class="[
-			small ? 'h-5 !w-[38px]' : 'h-8 !w-[52px]',
+			small ? 'h-5 !w-[40px]' : 'h-8 !w-[60px]',
 			modelValue ? 'bg-brand' : 'bg-button-bg',
 			disabled ? 'opacity-50 cursor-not-allowed' : 'btn-wrapper',
 		]"
@@ -19,8 +19,8 @@
 				small ? 'w-4 h-4 top-0.5 left-0.5' : 'w-[24px] h-[24px] top-1 left-1',
 				modelValue
 					? small
-						? 'translate-x-[18px] bg-black/90'
-						: 'translate-x-5 bg-black/90'
+						? 'translate-x-5 bg-black/90'
+						: 'translate-x-7 bg-black/90'
 					: 'bg-gray',
 			]"
 		/>
