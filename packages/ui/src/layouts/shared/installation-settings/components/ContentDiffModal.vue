63ea8230ba5868a1f197ea7e13d616dab2111727
Calum H. <calum@modrinth.com>
2026-03-17 17:47:58 +0000

    feat: content tab QA fixes for panel pre-release (#5588)
    * feat: use new_filters + other qa fixes
    
    * fix: double admons + lint

--- packages/ui/src/layouts/shared/installation-settings/components/ContentDiffModal.vue
+++ packages/ui/src/layouts/shared/installation-settings/components/ContentDiffModal.vue
@@ -3,15 +3,10 @@
 		<div class="max-w-[500px]">
 			<div class="flex flex-col gap-4 p-4">
 				<Admonition :type="hasUnknownContent ? 'warning' : 'info'" :header="admonitionHeader">
-					{{ description }}
-				</Admonition>
-
-				<Admonition
-					v-if="hasUnknownContent"
-					type="warning"
-					:header="formatMessage(messages.unknownContentHeader)"
-				>
-					{{ formatMessage(messages.unknownContentBody) }}
+					<div class="flex flex-col gap-2">
+						<span>{{ description }}</span>
+						<span v-if="hasUnknownContent">{{ formatMessage(messages.unknownContentBody) }}</span>
+					</div>
 				</Admonition>
 
 				<div v-if="diffs.length" class="flex gap-2">
@@ -191,10 +186,6 @@ const messages = defineMessages({
 	updatedCount: {
 		id: 'content.diff-modal.updated-count',
 		defaultMessage: '{count} updated',
-	},
-	unknownContentHeader: {
-		id: 'content.diff-modal.unknown-content-header',
-		defaultMessage: 'Unknown content',
 	},
 	unknownContentBody: {
 		id: 'content.diff-modal.unknown-content-body',
