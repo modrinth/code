c7ffd4bb9729092d161a5a19671f0516b15c1978
Prospector <6166773+Prospector@users.noreply.github.com>
2026-03-26 13:42:26 -0700

    fix: use ButtonStyled for app

--- apps/app-frontend/src/components/ui/settings/FeatureFlagSettings.vue
+++ apps/app-frontend/src/components/ui/settings/FeatureFlagSettings.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Button, Toggle } from '@modrinth/ui'
+import { ButtonStyled, Toggle } from '@modrinth/ui'
 import { ref, watch } from 'vue'
 
 import { get as getSettings, set as setSettings } from '@/helpers/settings.ts'
@@ -32,17 +32,19 @@ watch(
 			</h2>
 		</div>
 		<div class="flex items-center gap-2">
+			<ButtonStyled type="transparent">
+				<button
+					:disabled="themeStore.getFeatureFlag(option) === DEFAULT_FEATURE_FLAGS[option]"
+					@click="setFeatureFlag(option, DEFAULT_FEATURE_FLAGS[option])"
+				>
+					Reset to default
+				</button>
+			</ButtonStyled>
 			<Toggle
 				id="advanced-rendering"
 				:model-value="themeStore.getFeatureFlag(option)"
 				@update:model-value="() => setFeatureFlag(option, !themeStore.getFeatureFlag(option))"
 			/>
-			<Button
-				:disabled="themeStore.getFeatureFlag(option) === DEFAULT_FEATURE_FLAGS[option]"
-				@click="setFeatureFlag(option, DEFAULT_FEATURE_FLAGS[option])"
-			>
-				Reset to default
-			</Button>
 		</div>
 	</div>
 </template>
