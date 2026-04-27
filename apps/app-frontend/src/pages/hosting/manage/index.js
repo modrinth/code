3cd18b72053c08f56f6deab1533cdde678901341
Calum H. (IMB11) <contact@cal.engineer>
2026-04-27 13:02:46 +0100

    feat: world switching page implementation

--- apps/app-frontend/src/pages/hosting/manage/index.js
+++ apps/app-frontend/src/pages/hosting/manage/index.js
@@ -3,5 +3,6 @@ import Content from './Content.vue'
 import Files from './Files.vue'
 import Index from './Index.vue'
 import Overview from './Overview.vue'
+import Worlds from './Worlds.vue'
 
-export { Backups, Content, Files, Index, Overview }
+export { Backups, Content, Files, Index, Overview, Worlds }
