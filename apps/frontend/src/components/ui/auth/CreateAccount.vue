9184de92df135d691e3ccee18d297bc14787684d
tdgao <mr.trumgao@gmail.com>
2026-04-30 10:11:58 -0600

    fix: calculate age based on user's timezone

--- apps/frontend/src/components/ui/auth/CreateAccount.vue
+++ apps/frontend/src/components/ui/auth/CreateAccount.vue
@@ -136,7 +136,12 @@ const maxInputDate = computed(() => `${new Date().getFullYear()}-12-31`)
 const maxBirthDate = computed(() => {
 	const date = new Date()
 	date.setFullYear(date.getFullYear() - 13)
-	return date.toISOString().slice(0, 10)
+
+	const year = date.getFullYear()
+	const month = String(date.getMonth() + 1).padStart(2, '0')
+	const day = String(date.getDate()).padStart(2, '0')
+
+	return `${year}-${month}-${day}`
 })
 
 const getBirthYear = (dateOfBirth: string | null): number | null => {
