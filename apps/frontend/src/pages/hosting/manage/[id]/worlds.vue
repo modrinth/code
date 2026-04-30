6a78e67f1ebdd6b5c2e1f65dd11e2146aa8b6e0d
Calum H. (IMB11) <contact@cal.engineer>
2026-04-30 17:23:24 +0100

    fix: worlds layout

--- apps/frontend/src/pages/hosting/manage/[id]/worlds.vue
+++ apps/frontend/src/pages/hosting/manage/[id]/worlds.vue
@@ -1,30 +1,7 @@
 <script setup lang="ts">
-import {
-	commonMessages,
-	defineMessages,
-	injectModrinthServerContext,
-	ServersManageWorldsPage,
-	useVIntl,
-} from '@modrinth/ui'
-
-const { server } = injectModrinthServerContext()
-const { formatMessage } = useVIntl()
-
-const messages = defineMessages({
-	title: {
-		id: 'servers.manage.worlds.meta.title',
-		defaultMessage: 'Worlds - {server} - Modrinth',
-	},
-})
-
-useHead({
-	title: () =>
-		formatMessage(messages.title, {
-			server: server.value?.name ?? formatMessage(commonMessages.serverLabel),
-		}),
-})
+const route = useNativeRoute()
 </script>
 
 <template>
-	<ServersManageWorldsPage />
+	<NuxtPage :route="route" />
 </template>
