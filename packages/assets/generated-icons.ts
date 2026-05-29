04bdbd2d0aee3bd37e7d5bef54320d94f672e354
Prospector <6166773+Prospector@users.noreply.github.com>
2026-05-28 17:47:08 -0700

    prepr

--- packages/assets/generated-icons.ts
+++ packages/assets/generated-icons.ts
@@ -378,8 +378,8 @@ import _TrashExclamationIcon from './icons/trash-exclamation.svg?component'
 import _TriangleAlertIcon from './icons/triangle-alert.svg?component'
 import _UnderlineIcon from './icons/underline.svg?component'
 import _UndoIcon from './icons/undo.svg?component'
-import _UnfoldVerticalIcon from './icons/unfold-vertical.svg?component'
 import _UnfoldHorizontalIcon from './icons/unfold-horizontal.svg?component'
+import _UnfoldVerticalIcon from './icons/unfold-vertical.svg?component'
 import _UnknownIcon from './icons/unknown.svg?component'
 import _UnknownDonationIcon from './icons/unknown-donation.svg?component'
 import _UnlinkIcon from './icons/unlink.svg?component'
@@ -777,8 +777,8 @@ export const TrashExclamationIcon = _TrashExclamationIcon
 export const TriangleAlertIcon = _TriangleAlertIcon
 export const UnderlineIcon = _UnderlineIcon
 export const UndoIcon = _UndoIcon
-export const UnfoldVerticalIcon = _UnfoldVerticalIcon
 export const UnfoldHorizontalIcon = _UnfoldHorizontalIcon
+export const UnfoldVerticalIcon = _UnfoldVerticalIcon
 export const UnknownIcon = _UnknownIcon
 export const UnknownDonationIcon = _UnknownDonationIcon
 export const UnlinkIcon = _UnlinkIcon
