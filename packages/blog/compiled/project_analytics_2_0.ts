206813b74c4302b81752e4c929c7dc0abb939fd2
Calum H. <calum@modrinth.com>
2026-05-29 20:47:44 +0100

    feat: analytics 2.0 blogpost (#6235)
    * feat: analytics 2.0 blogpost
    
    * update blog post
    
    * performance: compress mp4s
    
    * date
    
    ---------
    
    Co-authored-by: Prospector <6166773+Prospector@users.noreply.github.com>
    Co-authored-by: tdgao <mr.trumgao@gmail.com>

---
+++ packages/blog/compiled/project_analytics_2_0.ts
@@ -0,0 +1,12 @@
+// AUTO-GENERATED FILE - DO NOT EDIT
+export const article = {
+  html: () => import(`./project_analytics_2_0.content`).then(m => m.html),
+  title: "Project Analytics 2.0",
+  summary: "New query builder, breakdowns, filtering, and richer project data.",
+  date: "2026-05-29T11:00:00.000Z",
+  slug: "project-analytics-2.0",
+  authors: [],
+  unlisted: false,
+  thumbnail: false,
+  
+};
