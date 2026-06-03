6707a703246928a374b65dfd7ffceb42a1ba82c6
Prospector <6166773+Prospector@users.noreply.github.com>
2026-06-03 13:20:40 -0700

    blog + prepr

---
+++ packages/blog/compiled/server_access.ts
@@ -0,0 +1,12 @@
+// AUTO-GENERATED FILE - DO NOT EDIT
+export const article = {
+  html: () => import(`./server_access.content`).then(m => m.html),
+  title: "Manage servers together",
+  summary: "Add other users to your server, assign roles, and track what’s changed.",
+  date: "2026-06-03T20:10:28.823Z",
+  slug: "server-access",
+  authors: [""],
+  unlisted: false,
+  thumbnail: true,
+  
+};
