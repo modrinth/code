6387092e77cce9efc80521e6ca7ccbec1568e4c4
Calum H. (IMB11) <contact@cal.engineer>
2026-06-05 12:00:00 +0100

    fix: wrong loader icon on server panel

--- packages/ui/src/components/servers/icons/LoaderIcon.vue
+++ packages/ui/src/components/servers/icons/LoaderIcon.vue
@@ -1,6 +1,6 @@
 <template>
 	<svg
-		v-if="loader === 'Fabric'"
+		v-if="normalizedLoader === 'Fabric'"
 		xmlns="http://www.w3.org/2000/svg"
 		xml:space="preserve"
 		fill-rule="evenodd"
@@ -19,7 +19,7 @@
 		/>
 	</svg>
 	<svg
-		v-else-if="loader === 'Quilt'"
+		v-else-if="normalizedLoader === 'Quilt'"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
 		xml:space="preserve"
 		fill-rule="evenodd"
@@ -59,7 +59,7 @@
 		></path>
 	</svg>
 	<svg
-		v-else-if="loader === 'Forge'"
+		v-else-if="normalizedLoader === 'Forge'"
 		ml:space="preserve"
 		fill-rule="evenodd"
 		stroke-linecap="round"
@@ -77,7 +77,7 @@
 		></path>
 	</svg>
 	<svg
-		v-else-if="loader === 'NeoForge'"
+		v-else-if="normalizedLoader === 'NeoForge'"
 		enable-background="new 0 0 24 24"
 		version="1.1"
 		viewBox="0 0 24 24"
@@ -109,7 +109,7 @@
 		</g>
 	</svg>
 	<svg
-		v-else-if="loader === 'Paper'"
+		v-else-if="normalizedLoader === 'Paper'"
 		xml:space="preserve"
 		fill-rule="evenodd"
 		stroke-linecap="round"
@@ -124,7 +124,7 @@
 		<path fill="currentColor" d="m12 18-4-2 10-9-6 11Z" />
 	</svg>
 	<svg
-		v-else-if="loader === 'Spigot'"
+		v-else-if="normalizedLoader === 'Spigot'"
 		viewBox="0 0 332 284"
 		style="
 			fill-rule: evenodd;
@@ -141,7 +141,7 @@
 		/>
 	</svg>
 	<svg
-		v-else-if="loader === 'Bukkit'"
+		v-else-if="normalizedLoader === 'Bukkit'"
 		viewBox="0 0 292 319"
 		style="fill-rule: evenodd; clip-rule: evenodd; stroke-linecap: round; stroke-linejoin: round"
 		stroke="currentColor"
@@ -154,7 +154,7 @@
 		</g>
 	</svg>
 	<svg
-		v-else-if="loader === 'Purpur'"
+		v-else-if="normalizedLoader === 'Purpur'"
 		xml:space="preserve"
 		fill-rule="evenodd"
 		stroke-linecap="round"
@@ -212,7 +212,7 @@
 			transform="matrix(-1.125 0 0 1.2569 309 -40.78)"
 		></use>
 	</svg>
-	<svg v-else-if="loader === 'Vanilla'" viewBox="0 0 20 20" fill="currentColor">
+	<svg v-else-if="normalizedLoader === 'Vanilla'" viewBox="0 0 20 20" fill="currentColor">
 		<path
 			fill-rule="evenodd"
 			d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
@@ -224,10 +224,13 @@
 
 <script setup lang="ts">
 import { LoaderIcon } from '@modrinth/assets'
+import { computed } from 'vue'
 
-import type { ServerLoader } from '#ui/utils/loaders'
+import { formatLoaderLabel, type ServerLoader } from '#ui/utils/loaders'
 
-defineProps<{
+const props = defineProps<{
 	loader: ServerLoader
 }>()
+
+const normalizedLoader = computed(() => formatLoaderLabel(props.loader))
 </script>
