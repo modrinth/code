faf600f9065a2cebb6666fe191ac81cf308b3045
fraa2a <fraa2a@users.noreply.github.com>
2026-06-05 17:14:52 +0200

    Apply upstream 215643c84 server project title fix

--- apps/frontend/src/pages/[type]/[project].vue
+++ apps/frontend/src/pages/[type]/[project].vue
@@ -2282,9 +2282,8 @@ const hasEditDetailsPermission = computed(() => {
 
 const projectTypeDisplay = computed(() => {
 	if (!project.value) return ''
-	return formatProjectType(
-		data.$getProjectTypeForDisplay(project.value.project_type, project.value.loaders),
-	)
+	const projectType = isServerProject.value ? 'minecraft_java_server' : project.value.project_type
+	return formatProjectType(data.$getProjectTypeForDisplay(projectType, project.value.loaders))
 })
 
 const following = computed(() => {
