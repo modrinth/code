c3a6db61de16ee669b9f4e2df88f3750468050ac
François-X. T. <fetch@ferrous.ch>
2026-06-16 17:34:32 -0400

    fix(frontend): don't strip ws path

--- packages/api-client/src/utils/node-url.ts
+++ packages/api-client/src/utils/node-url.ts
@@ -1,13 +1,12 @@
-const NODE_AUTH_PATH_REGEX = /\/modrinth\/v\d+\/(?:fs|ws)\/?$/
+const NODE_FS_PATH_REGEX = /\/modrinth\/v\d+\/fs\/?$/
 const HTTP_SCHEME_REGEX = /^https?:\/\//i
 const WS_SCHEME_REGEX = /^wss?:\/\//i
 
 export function getNodeBaseUrl(url: string): string {
-	const baseUrl = url.replace(NODE_AUTH_PATH_REGEX, '')
+	const baseUrl = url.replace(NODE_FS_PATH_REGEX, '')
 	return HTTP_SCHEME_REGEX.test(baseUrl) ? baseUrl : `https://${baseUrl}`
 }
 
 export function getNodeWebSocketUrl(url: string): string {
-	const baseUrl = url.replace(NODE_AUTH_PATH_REGEX, '')
-	return WS_SCHEME_REGEX.test(baseUrl) ? baseUrl : `wss://${baseUrl}`
+	return WS_SCHEME_REGEX.test(url) ? url : `wss://${url}`
 }
