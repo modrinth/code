3bc567b5295dfd9d1e742cc2c44bfa5006ba1111
François-X. T. <fetch@ferrous.ch>
2026-06-16 17:17:11 -0400

    fix(frontend): actually do the same for websocket

--- packages/api-client/src/utils/node-url.ts
+++ packages/api-client/src/utils/node-url.ts
@@ -1,7 +1,13 @@
-const NODE_FS_PATH_REGEX = /\/modrinth\/v\d+\/fs\/?$/
+const NODE_AUTH_PATH_REGEX = /\/modrinth\/v\d+\/(?:fs|ws)\/?$/
 const HTTP_SCHEME_REGEX = /^https?:\/\//i
+const WS_SCHEME_REGEX = /^wss?:\/\//i
 
 export function getNodeBaseUrl(url: string): string {
-	const baseUrl = url.replace(NODE_FS_PATH_REGEX, '')
+	const baseUrl = url.replace(NODE_AUTH_PATH_REGEX, '')
 	return HTTP_SCHEME_REGEX.test(baseUrl) ? baseUrl : `https://${baseUrl}`
+}
+
+export function getNodeWebSocketUrl(url: string): string {
+	const baseUrl = url.replace(NODE_AUTH_PATH_REGEX, '')
+	return WS_SCHEME_REGEX.test(baseUrl) ? baseUrl : `wss://${baseUrl}`
 }
