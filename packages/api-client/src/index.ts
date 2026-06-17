815d4db8c2dab430b7458160435a4ef4c5f3fe38
François-X. T. <fetch@ferrous.ch>
2026-06-17 15:48:18 -0400

    chore: fix export

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -43,6 +43,7 @@ export { XHRUploadClient } from './platform/xhr-upload-client'
 export { clearNodeAuthState, nodeAuthState, setNodeAuthState } from './state/node-auth'
 export * from './types'
 export { withJWTRetry } from './utils/jwt-retry'
+export { getNodeWebSocketUrl } from './utils/node-url'
 export {
 	type ParsedSseEvent,
 	type ParsedSseItem,
@@ -50,5 +51,4 @@ export {
 	parseSyncEventData,
 	SseParser,
 } from './utils/sse'
-export { getNodeWebSocketUrl } from './utils/node-url'
 export type { Override, RawDecimal } from './utils/types'
