18a3b8dd2f44307d690b78902a70273117b2e606
François-X. T. <fetch@ferrous.ch>
2026-06-24 15:22:54 -0400

    feat(labrinth): include next charge's tax amount

--- apps/labrinth/src/routes/internal/billing.rs
+++ apps/labrinth/src/routes/internal/billing.rs
@@ -99,6 +99,13 @@ struct SubscriptionsQuery {
     pub user_id: Option<ariadne::ids::UserId>,
 }
 
+#[derive(Serialize)]
+struct UserSubscriptionWithNextChargeTaxAmount {
+    #[serde(flatten)]
+    pub subscription: UserSubscription,
+    pub next_charge_tax_amount: Option<i64>,
+}
+
 #[get("subscriptions")]
 pub async fn subscriptions(
     req: HttpRequest,
@@ -117,7 +124,7 @@ pub async fn subscriptions(
     .await?
     .1;
 
-    let subscriptions =
+    let db_subscriptions =
         user_subscription_item::DBUserSubscription::get_all_user(
             if let Some(user_id) = query.user_id {
                 if user.role.is_admin() {
@@ -133,10 +140,20 @@ pub async fn subscriptions(
             },
             &**pool,
         )
-        .await?
-        .into_iter()
-        .map(UserSubscription::from)
-        .collect::<Vec<_>>();
+        .await?;
+
+    let mut subscriptions = Vec::with_capacity(db_subscriptions.len());
+    for subscription in db_subscriptions {
+        let next_charge_tax_amount =
+            DBCharge::get_open_subscription(subscription.id, &**pool)
+                .await?
+                .map(|charge| charge.tax_amount);
+
+        subscriptions.push(UserSubscriptionWithNextChargeTaxAmount {
+            subscription: UserSubscription::from(subscription),
+            next_charge_tax_amount,
+        });
+    }
 
     Ok(HttpResponse::Ok().json(subscriptions))
 }
