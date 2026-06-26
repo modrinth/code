e623cfa3e2c5951ddfee38d4535d7ca11bdab2bf
Calum H. (IMB11) <contact@cal.engineer>
2026-06-26 16:07:22 +0100

    fix: remove triggers use instant

--- packages/app-lib/migrations/20260626160000_clamp-negative-instance-playtime.sql
+++ packages/app-lib/migrations/20260626160000_clamp-negative-instance-playtime.sql
@@ -10,25 +10,3 @@ SET
 	END
 WHERE submitted_time_played < 0
 	OR recent_time_played < 0;
-
-CREATE TRIGGER instances_playtime_non_negative_insert
-BEFORE INSERT ON instances
-FOR EACH ROW
-WHEN NEW.submitted_time_played < 0
-	OR NEW.recent_time_played < 0
-	OR typeof(NEW.submitted_time_played) != 'integer'
-	OR typeof(NEW.recent_time_played) != 'integer'
-BEGIN
-	SELECT RAISE(ABORT, 'instance playtime must be a non-negative integer');
-END;
-
-CREATE TRIGGER instances_playtime_non_negative_update
-BEFORE UPDATE OF submitted_time_played, recent_time_played ON instances
-FOR EACH ROW
-WHEN NEW.submitted_time_played < 0
-	OR NEW.recent_time_played < 0
-	OR typeof(NEW.submitted_time_played) != 'integer'
-	OR typeof(NEW.recent_time_played) != 'integer'
-BEGIN
-	SELECT RAISE(ABORT, 'instance playtime must be a non-negative integer');
-END;
