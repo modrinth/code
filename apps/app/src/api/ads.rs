70c79f43ba1fe9c06cf22d5488832e8b28af064a
Prospector <6166773+Prospector@users.noreply.github.com>
2026-07-21 12:35:14 -0700

    fix: devtools opening on ads init

--- apps/app/src/api/ads.rs
+++ apps/app/src/api/ads.rs
@@ -582,8 +582,6 @@ pub async fn init_ads_window<R: Runtime>(
                 }
             })?;
 
-            webview.open_devtools();
-
             Some(webview)
         } else {
             None
