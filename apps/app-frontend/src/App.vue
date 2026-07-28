e0b395683757a72e82a7c320af3f275aa97eb535
Prospector <6166773+Prospector@users.noreply.github.com>
2026-07-28 12:33:57 -0700

    make quick instances resizable

--- apps/app-frontend/src/App.vue
+++ apps/app-frontend/src/App.vue
@@ -1555,7 +1555,6 @@ provideAppUpdateDownloadProgress(appUpdateDownload)
 			>
 				<ServerStackIcon />
 			</NavButton>
-			<div class="h-px w-6 mx-auto my-2 bg-surface-5"></div>
 			<suspense>
 				<QuickInstanceSwitcher />
 			</suspense>
