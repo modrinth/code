91f8d25b0cc3b9b297193b2983e0bfd1d4ae46aa
Calum H. (IMB11) <contact@cal.engineer>
2026-08-03 18:47:51 +0100

    fix: bad merge

--- packages/ui/src/components/servers/server-header/ServerInstanceManageHeader.vue
+++ packages/ui/src/components/servers/server-header/ServerInstanceManageHeader.vue
@@ -1,12 +1,75 @@
 <template>
 	<div class="w-full flex flex-col gap-4" :class="{ 'mt-4': isNuxt }">
-		<PageHeader
-			:header="props.name || props.fallbackName"
-			:leading="leadingItems"
-			:metadata="headerMetadata"
-			:actions="props.actions"
-			:header-class="props.headerClass"
-		/>
+		<PageHeader :title="props.name || props.fallbackName" :header-class="props.headerClass">
+			<template #leading>
+				<div class="flex size-16 shrink-0 items-center justify-center">
+					<ButtonStyled circular size="large">
+						<button
+							v-tooltip="props.backLabel"
+							type="button"
+							:aria-label="props.backLabel"
+							@click="router.push(props.backHref)"
+						>
+							<LeftArrowIcon aria-hidden="true" />
+						</button>
+					</ButtonStyled>
+				</div>
+			</template>
+
+			<template #metadata>
+				<PageHeaderMetadata>
+					<PageHeaderMetadataItem
+						v-for="item in headerMetadata"
+						:key="item.id"
+						:icon="item.icon"
+						:icon-props="item.iconProps"
+					>
+						{{ item.label }}
+					</PageHeaderMetadataItem>
+				</PageHeaderMetadata>
+			</template>
+
+			<template #actions>
+				<PageHeaderActions>
+					<template v-for="action in props.actions" :key="action.id">
+						<JoinedButtons
+							v-if="action.joinedActions?.length"
+							:actions="action.joinedActions"
+							:color="action.color ?? 'standard'"
+							:size="action.size ?? 'large'"
+							:disabled="action.disabled"
+							:primary-disabled="action.primaryDisabled"
+							:dropdown-disabled="action.dropdownDisabled"
+							:primary-muted="action.primaryMuted"
+						/>
+						<ButtonStyled
+							v-else
+							:color="action.color ?? 'standard'"
+							:size="action.size ?? 'large'"
+							:type="action.type ?? 'standard'"
+							:circular="action.circular ?? action.labelHidden ?? false"
+						>
+							<button
+								v-tooltip="action.tooltip"
+								type="button"
+								:disabled="action.disabled"
+								:aria-label="action.ariaLabel ?? action.tooltip ?? action.label"
+								@click="action.onClick"
+							>
+								<component
+									:is="action.icon"
+									v-if="action.icon"
+									:class="action.iconClass"
+									aria-hidden="true"
+									v-bind="action.iconProps"
+								/>
+								<span v-if="!action.labelHidden && !action.circular">{{ action.label }}</span>
+							</button>
+						</ButtonStyled>
+					</template>
+				</PageHeaderActions>
+			</template>
+		</PageHeader>
 	</div>
 </template>
 
@@ -16,7 +79,12 @@ import { LeftArrowIcon, TagCategoryGamepad2Icon as Gamepad2Icon, TimerIcon } fro
 import { type Component, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
-import PageHeader from '#ui/components/base/PageHeader.vue'
+import ButtonStyled from '#ui/components/base/ButtonStyled.vue'
+import JoinedButtons, { type JoinedButtonAction } from '#ui/components/base/JoinedButtons.vue'
+import PageHeader from '#ui/components/base/page-header/index.vue'
+import PageHeaderMetadata from '#ui/components/base/page-header/metadata/index.vue'
+import PageHeaderMetadataItem from '#ui/components/base/page-header/metadata/page-header-metadata-item.vue'
+import PageHeaderActions from '#ui/components/base/page-header/page-header-actions.vue'
 import LoaderIcon from '#ui/components/servers/icons/LoaderIcon.vue'
 import { injectModrinthClient } from '#ui/providers'
 import { formatLoaderLabel } from '#ui/utils/loaders'
@@ -42,14 +110,7 @@ type HeaderAction = {
 	color?: 'standard' | 'brand' | 'red' | 'orange' | 'green' | 'blue' | 'purple'
 	size?: 'standard' | 'large' | 'small'
 	type?: 'standard' | 'outlined' | 'transparent' | 'highlight' | 'highlight-colored-text' | 'chip'
-	joinedActions?: {
-		id: string
-		label: string
-		icon?: Component
-		action: () => void
-		color?: 'standard' | 'brand' | 'red' | 'orange' | 'green' | 'blue' | 'purple'
-		hoverFilled?: boolean
-	}[]
+	joinedActions?: JoinedButtonAction[]
 	primaryDisabled?: boolean
 	dropdownDisabled?: boolean
 	primaryMuted?: boolean
@@ -85,16 +146,6 @@ const props = withDefaults(
 const client = injectModrinthClient()
 const router = useRouter()
 const isNuxt = computed(() => client instanceof NuxtModrinthClient)
-const leadingItems = computed(() => [
-	{
-		id: 'back',
-		type: 'button' as const,
-		icon: LeftArrowIcon,
-		ariaLabel: props.backLabel,
-		tooltip: props.backLabel,
-		onClick: () => router.push(props.backHref),
-	},
-])
 const loaderLabel = computed(() => {
 	if (!props.loader) return null
 	const label = formatLoaderLabel(props.loader.toLowerCase())
