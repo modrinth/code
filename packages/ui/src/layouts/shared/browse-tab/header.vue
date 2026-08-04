0c3e2767750eadbc8ac4abdd1ca46c9e00bb794b
tdgao <mr.trumgao@gmail.com>
2026-08-03 23:50:53 -0600

    fix spacing

--- packages/ui/src/layouts/shared/browse-tab/header.vue
+++ packages/ui/src/layouts/shared/browse-tab/header.vue
@@ -179,7 +179,7 @@ async function handleSelectedProjectsLeaveResult(
 				</PageHeaderMetadata>
 			</template>
 		</PageHeader>
-		<Admonition v-if="installContext.warning" type="warning" class="mb-1">
+		<Admonition v-if="installContext.warning" type="warning" class="mt-4 mb-1">
 			{{ installContext.warning }}
 		</Admonition>
 	</template>
