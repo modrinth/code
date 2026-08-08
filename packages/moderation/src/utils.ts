dee70105552f6b719817fbd6d94f3dc4e48bfda0
coolbot <76798835+coolbot100s@users.noreply.github.com>
2026-08-07 20:57:06 -0700

    update r4 messages

--- packages/moderation/src/utils.ts
+++ packages/moderation/src/utils.ts
@@ -249,7 +249,7 @@ export function flattenProjectVariables(
 	vars[`PROJECT_CONTENT_DISCLOSURES_LINK`] =
 		`https://modrinth.com/project/${project.id}/settings/disclosures`
 	vars[`PROJECT_CONTENT_DISCLOSURES_FLINK`] =
-		`[Content disclosures](https://modrinth.com/project/${project.id}/settings/disclosures)`
+		`[Content Disclosures](https://modrinth.com/project/${project.id}/settings/disclosures)`
 
 	return vars
 }
