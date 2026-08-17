0efd585a5e58dcdf1f98315c15445a0479b2dcbf
sychic <47618543+Sychic@users.noreply.github.com>
2026-08-17 09:27:38 -0400

    refactor(component-derive): split impls to separate functions

--- packages/component-derive/src/component.rs
+++ packages/component-derive/src/component.rs
@@ -33,6 +33,8 @@ pub fn derive(input: &DeriveInput) -> Result<TokenStream> {
     let fields = &fields.fields;
     let struct_serial = struct_serial(&vis, &ident, fields)?;
     let struct_partial = struct_partial(&vis, &ident, fields)?;
+    let impl_apply_to = impl_apply_to(&ident, fields);
+    let impl_into_diff_from = impl_into_diff_from(&ident, fields);
 
     // `#[validate(nested)]` needs `Validate` in scope; `as _` avoids a name clash
     let validate_import = if fields.iter().any(|field| field.nested) {
@@ -46,6 +48,9 @@ pub fn derive(input: &DeriveInput) -> Result<TokenStream> {
 
         #struct_serial
         #struct_partial
+
+        #impl_apply_to
+        #impl_into_diff_from
     })
 }
 
@@ -111,83 +116,58 @@ fn struct_partial(
 ) -> Result<TokenStream> {
     let ident_partial = format_ident!("Partial{ident}");
 
-    let (fields, (apply_fields, diff_fields)): (Vec<_>, (Vec<_>, Vec<_>)) =
-        fields
-            .iter()
-            .filter_map(|field| {
-                if field.synthetic {
-                    return None;
-                }
+    let fields = fields
+        .iter()
+        .filter_map(|field| {
+            if field.synthetic {
+                return None;
+            }
 
-                let ident = &field
-                    .ident
-                    .as_ref()
-                    .expect("macro only works on structs with named fields");
-                let vis = &field.vis;
-                let ty = &field.ty;
-                let attrs = &field.attrs;
+            let ident = &field
+                .ident
+                .as_ref()
+                .expect("macro only works on structs with named fields");
+            let vis = &field.vis;
+            let ty = &field.ty;
+            let attrs = &field.attrs;
 
-                let (inner_ty, apply_value, diff_value, validate_attr) =
-                    if field.nested {
-                        let inner_ty = match nested_type(ty, "Partial") {
-                            Ok(inner_ty) => inner_ty,
-                            Err(err) => return Some(Err(err)),
-                        };
-                        (
-                            inner_ty,
-                            quote! { t.apply_to(&mut component.#ident) },
-                            quote! { self.#ident.into_diff_from(&base.#ident) },
-                            quote! { #[validate(nested)] },
-                        )
-                    } else {
-                        (
-                            quote! { #ty },
-                            quote! { component.#ident = t },
-                            quote! { self.#ident },
-                            quote! {},
-                        )
-                    };
-
-                let serde_attr = if !field.nested
-                    && let Type::Path(path) = ty
-                    && path
-                        .path
-                        .segments
-                        .first()
-                        .is_some_and(|segment| segment.ident == "Option")
-                {
-                    quote! {
-                        #[serde(
-                            default,
-                            skip_serializing_if = "::core::option::Option::is_none",
-                            with = "::serde_with::rust::double_option"
-                        )]
-                    }
-                } else {
-                    quote! { #[serde(default, skip_serializing_if = "::core::option::Option::is_none")] }
+            let (inner_ty, validate_attr) = if field.nested {
+                let inner_ty = match nested_type(ty, "Partial") {
+                    Ok(inner_ty) => inner_ty,
+                    Err(err) => return Some(Err(err)),
                 };
+                (inner_ty, quote! { #[validate(nested)] })
+            } else {
+                (quote! { #ty }, quote! {})
+            };
 
-                Some(Ok((
-                    quote! {
-                        #(#attrs)*
-                        #validate_attr
-                        #serde_attr
-                        #vis #ident: ::core::option::Option<#inner_ty>
-                    },
-                    (
-                        quote! {
-                            if let Some(t) = self.#ident {
-                                #apply_value;
-                            }
-                        },
-                        quote! {
-                            #ident: (self.#ident != base.#ident)
-                                .then(|| #diff_value)
-                        },
-                    ),
-                )))
-            })
-            .collect::<Result<(Vec<_>, (Vec<_>, Vec<_>))>>()?;
+            let serde_attr = if !field.nested
+                && let Type::Path(path) = ty
+                && path
+                    .path
+                    .segments
+                    .first()
+                    .is_some_and(|segment| segment.ident == "Option")
+            {
+                quote! {
+                    #[serde(
+                        default,
+                        skip_serializing_if = "::core::option::Option::is_none",
+                        with = "::serde_with::rust::double_option"
+                    )]
+                }
+            } else {
+                quote! { #[serde(default, skip_serializing_if = "::core::option::Option::is_none")] }
+            };
+
+            Some(Ok(quote! {
+                #(#attrs)*
+                #validate_attr
+                #serde_attr
+                #vis #ident: ::core::option::Option<#inner_ty>
+            }))
+        })
+        .collect::<Result<Vec<_>>>()?;
 
     Ok(quote! {
         #[derive(
@@ -201,16 +181,73 @@ fn struct_partial(
         #vis struct #ident_partial {
             #(#fields),*
         }
+    })
+}
 
+fn impl_apply_to(ident: &Ident, fields: &[ComponentField]) -> TokenStream {
+    let ident_partial = format_ident!("Partial{ident}");
+
+    let apply_fields = fields
+        .iter()
+        .filter_map(|field| {
+            if field.synthetic {
+                return None;
+            }
+
+            let ident = field
+                .ident
+                .as_ref()
+                .expect("macro only works on structs with named fields");
+            let apply_value = if field.nested {
+                quote! { t.apply_to(&mut component.#ident) }
+            } else {
+                quote! { component.#ident = t }
+            };
+
+            Some(quote! {
+                if let Some(t) = self.#ident {
+                    #apply_value;
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    quote! {
         impl #ident_partial {
-            pub fn apply_to(
-                self,
-                component: &mut #ident,
-            ) {
+            pub fn apply_to(self, component: &mut #ident) {
                 #(#apply_fields)*
             }
         }
+    }
+}
 
+fn impl_into_diff_from(ident: &Ident, fields: &[ComponentField]) -> TokenStream {
+    let ident_partial = format_ident!("Partial{ident}");
+
+    let diff_fields = fields
+        .iter()
+        .filter_map(|field| {
+            if field.synthetic {
+                return None;
+            }
+
+            let ident = field
+                .ident
+                .as_ref()
+                .expect("macro only works on structs with named fields");
+            let diff_value = if field.nested {
+                quote! { self.#ident.into_diff_from(&base.#ident) }
+            } else {
+                quote! { self.#ident }
+            };
+
+            Some(quote! {
+                #ident: (self.#ident != base.#ident).then(|| #diff_value)
+            })
+        })
+        .collect::<Vec<_>>();
+
+    quote! {
         impl #ident {
             pub fn into_diff_from(self, base: &Self) -> #ident_partial {
                 #ident_partial {
@@ -218,7 +255,7 @@ fn struct_partial(
                 }
             }
         }
-    })
+    }
 }
 
 fn nested_type(ty: &Type, prefix: &str) -> Result<TokenStream> {
