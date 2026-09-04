0694b509954ae9c4b45db8a390323eaee5529d7b
Calum H. (IMB11) <contact@cal.engineer>
2026-09-04 15:34:47 +0100

    fix: remove feature flag

--- apps/app-frontend/src/composables/use-app-settings.ts
+++ apps/app-frontend/src/composables/use-app-settings.ts
@@ -10,7 +10,6 @@ export const DEFAULT_FEATURE_FLAGS = {
 	show_sync_instances_update_modal: false,
 	pride_fundraiser: true,
 	i18n_debug: false,
-	ServerPlayFrontend: false,
 	localhost_sign_in: false,
 }
 
--- apps/frontend/src/pages/hosting/manage/[id].vue
+++ apps/frontend/src/pages/hosting/manage/[id].vue
@@ -5,7 +5,6 @@
 		:resolve-viewer="resolveViewer"
 		:show-copy-id-action="flags.developerMode"
 		:show-advanced-debug-info="flags.advancedDebugInfo"
-		:show-play-tab="flags.ServerPlayFrontend"
 		:stripe-publishable-key="config.public.stripePublishableKey as string"
 		:site-url="config.public.siteUrl as string"
 		:products="products"
