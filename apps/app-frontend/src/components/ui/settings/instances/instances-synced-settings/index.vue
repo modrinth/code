3a712cdfe0088400e9d48d69639adc2c4039a8e3
Calum H. (IMB11) <contact@cal.engineer>
2026-09-07 22:40:38 +0100

    fix: Syncing resource packs doesn't work Fixes #7441

--- apps/app-frontend/src/components/ui/settings/instances/instances-synced-settings/index.vue
+++ apps/app-frontend/src/components/ui/settings/instances/instances-synced-settings/index.vue
@@ -272,13 +272,13 @@ const baseSourcesLoading = computed(() =>
 	baseOption.value === null
 		? false
 		: baseOption.value === 'game_options'
-			? gameOptionSourcesQuery.isFetching.value
-			: instancesQuery.isFetching.value,
+			? gameOptionSourcesQuery.isPending.value
+			: instancesQuery.isPending.value,
 )
 const baseSourcesError = computed(() =>
 	baseOption.value === 'game_options'
-		? gameOptionSourcesQuery.isError.value
-		: instancesQuery.isError.value,
+		? gameOptionSourcesQuery.isError.value && !gameOptionSourcesQuery.data.value
+		: instancesQuery.isError.value && !instancesQuery.data.value,
 )
 let baseSourceGeneration = 0
 
