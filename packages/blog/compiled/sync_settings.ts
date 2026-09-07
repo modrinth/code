5709d73a90a4d1a5e5d990e60c96b1ec6ac69c6d
Calum H. <calum@modrinth.com>
2026-09-07 20:24:52 +0100

    chore: options.txt syncing blog (#7438)
    * chore: options.txt syncing blog
    
    * fix: authors
    
    * fix: vids
    
    * feat: update blog copy
    
    * feat: blog thumbnail

---
+++ packages/blog/compiled/sync_settings.ts
@@ -0,0 +1,12 @@
+// AUTO-GENERATED FILE - DO NOT EDIT
+export const article = {
+  html: () => import(`./sync_settings.content`).then(m => m.html),
+  title: "Sync settings across instances",
+  summary: "Keep game options, servers, resource packs, and more the same across your instances.",
+  date: "2026-09-07T19:00:00.000Z",
+  slug: "sync-settings",
+  authors: ["bOHH0P9Z","AJfd8YH6"],
+  unlisted: false,
+  thumbnail: true,
+  
+};
