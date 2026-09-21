b33628117a69901398270d0b8d48a2da91a842f8
aecsocket <43144841+aecsocket@users.noreply.github.com>
2026-09-21 14:38:46 +0100

    update migration for explicitness

--- apps/labrinth/migrations/20260921120000_link_platform_text.sql
+++ apps/labrinth/migrations/20260921120000_link_platform_text.sql
@@ -2,8 +2,46 @@ ALTER TABLE mods_links
 	ADD COLUMN platform TEXT;
 
 /*
-UPDATE mods_links ml
-SET platform = lp.name
-FROM link_platforms lp
-WHERE lp.id = ml.joining_platform_id;
+DO $$
+BEGIN
+	IF EXISTS (
+		SELECT 1
+		FROM link_platforms
+		WHERE name NOT IN (
+			'patreon',
+			'bmac',
+			'paypal',
+			'github',
+			'ko-fi',
+			'other',
+			'issues',
+			'wiki',
+			'discord',
+			'source',
+			'site',
+			'store'
+		)
+	) THEN
+		RAISE EXCEPTION 'invalid link platform found';
+	END IF;
+
+	UPDATE mods_links ml
+	SET platform = CASE lp.name
+		WHEN 'patreon' THEN 'patreon'
+		WHEN 'bmac' THEN 'bmac'
+		WHEN 'paypal' THEN 'paypal'
+		WHEN 'github' THEN 'github'
+		WHEN 'ko-fi' THEN 'kofi'
+		WHEN 'other' THEN 'other'
+		WHEN 'issues' THEN 'issues'
+		WHEN 'wiki' THEN 'wiki'
+		WHEN 'discord' THEN 'discord'
+		WHEN 'source' THEN 'source'
+		WHEN 'site' THEN 'site'
+		WHEN 'store' THEN 'store'
+	END
+	FROM link_platforms lp
+	WHERE lp.id = ml.joining_platform_id;
+END
+$$;
 */
