cb35a410764ef00d86a4a0778a6b69423d095aa4
Calum H. <calum@modrinth.com>
2026-09-21 15:36:45 +0100

    fix: perf issue loading instance pages (#7637)
    * fix: perf issue loading instance pages
    
    * fix: perf issue loading instance pages
    
    * fix: lint

--- packages/app-lib/src/state/instances/mod.rs
+++ packages/app-lib/src/state/instances/mod.rs
@@ -23,7 +23,8 @@ pub(crate) use self::commands::{
     dependencies_to_content_items, get_content_projects,
     get_installed_project_ids_for_instance, get_instance_install_candidates,
     get_linked_modpack_info, list_content, list_content_sets,
-    list_linked_modpack_content, refresh_content_updates, sync_content_files,
+    list_indexed_content, list_linked_modpack_content, refresh_content_updates,
+    sync_content_files,
 };
 pub(crate) use self::commands::{
     game_options_sync_is_enabled, load_game_option_preferences,
